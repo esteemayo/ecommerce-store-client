b19b213f228e5299987c3812d4a61455de6939a5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-04 14:16:00 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react'
+import styled from 'styled-components';
+
+const Navbar = () => {
+  return (
+    <Container>Navbar</Container>
+  );
+}
+
+const Container = styled.nav`
+  color: red;
+`
+
+export default Navbar
