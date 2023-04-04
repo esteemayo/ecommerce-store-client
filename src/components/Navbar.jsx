7e45069836c6a99e2380408a37bdcbe5bd3b205b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-04 14:44:02 +0100

    feat: add css properties

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,6 +30,9 @@ const Wrapper = styled.div`
   width: 100%;
   max-width: 102.4rem;
   margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const LogoBox = styled.div`
