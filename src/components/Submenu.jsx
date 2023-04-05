cc9be715ec7e01eb5f3694e93175492c089e73c2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-05 02:43:14 +0100

    fix: modify

--- src/components/Submenu.jsx
+++ src/components/Submenu.jsx
@@ -44,8 +44,6 @@ const Submenu = () => {
 
 const Container = styled.aside`
   padding: 2rem;
-  width: 50rem;
-  height: 40rem;
   background-color: #f4d4f5;
   border-radius: 1rem;
   position: absolute;
