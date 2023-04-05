8e783a4d9113c465035556f4f1408d1ae823abb2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-05 02:54:39 +0100

    feat: add a submenu slice

---
+++ src/features/submenu/submenuSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isSidebarOpen: false,
+  isSubmenuOpen: false,
+  location: {},
+  page: {
+    page: '',
+    links: [],
+  },
+};
+
+export const subMenuSlice = createSlice({
+  name: 'submenu',
+  initialState,
+  reducers: {
+    openSidebar: (state) => {
+      state.isSidebarOpen = true;
+    },
+    closeSidebar: (state) => {
+      state.isSidebarOpen = false;
+    },
+    openSubmenu: (state, { payload }) => {
+      console.log(payload);
+      state.isSubmenuOpen = true;
+    },
+    closeSubmenu: (state) => {
+      state.isSubmenuOpen = false;
+    },
+  },
+});
+
+export const { closeSidebar, closeSubmenu, openSidebar, openSubmenu } = subMenuSlice.actions;
+
+export default subMenuSlice.reducer;
