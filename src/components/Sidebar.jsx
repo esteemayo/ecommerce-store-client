88e41dc9e1a6eaeeefd86645dcbc96b3e0c41fb5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-05 20:11:38 +0100

    fix: modify

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch, useSelector } from 'react-redux';
+
 import { closeSidebar } from '@/features/submenu/submenuSlice';
 
 const Sidebar = () => {
