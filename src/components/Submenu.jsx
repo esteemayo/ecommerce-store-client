2226fda136de8f957e9e2c8d5f874bd6ba659dd1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-05 14:13:39 +0100

    feat: import react-redux module

--- src/components/Submenu.jsx
+++ src/components/Submenu.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 const Submenu = () => {
   const [columns, setColumns] = useState('col-2');
