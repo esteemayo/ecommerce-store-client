9ccb9041bba468aaa5140b01d0164d3057e92cc1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-05 20:40:49 +0100

    fix: modify

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -37,7 +37,7 @@ const Sidebar = () => {
                     );
                   })}
                 </ListsContainer>
-              </LinksContainer>
+              </LinksWrapper>
             );
           })}
         </LinksContainer>
