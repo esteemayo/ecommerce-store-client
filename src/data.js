bbc5fe8a9df0a4c1a017499be94e7a07ed71f567
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-05 14:03:34 +0100

    fix: add icon

--- src/data.js
+++ src/data.js
@@ -107,7 +107,7 @@ export const sublinks = [
     links: [
       {
         url: '/support-center',
-        icon: '',
+        icon: <FontAwesomeIcon icon={faPhone} />,
         label: 'support center',
       },
       {
