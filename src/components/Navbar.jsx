adb69340db6777677c3dc656669ee29b5a4c4e78
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-05 01:17:29 +0100

    fix: modify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
               type='button'
               className='link-btn'
             >
-              products
+              sells
             </Button>
           </ListItem>
           <ListItem>
