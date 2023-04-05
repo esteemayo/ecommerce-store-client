0d4fe2127733cb7f29701ed973464274d9c5888f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-05 01:51:01 +0100

    feat: add css properties

--- src/components/Submenu.jsx
+++ src/components/Submenu.jsx
@@ -13,6 +13,9 @@ const Container = styled.aside`
   height: 40rem;
   background-color: #f4d4f5;
   border-radius: 1rem;
+  position: absolute;
+  left: 50%;
+  top: 5rem;
 `
 
 export default Submenu;
