2bd7d6cf4cacfa039f6eaae2ca8565a57ad65ae1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-05 20:22:07 +0100

    fix: modify

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useDispatch, useSelector } from 'react-redux';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch, useSelector } from 'react-redux';
 
