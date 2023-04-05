641d4df69c609e27704e7fb9408e1ad7e0ef6644
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-05 02:41:31 +0100

    fix: modify

--- src/components/Submenu.jsx
+++ src/components/Submenu.jsx
@@ -88,7 +88,7 @@ const ListContainer = styled.ul`
   gap: 0.25rem 2rem;
   width: ${({ columns }) => columns === 'col-2' && '20rem'};
   width: ${({ columns }) => columns === 'col-3' && '30rem'};
-  width: ${({ columns }) => columns === 'col-4' && '40rem'};
+  width: ${({ columns }) => columns === 'col-4' && '50rem'};
 `;
 
 const ListItem = styled.li`
