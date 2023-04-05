a074087e6a54c20b616929246310490b257ed3c3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-05 01:48:38 +0100

    fix: modify

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
       */}
       <head />
       <body>
-        <div>
+        <div className='container'>
           <Navbar />
           <Submenu />
           {children}
