0051195169e9c0f81545311b74641774890d0ffd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-05 20:07:56 +0100

    fix: open sidebar if isSidebarOpen is true

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,8 +4,10 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux';
 
 const Sidebar = () => {
+  const { isSidebarOpen } = useSelector((state) => ({ ...state.submenu }));
+
   return (
-    <Container>
+    <Container type={isSidebarOpen}>
       <Wrapper>
         <CloseButton>
           <FontAwesomeIcon icon={faXmark} />
@@ -25,6 +27,10 @@ const Container = styled.aside`
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
+  visibility: ${({ type }) => type === 'show' ? 'visible' : 'hidden'};
+  transform: scale(${({ type }) => type ? 1 : 0});
+  z-index: ${({ type }) => type === 'show' ? 2 : -1};
+  transition: all 0.3s linear;
 
   display: flex;
   align-items: center;
