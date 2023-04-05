3c29be3d32106c890b4549c811f15f87b5fc0582
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-05 01:15:41 +0100

    feat: add submenu data

---
+++ src/data.js
@@ -0,0 +1,127 @@
+export const links = [
+  {
+    page: 'products',
+    sublinks: [
+      {
+        url: '/featured',
+        icon: '',
+        label: 'featured',
+      },
+      {
+        url: '/clothing',
+        icon: '',
+        label: 'clothing',
+      },
+      {
+        url: '/accessories',
+        icon: '',
+        label: 'accessories',
+      },
+    ],
+  },
+  {
+    page: 'sell',
+    sublinks: [
+      {
+        url: '/sell',
+        icon: '',
+        label: 'sell everywhere',
+      },
+      {
+        url: '/online-store',
+        icon: '',
+        label: 'online store',
+      },
+      {
+        url: '/retail-and-point-of-sale',
+        icon: '',
+        label: 'retail and point of sale',
+      },
+      {
+        url: '/international-ecommerce',
+        icon: '',
+        label: 'international ecommerce',
+      },
+    ],
+  },
+  {
+    page: 'manage',
+    sublinks: [
+      {
+        url: '/manage',
+        icon: '',
+        label: 'manage complexity',
+      },
+      {
+        url: '/automation',
+        icon: '',
+        label: 'automation',
+      },
+      {
+        url: '/shipping',
+        icon: '',
+        label: 'shipping',
+      },
+      {
+        url: '/payments',
+        icon: '',
+        label: 'payments',
+      },
+      {
+        url: '/notebooks',
+        icon: '',
+        label: 'notebooks',
+      },
+    ],
+  },
+  {
+    page: 'integrate',
+    sublinks: [
+      {
+        url: '/integrate',
+        icon: '',
+        label: 'integrate systems',
+      },
+      {
+        url: '/ecommerce-automation',
+        icon: '',
+        label: 'automation',
+      },
+      {
+        url: '/headless-commerce',
+        icon: '',
+        label: 'headless commerce',
+      },
+      {
+        url: '/partners',
+        icon: '',
+        label: 'partners',
+      },
+    ],
+  },
+  {
+    page: 'resources',
+    sublinks: [
+      {
+        url: '/support-center',
+        icon: '',
+        label: 'support center',
+      },
+      {
+        url: '/blog',
+        icon: '',
+        label: 'blog',
+      },
+      {
+        url: '/support-plans',
+        icon: '',
+        label: 'support plans',
+      },
+      {
+        url: '/stories',
+        icon: '',
+        label: 'customer stories',
+      },
+    ],
+  },
+];
