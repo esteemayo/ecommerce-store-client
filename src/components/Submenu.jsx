73b9962c1ffc1b6da763b303c0b9c87a3a3b74c9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-06 01:20:45 +0100

    feat: add css property

--- src/components/Submenu.jsx
+++ src/components/Submenu.jsx
@@ -64,6 +64,7 @@ const Container = styled.aside`
   transform: translateX(-50%);
   display: ${({ type }) => type === 'show' ? 'block' : 'none'};
   box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 2rem 5rem rgba(0, 0, 0, 0.09);
   z-index: 3000;
 
   &::before {
