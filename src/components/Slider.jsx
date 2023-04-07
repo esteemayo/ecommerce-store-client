d15dbdedc683654be9f825136297447c14a8495a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-07 21:19:56 +0100

    fix: format code

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -15,7 +15,12 @@ const Slider = () => {
           />
         </ImageWrapper>
         <ImageWrapper>
-          <Image width={1024} height={600} src="https://images.pexels.com/photos/3784391/pexels-photo-3784391.jpeg?auto=compress&cs=tinysrgb&w=600" alt='' />
+          <Image
+            width={1024}
+            height={600}
+            src="https://images.pexels.com/photos/3784391/pexels-photo-3784391.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=''
+          />
         </ImageWrapper>
         <ImageWrapper>
           <Image width={1024} height={600} src="https://images.pexels.com/photos/4968390/pexels-photo-4968390.jpeg?auto=compress&cs=tinysrgb&w=600" alt='' />
