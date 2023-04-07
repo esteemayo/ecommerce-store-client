9ebd6177e7510c02e898f06fbed46efa5c554824
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-07 21:18:53 +0100

    fix: modify

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -14,7 +14,6 @@ const Slider = () => {
         </ImageWrapper>
         <ImageWrapper>
           <Image width={1024} height={600} src="https://images.pexels.com/photos/4968390/pexels-photo-4968390.jpeg?auto=compress&cs=tinysrgb&w=600" alt='' />
-          <p className="legend">Legend 3</p>
         </ImageWrapper>
       </Carousel>
     </Container>
