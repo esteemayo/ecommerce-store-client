44605336550c68743d239ced720c34f9b8c33444
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-09 00:20:50 +0100

    fix: format code

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -12,7 +12,12 @@ const Category = () => {
 
   return (
     <Container>
-      <Image src='https://images.pexels.com/photos/1598507/pexels-photo-1598507.jpeg?auto=compress&cs=tinysrgb&w=600' width={100} height={100} alt='' />
+      <Image
+        src='https://images.pexels.com/photos/1598507/pexels-photo-1598507.jpeg?auto=compress&cs=tinysrgb&w=600'
+        width={100}
+        height={100}
+        alt=''
+      />
     </Container>
   );
 }
