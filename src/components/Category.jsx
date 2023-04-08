0b5a5578858ad6ff62ead615c1a43c24251baac8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-09 00:30:20 +0100

    fix: modify

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -79,7 +79,7 @@ const Category = () => {
 const Container = styled.article`
   display: flex;
   align-items: center;
-  `;
+`;
 
 const Wrapper = styled.div`
   box-shadow: -1px -1px 35px 3px rgba(0 ,0, 0, 0.17);
