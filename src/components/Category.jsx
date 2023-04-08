2d411dfc596791895f0237afeaedb0b060a9bb98
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-09 00:29:08 +0100

    feat: add css properties

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -77,6 +77,8 @@ const Category = () => {
 }
 
 const Container = styled.article`
+  display: flex;
+  align-items: center;
   box-shadow: -1px -1px 35px 3px rgba(0 ,0, 0, 0.17);
   -webkit-box-shadow: -1px -1px 35px 3px rgba(0, 0, 0, 0.17);
   -moz-box-shadow: -1px -1px 35px 3px rgba(0, 0, 0, 0.17);
