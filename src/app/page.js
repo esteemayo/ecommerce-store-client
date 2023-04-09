360085a376636def4720d7f9477b05dcee365238
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-09 15:27:59 +0100

    feat: add FeaturedProducts component

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@
 import styled from 'styled-components';
 import Slider from '@/components/Slider';
 import Categories from '@/components/Categories';
+import FeaturedProducts from '@/components/FeaturedProducts';
 
 const Home = () => {
   return (
     <Container>
       <Slider />
       <Categories />
+      <FeaturedProducts />
     </Container>
   );
 }
