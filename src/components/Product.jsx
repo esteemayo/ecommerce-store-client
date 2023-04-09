a633fffc45b12527fd3e7758642c8049f1b46d24
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-09 17:22:57 +0100

    feat: add css properties

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -112,6 +112,12 @@ const Wrapper = styled.div`
   box-shadow: 1px 3px 17px 1px rgba(156,156,156,0.8);
   -webkit-box-shadow: 1px 3px 17px 1px rgba(156,156,156,0.8);
   -moz-box-shadow: 1px 3px 17px 1px rgba(156,156,156,0.8);
+
+  & > img {
+    width: 100%;
+    display: block;
+    object-fit: cover;
+  }
 `;
 
 const InfoContainer = styled.div`
