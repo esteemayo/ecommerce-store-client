19bb8a8db269940e8a05e51151c595ac009c91d3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-09 15:34:02 +0100

    feat: add a div erapper tag

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -3,12 +3,18 @@ import styled from 'styled-components';
 const FeaturedProducts = () => {
   return (
     <Container>
-      FeaturedProducts
+      <Wrapper>
+        FeaturedProducts
+      </Wrapper>
     </Container>
   );
 }
 
 const Container = styled.section`
+  background-color: #f9f9f9;
+`;
+
+const Wrapper = styled.div`
   width: 100%;
   max-width: 102.4rem;
   margin: 0 auto;
