881a2efcf61fcd8596f3d41583cc408f297f192d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-09 14:20:27 +0100

    fix: iterate over images array

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -12,19 +12,23 @@ const Category = () => {
 
   return (
     <Container>
-      <Wrapper>
-        <Image
-          src='https://images.pexels.com/photos/1598507/pexels-photo-1598507.jpeg?auto=compress&cs=tinysrgb&w=600'
-          width={100}
-          height={100}
-          alt=''
-        />
-        <HeadingWrapper>
-          <MainHeading>Jeans</MainHeading>
-          <SubHeading>100</SubHeading>
-        </HeadingWrapper>
-        <Button>Click</Button>
-      </Wrapper>
+      {images.map((item, index) => {
+        return (
+          <Wrapper key={index}>
+            <Image
+              src={item}
+              width={100}
+              height={100}
+              alt=''
+            />
+            <HeadingWrapper>
+              <MainHeading>Jeans</MainHeading>
+              <SubHeading>100</SubHeading>
+            </HeadingWrapper>
+            <Button>Click</Button>
+          </Wrapper>
+        );
+      })}
     </Container>
   );
 }
