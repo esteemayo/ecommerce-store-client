fdda7694861ea869dcee8cd182d3b2941011391e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-09 17:30:46 +0100

    feat: add css properties

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -103,10 +103,11 @@ const Container = styled.article`
   display: flex;
   flex-wrap: wrap;
   align-items: center;
-  gap: 6rem;
+  gap: 4rem;
 `;
 
 const Wrapper = styled.div`
+  width: 30%;
   background-color: transparent;
   border-radius: 0.5rem;
   box-shadow: 1px 3px 17px 1px rgba(156,156,156,0.8);
@@ -123,7 +124,7 @@ const Wrapper = styled.div`
 `;
 
 const InfoContainer = styled.div`
-  
+  padding: 2rem;
 `;
 
 const ProductName = styled.h3`
