27856eecc5364e4934f8133812aa9373505e0a15
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-09 15:24:11 +0100

    fix: modify

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Category = () => {
