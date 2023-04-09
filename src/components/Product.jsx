be3da07a31b8f7a8cb1d57add3dbb7e337efe351
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-09 15:59:51 +0100

    feat: add next/image

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,11 @@
+import Image from 'next/image';
 import styled from 'styled-components';
 
 const Product = () => {
   return (
     <Container>
       <Wrapper>
-        Product
+        <Image src='https://images.pexels.com/photos/63690/pexels-photo-63690.jpeg?auto=compress&cs=tinysrgb&w=600' width={200} height={200} alt='' />
       </Wrapper>
     </Container>
   );
