1358924e17ae3b367f1d7ea421a6d37d187afe73
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-10 14:09:23 +0100

    feat: add css properties

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -215,7 +215,7 @@ const FavWrapper = styled.div`
 const WishlistButton = styled.button`
   border: none;
   display: inline-block;
-  text-transform: capitalize;
+  font-weight: 600;
   font-size: 1.4rem;
   background-color: transparent;
   color: #858585;
