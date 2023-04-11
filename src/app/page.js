cfa5f86e1f5663e8e1f747d59c408ea9f87cf032
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-11 17:44:06 +0100

    feat: add TopReviews component

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Slider from '@/components/Slider';
 import Categories from '@/components/Categories';
 import FeaturedProducts from '@/components/FeaturedProducts';
+import TopReviews from '@/components/TopReviews';
 
 const Home = () => {
   return (
