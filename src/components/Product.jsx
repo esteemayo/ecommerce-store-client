bc0f28b1d597bd9170a9df88877dfa93ceff7df8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-11 17:46:39 +0100

    fix: modify

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Image from 'next/image';
 import { faShoppingCart, faStar } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import StarRating from './StarRating';
 
