1e1ca0bb3592c5d2fea91176b046b7af3c7831af
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-11 15:15:23 +0100

    feat: add css property

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -200,6 +200,12 @@ const Button = styled.button`
   text-transform: uppercase;
   background-color: #2d75c7;
   color: #fff;
+  background-image: linear-gradient(
+    120deg,
+    transparent 0%,
+    transparent 50%,
+    var(--color-gray-dark-2) 50%
+  );
   outline-color: #04203f;
   border-radius: 0.5rem;
   cursor: pointer;
