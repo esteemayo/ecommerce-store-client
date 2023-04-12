ec3e06639e1742683ab5e6af3f4878ea6bab0f36
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-12 15:27:08 +0100

    fix: add font awesome icon

--- src/components/TopReviews.jsx
+++ src/components/TopReviews.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const TopReviews = () => {
   return (
