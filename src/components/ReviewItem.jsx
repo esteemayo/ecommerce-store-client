d40f21783030ff53d72860b9b612b806688dcfa0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-12 19:25:13 +0100

    feat: add css property

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -79,6 +79,7 @@ const Button = styled.button`
   border: none;
   display: inline-block;
   text-align: left;
+  font-weight: 600;
   font-size: 1.7rem;
   background-color: transparent;
   color: #000;
