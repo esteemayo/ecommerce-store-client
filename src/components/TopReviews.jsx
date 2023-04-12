5666110444b8697f1b35e09fc5ede8f9715dfd3d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-12 20:19:27 +0100

    fix: modify

--- src/components/TopReviews.jsx
+++ src/components/TopReviews.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
