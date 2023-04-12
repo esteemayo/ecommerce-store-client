ec11ce67ed8db28d6e06b95878f6fca4f4a22928
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-12 21:12:33 +0100

    feat: add css property

--- src/components/TopReviews.jsx
+++ src/components/TopReviews.jsx
@@ -44,7 +44,7 @@ const Heading = styled.h1`
   font-weight: 600;
   font-size: 2.7rem;
   color: #1658a3;
-  margin-bottom: 2rem;
+  margin-bottom: 5rem;
   display: flex;
   flex-direction: column;
 
