df0883db3ad096082b4edeb481ffb6a90b97f7e0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-12 20:26:03 +0100

    fix: modify

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -20,7 +20,7 @@ const ReviewItem = () => {
           <Button>Shop now</Button>
         </Left>
         <Right>
-          <Image src='./reviewer-1.webp' width={100} height={100} alt='' />
+          <Image src='/reviewer-1.webp' width={100} height={100} alt='' />
         </Right>
       </Wrapper>
     </Container>
