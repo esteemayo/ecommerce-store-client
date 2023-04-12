ae6e813e7ea288275191eaff9f39f49c08fe9341
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-12 21:39:23 +0100

    fix: increase margin bottom

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -30,7 +30,7 @@ const Heading = styled.h1`
   font-weight: 600;
   font-size: 2.7rem;
   color: #1658a3;
-  margin-bottom: 3rem;
+  margin-bottom: 7rem;
   display: flex;
   flex-direction: column;
 
