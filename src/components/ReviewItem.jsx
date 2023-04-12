cb123930306f9e88bc60cde8901c9929f4198092
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-12 21:43:17 +0100

    feat: add css property

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -106,6 +106,7 @@ const Button = styled.button`
   text-align: left;
   font-weight: 600;
   font-size: 1.8rem;
+  width: 10rem;
   background-color: transparent;
   color: #000;
   cursor: pointer;
