e88c893f7d269ca146f6e871e8723bebaf4e31b1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-14 00:43:00 +0100

    feat: add css properties

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -15,9 +15,14 @@ const Container = styled.div`
   width: 4rem;
   height: 4rem;
   background-color: var(--clr-white);
+  background-color: var(--clr-black);
   color: #777;
   border-radius: 50%;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.04);
+  display: flex;
+  align-items: center;
+  jsutify-content: center;
+  text-align: center;
 `;
 
 const Wrapper = styled.div`
