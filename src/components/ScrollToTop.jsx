39b61d9fc22b2b380f7d78dab8a93ac3d88df3f0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-14 00:49:49 +0100

    fix: modify

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -30,7 +30,16 @@ const Container = styled.div`
 `;
 
 const Wrapper = styled.div`
-
+  width: 4rem;
+  height: 4rem;
+  background-color: var(--clr-white);
+  background-color: var(--clr-black);
+  color: #777;
+  border-radius: 50%;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.04);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default ScrollToTop;
