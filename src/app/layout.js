744b0146b3f182cf5105c116510ddaa5278b25d1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-14 00:23:48 +0100

    fix: modify

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,6 @@ import ScrollToTop from '@/components/ScrollToTop';
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './globals.css';
-import ScrollToTop from '@/components/ScrollToTop';
 
 export default function RootLayout({ children }) {
   return (
