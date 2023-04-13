746a74604eec267659bacd78eb0bb8da6edcb29b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-13 14:41:33 +0100

    feat: add Footer component

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
             <Submenu />
             <Sidebar />
             {children}
+            <Footer />
           </div>
         </Provider>
       </body>
