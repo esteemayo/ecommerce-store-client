5bda3388f9273cde0a725b63461ea7120626441e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-13 14:51:50 +0100

    feat: create fonts.js file

---
+++ src/styles/fonts.js
@@ -0,0 +1,7 @@
+import { Inter, Cormorant_Garamond } from '@next/font/google';
+
+export const inter = Inter();
+export const cormorantGaramond = Cormorant_Garamond({
+  subsets: ['cyrillic'],
+  weight: '300'
+});
