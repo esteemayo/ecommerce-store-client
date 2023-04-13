0ab8e82edd0b0ae2a521de3874f70e132faa9e69
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-13 15:01:31 +0100

    fix: delete folder

--- src/styles/fonts.js
+++
@@ -1,7 +0,0 @@
-import { Inter, Cormorant_Garamond } from '@next/font/google';
-
-export const inter = Inter();
-export const cormorantGaramond = Cormorant_Garamond({
-  subsets: ['cyrillic'],
-  weight: '300'
-});
