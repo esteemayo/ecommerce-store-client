6863504b87d9f0ae0cdabc37234fd000c5fff1b9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-13 14:29:37 +0100

    fix: modify

--- src/components/TopReviews.jsx
+++ src/components/TopReviews.jsx
@@ -12,7 +12,7 @@ const TopReviews = () => {
 
   const handleClick = (direction) => {
     setIsMoved(true);
-    const distance = reviewRef.current.getBoundingClientRect().x - 50;
+    const distance = reviewRef.current.getBoundingClientRect().x - 54;
 
     if (direction === 'left') {
       setCurrentSlide(currentSlide - 1);
