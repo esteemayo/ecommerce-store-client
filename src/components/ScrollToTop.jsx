7fbe2847f1ca9614fe8ce9d2491c021ad8ee4b86
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-14 01:07:56 +0100

    fix: handle scroll functionality

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -24,7 +24,7 @@ const ScrollToTop = () => {
   return (
     <Container>
       {isVisible && (
-        <Wrapper>
+        <Wrapper onClick={scrollHandler}>
           <KeyboardDoubleArrowUpIcon />
         </Wrapper>
       )}
