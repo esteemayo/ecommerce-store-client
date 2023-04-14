dbf934328660016015cf0b4e91b14271f697e361
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-14 02:06:17 +0100

    fix: modify

--- src/data.js
+++ src/data.js
@@ -20,11 +20,6 @@ import {
   faStore,
   faTruckFast,
 } from '@fortawesome/free-solid-svg-icons';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import PinterestIcon from '@mui/icons-material/Pinterest';
 
 export const sublinks = [
   {
