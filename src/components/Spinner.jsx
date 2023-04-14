c53b6e4aaca3420f0786c9c1ffbdc8ef6b5332b2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-14 03:01:58 +0100

    feat: create a rotate animation

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -17,6 +17,13 @@ const Container = styled.div`
   height: ${({ size }) => size === 'lg' && '10rem'};
   border: 3px solid #f5f5f5;
   border-top-color: #979797;
+  border-radius: 50%;
+
+  @keyframes rotate {
+    to {
+      transform: rotate(360deg);
+    }
+  }
 `;
 
 export default Spinner;
