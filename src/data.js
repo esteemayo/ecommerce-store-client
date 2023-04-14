f0a0f9463fbd4e416b2af2b554f51de588f192d5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-14 01:55:21 +0100

    fix: add LinkedInIcon

--- src/data.js
+++ src/data.js
@@ -23,6 +23,7 @@ import {
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export const sublinks = [
   {
