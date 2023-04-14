f1f0e6d6b0c152620ff06e2f026c42b0ae8973a2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-14 02:06:25 +0100

    fix: modify

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PinterestIcon from '@mui/icons-material/Pinterest';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBagShopping,
