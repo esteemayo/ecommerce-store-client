a5b8c14b374b540b96d834edab99d1bfd850837c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-15 12:04:30 +0100

    fix: modify

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -11,7 +11,7 @@ const Loader = () => {
 const Container = styled.div`
   width: 100vw;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.1);
+  background-color: rgba(0, 0, 0, 0.5);
   position: fixed;
   top: 0;
   left: 0;
