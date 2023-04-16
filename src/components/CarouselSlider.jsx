ff1f6de88b63c7aa52166de4efded13dcc47a6a6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-16 10:36:57 +0100

    fix: modify

--- src/components/CarouselSlider.jsx
+++ src/components/CarouselSlider.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import styled from 'styled-components';
+import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
