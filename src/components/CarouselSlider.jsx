26f5608f3dd5eec9e9ee8dea7f9ee0009bfd8fca
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-16 09:26:36 +0100

    feat: add faChevronLeft

--- src/components/CarouselSlider.jsx
+++ src/components/CarouselSlider.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 const CarouselSlider = () => {
   return (
