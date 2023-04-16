6d8a8fb611a4c75ac2bc2b7ecbd9fd72ddbc072b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-16 10:17:43 +0100

    feat: import useState react hook

--- src/components/CarouselSlider.jsx
+++ src/components/CarouselSlider.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const CarouselSlider = () => {
   const data = [
