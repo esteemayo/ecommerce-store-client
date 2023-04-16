32b8b2983f120da5b916ddb8758fbde468faeaf2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-16 14:23:01 +0100

    fix: refactor code

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -29,7 +29,7 @@ const Heading = styled.h1`
   font-family: inherit;
   font-weight: 600;
   font-size: 2.7rem;
-  color: #1658a3;
+  color: var(--clr-secondary-blue);
   margin-bottom: 7rem;
   display: flex;
   flex-direction: column;
