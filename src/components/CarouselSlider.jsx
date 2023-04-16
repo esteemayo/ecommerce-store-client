a6101fcaffec63b15e85071f36b9175c6abfeda6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-16 13:52:35 +0100

    feat: import useDispatch react redux hook

--- src/components/CarouselSlider.jsx
+++ src/components/CarouselSlider.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useState } from 'react';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { useDispatch } from 'react-redux';
 
 const CarouselSlider = () => {
   const [slideNumber, setSlideNumber] = useState(0);
