b3d0aef9a8206332fffdf9230b4e85b7e9f5bd4b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-16 09:13:37 +0100

    feat: add css properties

--- src/components/CarouselSlider.jsx
+++ src/components/CarouselSlider.jsx
@@ -19,6 +19,9 @@ const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   text-align: center;
+  position: relative;
+  display: flex;
+  overflow: hidden;
 `;
 
 export default CarouselSlider;
