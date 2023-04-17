45de291257baa145ee0ee8d7587ea9fd8f62f1db
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-17 13:39:32 +0100

    fix: iterate over footerLinks array

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -75,11 +75,16 @@ const Footer = () => {
         </FooterRight>
       </Wrapper>
       <FooterLinkContainer>
-        <LinkItem>
-          <Link href='#' passHref>
-            Terms of purchase
-          </Link>
-        </LinkItem>
+        {footerLinks.map((link) => {
+          const { id, url, text } = link;
+          return (
+            <LinkItem key={id}>
+              <Link href={url} passHref>
+                {text}
+              </Link>
+            </LinkItem>
+          );
+        })}
       </FooterLinkContainer>
       <FooterText>
         &copy; {year} by Emmanuel Adebayo&trade;. All rights reserved. We are a young company always looking for new and creative ideas to help you with our products in your everyday work.
