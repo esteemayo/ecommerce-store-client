560892f0ac0a67ae92d8624370d32216ce643925
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-17 21:03:58 +0100

    feat: add css properties

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -12,6 +12,10 @@ const Register = () => {
 const Container = styled.main`
   width: 100vw;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default Register;
