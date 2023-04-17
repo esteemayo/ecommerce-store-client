f0f0954acbeaa4774ea508b7bc498a1451169e4f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-17 21:05:34 +0100

    feat: add a div tag

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -4,7 +4,9 @@ import styled from 'styled-components';
 const Register = () => {
   return (
     <Container>
-      Register
+      <Wrapper>
+        Register
+      </Wrapper>
     </Container>
   );
 }
