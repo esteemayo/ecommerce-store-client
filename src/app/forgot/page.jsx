186b8c9cf56e41ea6cfaac21068514ef652c912b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-18 14:03:44 +0100

    feat: add css properties

--- src/app/forgot/page.jsx
+++ src/app/forgot/page.jsx
@@ -23,6 +23,9 @@ const Wrapper = styled.div`
   width: 40rem;
   padding: 2rem;
   border-radius: 1rem;
+  box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
+  -webkit-box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
+  -moz-box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
 `;
 
 export default Forgot;
