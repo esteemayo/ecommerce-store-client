6a304ba86672e22e2c2681469e97c455b213383d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-18 14:01:33 +0100

    feat: add div tag

--- src/app/forgot/page.jsx
+++ src/app/forgot/page.jsx
@@ -4,7 +4,9 @@ import styled from 'styled-components';
 const Forgot = () => {
   return (
     <Container>
-      Forgot
+      <Wrapper>
+        Forgot
+      </Wrapper>
     </Container>
   );
 }
