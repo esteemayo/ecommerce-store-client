a08737c8c4bcdf20f9ee6e3eae3c7279863b5f3f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-18 14:02:33 +0100

    feat: add css properties

--- src/app/forgot/page.jsx
+++ src/app/forgot/page.jsx
@@ -14,6 +14,9 @@ const Forgot = () => {
 const Container = styled.main`
   width: 100vw;
   height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Wrapper = styled.div`
