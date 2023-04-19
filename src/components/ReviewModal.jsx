1ab26f664f7d1f1b31945f167f16a58816a0a9b3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-19 09:40:03 +0100

    feat: add css stylings

--- src/components/ReviewModal.jsx
+++ src/components/ReviewModal.jsx
@@ -11,6 +11,10 @@ const ReviewModal = () => {
 const Container = styled.div`
   width: 100vw;
   height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
 `;
 
 export default ReviewModal;
