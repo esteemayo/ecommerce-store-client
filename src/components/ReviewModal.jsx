89388339fe9f50639e91c50612f61346781154c2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-19 14:14:17 +0100

    feat: add useState react hook

--- src/components/ReviewModal.jsx
+++ src/components/ReviewModal.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import Rating from '@mui/material/Rating';
+import { useState } from 'react';
 
 const ReviewModal = () => {
   return (
