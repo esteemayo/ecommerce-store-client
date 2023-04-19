ed28a4f14e23bac30e2a1df26e41b0fce76ab355
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-19 14:12:17 +0100

    feat: add Rating from mui

--- src/components/ReviewModal.jsx
+++ src/components/ReviewModal.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import Rating from '@mui/material/Rating';
 
 const ReviewModal = () => {
   return (
