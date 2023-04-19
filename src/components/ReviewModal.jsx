d2ab51d5c8226f8e048ece0932605c11404506be
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-19 09:53:32 +0100

    feat: add css properties

--- src/components/ReviewModal.jsx
+++ src/components/ReviewModal.jsx
@@ -27,6 +27,9 @@ const Wrapper = styled.div`
   width: 50rem;
   padding: 2rem;
   background-color: var(--clr-white);
+  box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
+  -webkit-box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
+  -moz-box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
 `;
 
 export default ReviewModal;
