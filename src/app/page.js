4b484f90b026d29612fcbd352d27e7a540306f30
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-19 13:37:25 +0100

    fix: modify

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Categories from '@/components/Categories';
 import FeaturedProducts from '@/components/FeaturedProducts';
 import TopReviews from '@/components/TopReviews';
 import CarouselSlider from '@/components/CarouselSlider';
+import ReviewModal from '@/components/ReviewModal';
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Categories />
       <FeaturedProducts />
       <TopReviews />
+      <ReviewModal />
     </Container>
   );
 }
