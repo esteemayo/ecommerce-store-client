54cf2763124a2544838b31ccbde7374f22b3f92c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-19 14:10:32 +0100

    feat: add StarRating component

--- src/components/ReviewModal.jsx
+++ src/components/ReviewModal.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import StarRating from './StarRating';
 
 const ReviewModal = () => {
   return (
