273a7797498d87f5aa0d494a2110bf1391e5d2cb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-19 14:11:45 +0100

    fix: modify

--- src/components/ReviewModal.jsx
+++ src/components/ReviewModal.jsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import StarRating from './StarRating';
 
 const ReviewModal = () => {
   return (
@@ -13,7 +12,6 @@ const ReviewModal = () => {
           </CloseButton>
         </ButtonContainer>
         <Header>Overall rating</Header>
-        <StarRating />
       </Wrapper>
     </Container>
   );
