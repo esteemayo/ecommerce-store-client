bef9f0deb7b7ef4836df7d6d733f376a73f09dd0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-20 21:44:52 +0100

    feat: add a div tag

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -5,7 +5,9 @@ const ReviewCard = () => {
     <Container>
       <Wrapper>
         <ReviewContainer>
+          <Reviewer>
 
+          </Reviewer>
         </ReviewContainer>
       </Wrapper>
     </Container>
