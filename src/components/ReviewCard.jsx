6402ce6522f9b3cfe14393ce73e4d0ba45ed4fc2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-20 22:10:57 +0100

    fix: modify

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpFromBracket } from '@fortawesome/free-solid-svg-icons';
+
 import StarRating from './StarRating';
 
 const ReviewCard = () => {
