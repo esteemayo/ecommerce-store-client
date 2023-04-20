bee9262fb9fbeec3414de04243d3b786d693eb70
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-20 20:06:53 +0100

    fix: modify

--- src/components/ReviewModal.jsx
+++ src/components/ReviewModal.jsx
@@ -5,7 +5,7 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
 import { useState } from 'react';
 
-const ReviewModal = ({ isModalOpen, onClose }) => {
+const ReviewModal = ({ isModalOpen, setIsModalOpen }) => {
   const [rating, setRating] = useState(null);
   const [review, setReview] = useState(null);
 
