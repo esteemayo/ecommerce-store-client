41a08cb51ada2e9d68ff1801747d6a74fe025f0a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-20 21:58:29 +0100

    feat: add a time tag

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -14,7 +14,7 @@ const ReviewCard = () => {
         </ReviewContainer>
         <Review>Gorgeous design! Even more responsive than the previous version. A pleasure to use!</Review>
         <DateContainer>
-
+          <ReviewDate dateTime='2023-02-13 20:00'>Feb 13, 2023</ReviewDate>
         </DateContainer>
       </Wrapper>
     </Container>
