7d187058b0dffca6dce5b7062fde77cf086efb43
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-22 23:17:55 +0100

    fix: modify

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,6 +1,4 @@
 'use client';
-import Recommendation from '@/components/Recommendation';
-import StarRating from '@/components/StarRating';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
