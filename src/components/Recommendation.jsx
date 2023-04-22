292c7573f7548553ec706c3c8d018720c78ad800
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-22 23:32:48 +0100

    feat: add h1 tag

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -4,7 +4,7 @@ const Recommendation = () => {
   return (
     <Container>
       <Wrapper>
-        Recommendation
+        <Heading>You might also like</Heading>
       </Wrapper>
     </Container>
   );
