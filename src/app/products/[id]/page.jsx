61b1ffa6e560b4d91211c3715be7cae7f10c1e3d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-22 23:17:12 +0100

    feat: add Recommendation component

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,4 +1,5 @@
 'use client';
+import Recommendation from '@/components/Recommendation';
 import StarRating from '@/components/StarRating';
 import Image from 'next/image';
 import Link from 'next/link';
