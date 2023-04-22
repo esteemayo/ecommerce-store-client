9a5a97613d2d1192b8f367efa8fd5a4f189ae7f4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-22 02:38:15 +0100

    feat: add css properties

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -136,7 +136,10 @@ const ProductWrapper = styled.div`
 `;
 
 const ProductName = styled.h1`
-  
+  display: inline-block;
+  font-weight: 500;
+  font-size: 2.4rem;
+  line-height: 1.3;
 `;
 
 export default SingleProduct;
