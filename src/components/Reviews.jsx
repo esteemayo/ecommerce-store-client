59cb6dc98b29a923bbf7b1bafd3837662d7f103f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-23 03:23:29 +0100

    fix: modify

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import StarRating from './StarRating';
-import { useState } from 'react';
 import ReviewModal from './ReviewModal';
 
 const Reviews = () => {
