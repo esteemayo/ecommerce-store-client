6572410d32c95afb50363e5a98f1f3be14426f63
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-24 00:43:08 +0100

    feat: add css properties

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -222,7 +222,11 @@ const PriceDiscount = styled.p`
 `;
 
 const SalePrice = styled.p`
-
+  font-weight: 500;
+  font-size: 1.6rem;
+  color: #fa4b21;
+  line-height: 1.3;
+  cursor: default;
 `;
 
 const Message = styled.p`
