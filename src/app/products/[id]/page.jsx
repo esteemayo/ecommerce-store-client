18e9aa75f13bf1239c34715f296e2e1363a910d0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-24 18:06:12 +0100

    fix: modify

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -455,7 +455,7 @@ const DescriptionHeader = styled.h4`
   display: inline-block;
   font-weight: 400;
   font-size: 1.8rem;
-  margin: 3rem 0;
+  margin-bottom: 3rem;
 `
 
 const DescriptionText = styled.p`
