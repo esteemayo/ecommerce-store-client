89fb61d09e74fabaf7382260c81165228412ca00
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-25 01:10:00 +0100

    feat: add css properties

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -498,6 +498,11 @@ const ImageOverlay = styled.section`
 const ImageContainer = styled.div`
   width: 100%;
   height: 80%;
+
+  img {
+    width: 90%;
+    height: 100%;
+  }
 `;
 
 export default SingleProduct;
