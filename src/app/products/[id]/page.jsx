5512ee292fb8ce227156b2cfb6cb1a9edb076006
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-25 02:36:23 +0100

    fix: format code

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -161,7 +161,11 @@ const SingleProduct = () => {
         <Line />
         <Recommendation />
         <Reviews reviews={reviews} />
-        <ImageOverlay className='imageOverlay' type={isOpen ? 'show' : ''} onClick={closeModalHandler}>
+        <ImageOverlay
+          className='imageOverlay'
+          type={isOpen ? 'show' : ''}
+          onClick={closeModalHandler}
+        >
           <CloseButton type='button' onClick={() => setIsOpen(false)}>
             <CloseIcon />
           </CloseButton>
