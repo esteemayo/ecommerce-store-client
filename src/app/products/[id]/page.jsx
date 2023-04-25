ba7f6d73df034d9466a11c0f5cea2a525c087052
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-25 01:22:56 +0100

    feat: add FontAwesomeIcon

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Link from 'next/link';
 import { useState } from 'react';
 import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Recommendation from '@/components/Recommendation';
 import Reviews from '@/components/Reviews';
