1516bd957a53af8ca9505275a037243d6d676600
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-25 01:26:11 +0100

    feat: add a button tag

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -148,6 +148,7 @@ const SingleProduct = () => {
         <Recommendation />
         <Reviews reviews={reviews} />
         <ImageOverlay>
+          <ArrowButton></ArrowButton>
           <ImageContainer>
             <Image
               src={images[slideIndex]}
