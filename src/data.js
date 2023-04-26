f36ea6c36dc13afea3907d977b46bda988f99c95
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-26 19:02:43 +0100

    fix: modify

--- src/data.js
+++ src/data.js
@@ -394,7 +394,7 @@ export const topReviews = [
     rating: 5,
     review: 'Exactly what I wanted - slightly elevated but still warm. Fits like a dream with nice big pockets with zippers. Great addition!',
     user: {
-      name: 'Sarah P.',
+      name: 'Bill James',
       photo: '/img/user-3.jpg'
     },
   },
