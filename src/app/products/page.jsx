86fb92fdc59fea1cf8af729284af3e8941f08fd3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-27 00:55:06 +0100

    feat: add a div tag

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -4,7 +4,9 @@ import styled from 'styled-components';
 const Products = () => {
   return (
     <Container>
-      Products
+      <FilterContainer>
+        
+      </FilterContainer>
     </Container>
   );
 }
