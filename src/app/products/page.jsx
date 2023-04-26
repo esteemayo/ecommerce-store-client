ca002ef563fee401a142e173277b67bfa3b71c68
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-27 00:35:43 +0100

    feat: create all products page

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const page = () => {
+const Products = () => {
   return (
-    <div>page</div>
+    <div>Products</div>
   );
 }
 
-export default page;
+export default Products;
