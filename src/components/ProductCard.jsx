1194f60d703b1b383bf7a4e93229f0bfc11691a0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-27 04:10:19 +0100

    fix: modify

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -61,45 +61,6 @@ const ImageContainer = styled.div`
   }
 `;
 
-const ReviewContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  margin-bottom: 0.5rem;
-
-  svg {
-    font-size: 2.3rem;
-    fill: #89a8a0;
-  }
-`;
-
-const Reviews = styled.div`
-  span {
-    color: #89a8a0;
-  }
-`;
-
-const PriceContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  line-height: 1.2;
-  margin: 1rem 0;
-`;
-
-const PriceDiscount = styled.p`
-  display: inline-block;
-  text-decoration: line-through;
-  font-size: 1.4rem;
-`;
-
-const ProductPrice = styled.p`
-  display: inline-block;
-  font-weight: 600;
-  font-size: 3rem;
-  color: #999;
-`;
-
 const Right = styled.div`
 
 `;
