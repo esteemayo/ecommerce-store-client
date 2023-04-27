7c8a821a1ce2d8be26c90fc6edda9ca4e4bb2d65
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-27 03:09:56 +0100

    feat: add css properties

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -11,6 +11,9 @@ const ProductCard = () => {
 const Container = styled.div`
   width: 30%;
   background-color: var(--color-white);
+  box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
+  -webkit-box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
+  -moz-box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
 `;
 
 export default ProductCard;
