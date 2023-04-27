6828b3a9bf972299b641943fb6e6890487a190fa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-27 14:38:02 +0100

    feat: add faScaleBalanced icon

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import StarRating from './StarRating';
+import { faScaleBalanced } from '@fortawesome/free-solid-svg-icons';
 
 const ProductCard = () => {
   return (
