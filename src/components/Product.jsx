7680596f81ac70e9c8337cad67231cb2c66bba1b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-28 00:37:53 +0100

    fix: modify

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -78,8 +78,8 @@ const Wrapper = styled.div`
   -webkit-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
   -moz-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
 
-  & > img {
-    width: 100%;
+  img {
+    width: 30rem;
     display: block;
     object-fit: cover;
     border-top-left-radius: 0.5rem;
