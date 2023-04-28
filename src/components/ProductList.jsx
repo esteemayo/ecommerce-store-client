75f2c530002a7460d9015f3f2ecdc9d4007a2c8d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-28 03:31:34 +0100

    feat: add css properties

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -9,7 +9,14 @@ const ProductList = () => {
 }
 
 const Container = styled.section`
-  
+  width: 100%;
+  max-width: 102.4rem;
+  margin: 0 auto;
+  padding: 8rem 0;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 5rem;
 `;
 
 export default ProductList;
