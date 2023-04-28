a3daddd655dce05f2ee92bea43e202d791dbb483
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-28 02:15:31 +0100

    feat: add css property

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -25,6 +25,7 @@ const Wrapper = styled.div`
   box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
   -webkit-box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
   -moz-box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
+  border-radius: 0.5rem;
 `;
 
 export default Pagination;
