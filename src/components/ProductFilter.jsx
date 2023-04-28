62c3372e11f7b539bf2d1cba4e738c1f50a4efd7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-28 17:43:33 +0100

    fix: destructure props

--- src/components/ProductFilter.jsx
+++ src/components/ProductFilter.jsx
@@ -5,7 +5,7 @@ const getUnique = (items, value) => {
   return [...new Set(tempItems)];
 };
 
-const ProductFilter = ({ products }) => {
+const ProductFilter = ({ products, category, size, color, minPrice, maxPrice }) => {
   let colors = getUnique(products, 'color');
   colors = colors.map((color, index) => {
     return (
