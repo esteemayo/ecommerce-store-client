deb24a1ba324074271b6a6a3b048e50295a6fbec
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-28 03:34:08 +0100

    feat: add ProductList component

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -6,6 +6,7 @@ import Product from '@/components/Product';
 import { useDispatch } from 'react-redux';
 import { closeSubmenu } from '@/features/submenu/submenuSlice';
 import Pagination from '@/components/Pagination';
+import ProductList from '@/components/ProductList';
 
 const Products = () => {
   const dispatch = useDispatch();
