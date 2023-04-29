468a6050f04c66aa98b9ebfdb9c39e765abad14d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-29 17:06:52 +0100

    fix: modify

--- src/components/WishlistCard.jsx
+++ src/components/WishlistCard.jsx
@@ -38,7 +38,7 @@ const Container = styled.article`
   -moz-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.25);
   border-radius: 0.5rem;
   display: flex;
-  align-items: flex-start;
+  align-items: center;
   justify-content: space-between;
 `;
 
