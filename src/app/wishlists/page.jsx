83026d9fa4aabfa2e2b3b894635a6c2ac0dfe6e7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-29 12:46:05 +0100

    feat: add a div tag

--- src/app/wishlists/page.jsx
+++ src/app/wishlists/page.jsx
@@ -5,10 +5,12 @@ const WishLists = () => {
   return (
     <Container>
       <Wrapper>
-        <HeadingContainer>
-          <Heading>My wishlist</Heading>
-          <Date dateTime='2023-04-29T00:42:49.511Z'>29 April, 2023</Date>
-        </HeadingContainer>
+        <Header>
+          <HeadingContainer>
+            <Heading>My wishlist</Heading>
+            <Date dateTime='2023-04-29T00:42:49.511Z'>29 April, 2023</Date>
+          </HeadingContainer>
+        </Header>
       </Wrapper>
     </Container>
   );
