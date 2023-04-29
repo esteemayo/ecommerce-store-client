b85ae769609d08353a002777272148dba5aa8a8f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-29 17:54:14 +0100

    feat: add next/link

--- src/components/WishlistCard.jsx
+++ src/components/WishlistCard.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import Image from 'next/image';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
 
 const WishlistCard = () => {
   return (
