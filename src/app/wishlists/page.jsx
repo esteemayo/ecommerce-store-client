822da6a1a1c141efb8dc08e42de81da6fd772147
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-29 12:49:16 +0100

    feat: add css properties

--- src/app/wishlists/page.jsx
+++ src/app/wishlists/page.jsx
@@ -30,7 +30,9 @@ const Wrapper = styled.div`
 `;
 
 const Header = styled.div`
-  
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const HeadingContainer = styled.div`
