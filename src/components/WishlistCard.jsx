c8f78cea35f4570e9c39cf347908117d752f2bd8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-29 17:44:30 +0100

    feat: add css properties

--- src/components/WishlistCard.jsx
+++ src/components/WishlistCard.jsx
@@ -100,6 +100,13 @@ const CartButton = styled.button`
   padding: 1rem 2rem;
   background-color: #6d857f;
   color: var(--clr-white);
+  background-image: linear-gradient(
+    120deg,
+    transparent 0%,
+    transparent 50%,
+    #89a8a0 50%
+  );
+  background-size: 220%;
 `;
 
 export default WishlistCard;
