9346804db1c68a7badff9972c50d273ff4838892
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-29 12:43:21 +0100

    feat: add a time tag

--- src/app/wishlists/page.jsx
+++ src/app/wishlists/page.jsx
@@ -7,6 +7,7 @@ const WishLists = () => {
       <Wrapper>
         <HeadingContainer>
           <Heading>My wishlist</Heading>
+          <Date dateTime='2023-04-29T00:42:49.511Z'>29 April, 2023</Date>
         </HeadingContainer>
       </Wrapper>
     </Container>
