1ea0c1c1470f22d2e4e1c44f37bf0703217201eb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-29 20:28:08 +0100

    fix: hide the submenu

--- src/app/wishlists/page.jsx
+++ src/app/wishlists/page.jsx
@@ -10,7 +10,7 @@ const WishLists = () => {
   const dispatch = useDispatch();
 
   return (
-    <Container>
+    <Container onMouseOver={() => dispatch(closeSubmenu())}>
       <Wrapper>
         <Header>
           <HeadingContainer>
