f4722aa87173524294268aa6ea36788953480fda
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-29 21:29:28 +0100

    feat: add a button tag

--- src/components/WishlistHeader.jsx
+++ src/components/WishlistHeader.jsx
@@ -7,6 +7,7 @@ const WishlistHeader = () => {
         <Heading>My wishlist</Heading>
         <Date dateTime='2023-04-29T00:42:49.511Z'>29 April, 2023</Date>
       </HeadingContainer>
+      <SendButton type='button'>Send wishlist</SendButton>
     </Container>
   );
 }
