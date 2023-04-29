098aec48ddb87fc5631f35cf3521e575281c9ebe
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-29 17:49:31 +0100

    fix: modify

--- src/components/WishlistCard.jsx
+++ src/components/WishlistCard.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import styled from 'styled-components';
+import Image from 'next/image';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
