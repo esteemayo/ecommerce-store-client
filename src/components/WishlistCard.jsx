b5cb053da6567a5c48ab93c8fe0caf4c82ed81ac
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-29 16:32:23 +0100

    feat: add css property

--- src/components/WishlistCard.jsx
+++ src/components/WishlistCard.jsx
@@ -14,6 +14,7 @@ const WishlistCard = () => {
 }
 
 const Container = styled.article`
+  padding: 3rem;
   background-color: var(--clr-white);
   box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.25);
   -webkit-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.25);
