a7cda1999148321c796b46ccead7b806bbea9800
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-30 00:45:02 +0100

    feat: add css properties

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -15,6 +15,9 @@ const Container = styled.section`
   position: fixed;
   top: 0;
   left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default DeleteModal;
