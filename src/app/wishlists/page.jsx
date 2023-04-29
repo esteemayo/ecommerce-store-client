e040db8325ac070055f41d428030e4abed16b0a5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-29 12:30:11 +0100

    feat: add a div tag

--- src/app/wishlists/page.jsx
+++ src/app/wishlists/page.jsx
@@ -4,7 +4,9 @@ import styled from 'styled-components';
 const WishLists = () => {
   return (
     <Container>
-      WishLists
+      <Wrapper>
+        WishLists
+      </Wrapper>
     </Container>
   );
 }
