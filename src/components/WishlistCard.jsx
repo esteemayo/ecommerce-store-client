a8daa293b5f5399129667d17947668f502868ffd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-30 00:29:53 +0100

    fix: refactor code

--- src/components/WishlistCard.jsx
+++ src/components/WishlistCard.jsx
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CloseIcon from '@mui/icons-material/Close';
 import { NumericFormat } from 'react-number-format';
 
-const WishlistCard = ({ id, name, desc, image, price, onDelete }) => {
+const WishlistCard = ({ wishllists, onDelete }) => {
   return (
     <Container>
       <Wrapper>
