658c242b0a4cd5f832b05963d577c00c96494eb8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-30 19:10:31 +0100

    feat: add p tag

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -18,6 +18,7 @@ const CartModal = ({ isModalOpen, closeModal }) => {
           <ProductName>The Stanley Rain Jacket in Black</ProductName>
           <PriceContainer>
             <ProductPrice>$99.99</ProductPrice>
+            <PriceDiscount>-22%</PriceDiscount>
           </PriceContainer>
         </ProductContainer>
       </Wrapper>
