dce820947ce531d0339666086f6ba158cab03ec4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-30 20:09:18 +0100

    feat: add FontAwesomeIcon

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import CloseIcon from '@mui/icons-material/Close';
 import Image from 'next/image';
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const CartModal = ({ isModalOpen, closeModal }) => {
   const [size, setSize] = useState(null);
