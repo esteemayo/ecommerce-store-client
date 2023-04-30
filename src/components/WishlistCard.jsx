24a4bc349ba3ec1b57a4646df8c6da45ab21f4a0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-30 02:32:45 +0100

    feat: add selectedId to the state value

--- src/components/WishlistCard.jsx
+++ src/components/WishlistCard.jsx
@@ -10,6 +10,7 @@ import { useState } from 'react';
 import DeleteModal from './DeleteModal';
 
 const WishlistCard = ({ wishlists, onDelete }) => {
+  const [selectedId, setSelectedId] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
