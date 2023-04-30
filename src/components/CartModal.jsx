a4e6a604ebac3c563ce2dc25dd4439f245961eff
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-30 18:12:21 +0100

    feat: add css properties

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -19,6 +19,9 @@ const Container = styled.div`
   width: 100vw;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
 `;
 
 const Wrapper = styled.div`
