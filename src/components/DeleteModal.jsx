c4e673cfb4bcdadfae430f96ef0cad8c6d2e6e94
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-04-30 02:58:28 +0100

    feat: create deleteWishlistHandler to delete wishlists item

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -8,6 +8,11 @@ const DeleteModal = ({ openModal, closeModal, onDelete, wishlistId }) => {
     }
   };
 
+  const deleteWishlistHandler = (wishlistId) => {
+    onDelete(wishlistId);
+    closeModal(false);
+  };
+
   return (
     <Container
       className='container'
@@ -22,7 +27,7 @@ const DeleteModal = ({ openModal, closeModal, onDelete, wishlistId }) => {
         <WarningMessage>Are you sure you wanted to remove this product from your wishlist?</WarningMessage>
         <ButtonContainer>
           <CancelButton type='button' onClick={() => closeModal(false)}>Not now</CancelButton>
-          <DeleteButton type='button' onClick={() => onDelete(wishlistId)}>Remove</DeleteButton>
+          <DeleteButton type='button' onClick={() => }>Remove</DeleteButton>
         </ButtonContainer>
       </Wrapper>
     </Container>
