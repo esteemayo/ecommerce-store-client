0c998ccab29f4965a144a271713def4b2a3a52c0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-02 00:39:20 +0100

    fix: modify

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -41,10 +41,10 @@ const HeadingSub = styled.span`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 2.5rem;
-  height: 2.5rem;
+  width: 3rem;
+  height: 3rem;
   font-weight: 300;
-  font-size: 2rem;
+  font-size: 1.7rem;
   background-color: var(--clr-white);
   color: inherit;
   box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
