79a7499cd5f89ba3450e6664dfb8e3df64c9517b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-01 17:00:12 +0100

    fix: modify

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -16,14 +16,12 @@ const Pagination = () => {
 }
 
 const Container = styled.section`
-  width: 100vw;
+  /* width: 100vw; */
   padding: 5rem 0 10rem;
 `;
 
 const Wrapper = styled.div`
   width: 100%;
-  max-width: 102.4rem;
-  margin: 0 auto;
   padding: 2rem 3rem;
   background-color: var(--clr-white);
   color: inherit;
