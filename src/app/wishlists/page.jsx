4e7a95e8ae379317638831a3af177cbb7df4c4b9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-01 10:47:09 +0100

    fix: add CartModal component

--- src/app/wishlists/page.jsx
+++ src/app/wishlists/page.jsx
@@ -7,6 +7,7 @@ import { wishlists } from '@/data';
 import { closeSubmenu } from '@/features/submenu/submenuSlice';
 import WishlistCard from '@/components/WishlistCard';
 import WishlistHeader from '@/components/WishlistHeader';
+import CartModal from './CartModal';
 
 const WishLists = () => {
   const dispatch = useDispatch();
