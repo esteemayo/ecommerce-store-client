e39866b75ac316be2d3cc3522684a42901d5cdde
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-02 02:19:22 +0100

    fix: modify

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import Image from 'next/image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const CartItem = () => {
