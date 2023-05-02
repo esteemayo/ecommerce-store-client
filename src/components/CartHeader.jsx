e1124dc59f8b5c91004292ba5dabc09ced2bf636
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-02 18:42:56 +0100

    feat: add css properties

--- src/components/CartHeader.jsx
+++ src/components/CartHeader.jsx
@@ -9,7 +9,11 @@ const CartHeader = () => {
 }
 
 const Container = styled.div`
-  
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2rem 5rem 0.5rem 5rem;
+  margin-bottom: 3rem;
 `;
 
 export default CartHeader;
