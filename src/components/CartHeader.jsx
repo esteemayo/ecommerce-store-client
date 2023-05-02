1ce589388df611b299e767d6bb60b1749463e4cb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-02 18:44:27 +0100

    fix: refactor code

--- src/components/CartHeader.jsx
+++ src/components/CartHeader.jsx
@@ -29,4 +29,20 @@ const HeadingMain = styled.span`
   font-weight: 400;
 `;
 
+const HeadingSub = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 3rem;
+  height: 3rem;
+  font-weight: 300;
+  font-size: 1.7rem;
+  background-color: var(--clr-white);
+  color: inherit;
+  box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
+  -webkit-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
+  -moz-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
+  border-radius: 50%;
+`;
+
 export default CartHeader;
