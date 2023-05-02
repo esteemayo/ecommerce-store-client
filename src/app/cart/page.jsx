61ad0128ba3ebd0dea5c7912a0072bc21fb2970d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-02 18:23:01 +0100

    feat: add css property

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -44,6 +44,7 @@ const HeadingContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   padding: 2rem 5rem 0.5rem 5rem;
+  margin-bottom: 3rem;
 `;
 
 const Heading = styled.h1`
