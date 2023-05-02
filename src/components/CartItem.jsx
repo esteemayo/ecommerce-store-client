014855e71b8801b29b8599ea58929facc63b971e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-02 17:36:30 +0100

    feat: import react-number-format

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import Image from 'next/image';
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { NumericFormat } from 'react-number-format';
 
 const CartItem = ({ id, name, size, color, image, price, quantity }) => {
   return (
