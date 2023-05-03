2bb88cc527c9534da488b8b40b2e1e85c940263a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-04 00:08:49 +0100

    fix: reduce font size

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -126,7 +126,7 @@ const Button = styled.button`
   border: none;
   display: inline-block;
   text-transform: capitalize;
-  font-size: 1.8rem;
+  font-size: 1.5rem;
   width: 100%;
   padding: ${({ btnType }) => btnType === 'check' ? '1rem 3rem' : '1rem 0.5rem'};
   background-color: var(--clr-black);
