2af78808079b763b56b09c63558c78d3de5e508c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-03 17:26:48 +0100

    feat: add react-number-format module

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { NumericFormat } from 'react-number-format';
 
 const CartTotal = ({ isOpen, setIsOpen, setIsCash }) => {
   const handleClose = (e) => {
