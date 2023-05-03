ea94893b4736f7badc3a2394e6254bb7f69d521d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-03 17:51:57 +0100

    feat: add cartSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from '@/features/cart/cartSlice';
 import subMenuReducer from '@/features/submenu/submenuSlice';
 
 const store = configureStore({
