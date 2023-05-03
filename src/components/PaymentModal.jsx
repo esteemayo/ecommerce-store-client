2213a9482c7ee6b47ff2ff6b6d1a97b057d40309
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-03 03:06:52 +0100

    feat: add css properties

--- src/components/PaymentModal.jsx
+++ src/components/PaymentModal.jsx
@@ -12,6 +12,12 @@ const Overlay = styled.aside`
   width: 100vw;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default PaymentModal;
