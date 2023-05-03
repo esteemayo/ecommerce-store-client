e344bd767ddc25e69febd75e41377bfc9dd8e340
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-03 02:59:30 +0100

    fix: receive setIsCash props

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 
-const CartTotal = () => {
+const CartTotal = ({ setIsCash }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
