654e9e2ceb7a179197fc55bbee17e6bb78aa4f18
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-04 01:06:36 +0100

    feat: add css properties

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -154,7 +154,10 @@ const Overlay = styled.div`
 `;
 
 const Container = styled.div`
-
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 const Wrapper = styled.div`
