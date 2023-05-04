e0b6c7ba7b47addb74b41d7925e9104281ca55d3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-04 18:50:04 +0100

    feat: add css properties

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -62,7 +62,10 @@ const Overlay = styled.section`
 `;
 
 const Container = styled.div`
-
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 const Wrapper = styled.div`
