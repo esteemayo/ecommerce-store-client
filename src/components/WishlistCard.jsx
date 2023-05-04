28a06aea1bad453fd396383f4ad6088fd7e02198
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-04 14:12:31 +0100

    fix: format code

--- src/components/WishlistCard.jsx
+++ src/components/WishlistCard.jsx
@@ -27,7 +27,12 @@ const WishlistCard = ({ wishlists, onDelete, openModal, setIsSelectedProduct }) 
           <Wrapper key={id}>
             <Left>
               <ImageContainer>
-                <Image src={image ? image[0] : '/img/img-1.jpg'} width={80} height={80} alt='' />
+                <Image
+                  src={image ? image[0] : '/img/img-1.jpg'}
+                  width={80}
+                  height={80}
+                  alt=''
+                />
               </ImageContainer>
               <OverviewContainer>
                 <ProductName>
