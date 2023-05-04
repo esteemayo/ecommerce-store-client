5b7c0aa4bdcdf5261d9d9d12edf115f650c5545b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-04 14:57:06 +0100

    fix: format code

--- src/components/WishlistHeader.jsx
+++ src/components/WishlistHeader.jsx
@@ -8,7 +8,9 @@ const WishlistHeader = () => {
     <Container>
       <HeadingContainer>
         <Heading>My wishlist</Heading>
-        <DateTime dateTime={today}>{today.toLocaleDateString('en-us', options)}</DateTime>
+        <DateTime dateTime={today}>
+          {today.toLocaleDateString('en-us', options)}
+        </DateTime>
       </HeadingContainer>
       <SendButton type='button'>Send wishlist</SendButton>
     </Container>
