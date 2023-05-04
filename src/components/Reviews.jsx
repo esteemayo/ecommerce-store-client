80922fbe82388c75276661eb1151c09d5a6a7ed8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-05 00:50:27 +0100

    feat: add reviewItems

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -4,6 +4,7 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 import StarRating from './StarRating';
 import ReviewModal from './ReviewModal';
+import { reviewItems } from '@/data';
 import ReviewCard from './ReviewCard';
 
 const Reviews = () => {
