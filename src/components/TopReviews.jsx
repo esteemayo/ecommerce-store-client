83f04185b6a4c4b7abb66c73276a023f6c620d0e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-05 18:39:05 +0100

    fix: refactor code

--- src/components/TopReviews.jsx
+++ src/components/TopReviews.jsx
@@ -71,29 +71,6 @@ const HeadingContainer = styled.div`
   margin: 0 auto;
 `;
 
-const Heading = styled.h1`
-  width: 100%;
-  max-width: 120rem;
-  margin: 0 auto;
-  display: inline-block;
-  font-family: inherit;
-  font-weight: 600;
-  font-size: 2.7rem;
-  color: var(--clr-secondary-green);
-  margin-bottom: 7rem;
-  display: flex;
-  flex-direction: column;
-
-  &::after {
-    content: '';
-    display: inline-block;
-    width: 4rem;
-    height: 3px;
-    background-color: var(--clr-secondary-green);
-    border-radius: 3px;
-  }
-`;
-
 const Wrapper = styled.div`
   position: relative;
 `;
