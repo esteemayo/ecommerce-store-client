a6d1b7bf6bff7528526b5610e49a548f59e2ea02
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-06 14:44:29 +0100

    feat: add css properties

--- src/app/products/category/[category]/page.jsx
+++ src/app/products/category/[category]/page.jsx
@@ -93,6 +93,8 @@ const Label = styled.label`
   display: inline-block;
   text-transform: capitalize;
   font-size: 1.6rem;
+  letter-spacing: 3px;
+  margin-bottom: 0.5rem;
 `;
 
 const Select = styled.select`
