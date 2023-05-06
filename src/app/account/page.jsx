9f54b53a5e198b100cc3cefc8a2251087d6bf496
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-06 22:38:38 +0100

    feat: add css properties

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -44,7 +44,10 @@ const HeadingText = styled.p`
 `;
 
 const AccountContainer = styled.div`
-  
+  box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
+  -webkit-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
+  -moz-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
+  border-radius: 1rem;
 `;
 
 const AccountWrapper = styled.div`
