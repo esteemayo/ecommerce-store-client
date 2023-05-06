4d067329b65df3a39989a90a82a39491dacef071
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-06 23:21:02 +0100

    feat: add css properties

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -134,6 +134,8 @@ const FormLabel = styled.label`
 const FormInput = styled.input`
   border: none;
   display: inline-block;
+  font-family: inherit;
+  font-size: 1.5rem;
 `;
 
 const Right = styled.div`
