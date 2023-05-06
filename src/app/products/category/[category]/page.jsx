0ac8330d49d569ac6e64aa3ec1cf52075fe7789d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-06 19:02:52 +0100

    feat: add useDispatch react redux hook

--- src/app/products/category/[category]/page.jsx
+++ src/app/products/category/[category]/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { storeProducts } from '@/data';
 import ProductList from '@/components/ProductList';
