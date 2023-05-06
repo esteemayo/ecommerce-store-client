c84c281d7ec2e4a3d1e92353e86a245e3cc955fd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-06 17:16:04 +0100

    fix: modify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -290,8 +290,8 @@ const CartTotal = styled.span`
   align-items: center;
   justify-content: center;
   position: absolute;
-  top: -0.5rem;
-  right: -0.5rem;
+  top: -0.7rem;
+  right: -1rem;
 `;
 
 export default Navbar;
