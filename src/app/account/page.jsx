570907c5a7abe5c1074f76f32fe05aaaf2160d29
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-06 19:24:51 +0100

    feat: add div tag

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -4,7 +4,9 @@ import styled from 'styled-components';
 const Account = () => {
   return (
     <Container>
-      Account
+      <Wrapper>
+        Account
+      </Wrapper>
     </Container>
   );
 }
