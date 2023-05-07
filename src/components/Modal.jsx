5ed95ad9a130ab06004c1222648eb83a22c4102c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-07 16:11:21 +0100

    feat: add css properties

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -36,7 +36,11 @@ const Container = styled.div`
 `;
 
 const Wrapper = styled.div`
-  
+  width: 40rem;
+  padding: 2rem 4rem;
+  background-color: var(--clr-white);
+  border-radius: 0.5rem;
+  position: relative;
 `;
 
 const ButtonContainer = styled.div`
