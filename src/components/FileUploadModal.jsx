d787763aafed4e0266926b7d84bfe403d81726f1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-07 02:39:04 +0100

    fix: receives props

--- src/components/FileUploadModal.jsx
+++ src/components/FileUploadModal.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const FileUploadModal = () => {
+const FileUploadModal = ({ openModal, closeModal }) => {
   return (
     <Overlay>
       FileUploadModal
