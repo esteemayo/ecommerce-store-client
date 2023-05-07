4cb667ca2dd34137c8375700e18fc566fc7545e1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-07 20:46:47 +0100

    feat: add css properties

--- src/components/UpdatePassword.jsx
+++ src/components/UpdatePassword.jsx
@@ -118,6 +118,15 @@ const FormButton = styled.button`
   font-size: 1.4rem;
   width: 50%;
   padding: 1.5rem;
+  background-color: #6d857f;
+  color: var(--clr-white);
+  background-image: linear-gradient(
+    120deg,
+    transparent 0%,
+    transparent 50%,
+    #89a8a0 50%
+  );
+  background-size: 220%;
 `;
 
 export default UpdatePassword;
