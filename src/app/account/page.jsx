93383e27e73a8851938a674d1bb3758294ef7756
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-07 02:35:41 +0100

    fix: modify

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import FileUploadModal from '@/components/FileUploadModal';
 
