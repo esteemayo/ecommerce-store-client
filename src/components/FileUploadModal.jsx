3ae79897eff3d7e436e7724cf7380dca66643ff8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-07 09:30:42 +0100

    feat: add css properties

--- src/components/FileUploadModal.jsx
+++ src/components/FileUploadModal.jsx
@@ -80,7 +80,9 @@ const CloseButton = styled.button`
 `;
 
 const UploadContainer = styled.div`
-  
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const InputContainer = styled.div`
