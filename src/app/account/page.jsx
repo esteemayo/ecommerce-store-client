277816f516a2ab527f2ecd1edcbdbb36f61970fb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-07 02:33:30 +0100

    feat: add useState react hook

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -2,6 +2,7 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const Account = () => {
   return (
