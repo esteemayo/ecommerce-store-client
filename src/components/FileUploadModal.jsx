4ea69d354df2ee7b7e610852ed127914461ad6f8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-07 09:22:07 +0100

    feat: add css properties

--- src/components/FileUploadModal.jsx
+++ src/components/FileUploadModal.jsx
@@ -90,6 +90,12 @@ const Label = styled.label`
 `;
 
 const Input = styled.input`
+  width: 0.1px;
+  height: 0.1px;
+  opacity: 0;
+  overflow: hidden;
+  position: absolute;
+  z-index: -1;
 `;
 
 export default FileUploadModal;
