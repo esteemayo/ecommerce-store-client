ef283b1695c7d0955463ddd2cf638a9008af0227
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-07 02:48:04 +0100

    feat: add css properties

--- src/components/FileUploadModal.jsx
+++ src/components/FileUploadModal.jsx
@@ -26,7 +26,10 @@ const Overlay = styled.aside`
 `;
 
 const Container = styled.div`
-  
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 const Wrapper = styled.div`
