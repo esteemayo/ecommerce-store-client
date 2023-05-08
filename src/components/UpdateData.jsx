0486665a0969d846c7cfa8fad67f3e42571057ec
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-08 15:32:41 +0100

    feat: add css properties

--- src/components/UpdateData.jsx
+++ src/components/UpdateData.jsx
@@ -121,6 +121,13 @@ const FormButton = styled.button`
   padding: 1.5rem;
   background-color: var(--clr-primary-green);
   color: var(--clr-white);
+  background-image: linear-gradient(
+    120deg,
+    transparent 0%,
+    transparent 50%,
+    #89a8a0 50%
+  );
+  background-size: 220%;
 `;
 
 export default UpdateData;
