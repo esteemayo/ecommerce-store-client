77cd09ead3b5d93322efe3c2ad8dc6dd4a596e2d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-08 16:23:37 +0100

    fix: modify

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -80,13 +80,16 @@ const Account = () => {
               </Left>
               <Right>
                 <AvatarContainer>
-                  <Image
-                    src='/img/user-9.jpg'
-                    width={100}
-                    height={100}
-                    alt=''
-                  />
-                  {/* <FontAwesomeIcon icon={faUser} /> */}
+                  {user ? (
+                    <Image
+                      src='/img/user-9.jpg'
+                      width={100}
+                      height={100}
+                      alt=''
+                    />
+                  ) : (
+                    <FontAwesomeIcon icon={faUser} />
+                  )}
                 </AvatarContainer>
                 <FileButton type='button' onClick={() => setIsFileUpload(true)}>Upload a picture</FileButton>
               </Right>
