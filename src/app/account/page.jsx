b366c1482c79553780a4079416bb10e1a5e679bf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-08 16:15:19 +0100

    fix: add image

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -78,6 +78,7 @@ const Account = () => {
               </Left>
               <Right>
                 <AvatarContainer>
+                  <Image src='/img/user-9.jpg' width={100} height={100} alt='' />
                   <FontAwesomeIcon icon={faUser} />
                 </AvatarContainer>
                 <FileButton type='button' onClick={() => setIsFileUpload(true)}>Upload a picture</FileButton>
