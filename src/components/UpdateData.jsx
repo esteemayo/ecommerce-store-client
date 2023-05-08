208cd566173559e22614b14adf8534a70c5fe9ec
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-08 15:21:16 +0100

    feat: add css properties

--- src/components/UpdateData.jsx
+++ src/components/UpdateData.jsx
@@ -54,6 +54,10 @@ const Form = styled.form`
 const FormGroup = styled.div`
   display: flex;
   flex-direction: column;
+
+  &:not(:last-of-type) {
+    margin-bottom: 2rem;
+  }
 `;
 
 const FormLabel = styled.label`
