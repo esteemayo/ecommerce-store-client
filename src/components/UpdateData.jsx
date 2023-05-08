407f271ddc32376da781c5a5a3497d1ee7a6906a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-08 16:05:43 +0100

    fix: modify

--- src/components/UpdateData.jsx
+++ src/components/UpdateData.jsx
@@ -116,7 +116,6 @@ const FormGroup = styled.div`
 const FormLabel = styled.label`
   display: inline-block;
   width: 15rem;
-  text-transform: capitalize;
   font-weight: 600;
   font-size: 1.4rem;
   color: #888;
