711059db30372cc7abafc8b1cc0aa721542a76e1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-08 16:18:10 +0100

    fix: modify

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -7,6 +7,7 @@ import Image from 'next/image';
 
 import Modal from '@/components/Modal';
 import FileUploadModal from '@/components/FileUploadModal';
+import UpdateData from '@/components/UpdateData';
 import UpdatePassword from '@/components/UpdatePassword';
 
 const Account = () => {
