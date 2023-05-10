462f4b0ad73ce958ade978924cfd1cff43e81766
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-10 02:02:16 +0100

    fix: modify

--- src/app/orders/[id]/page.jsx
+++ src/app/orders/[id]/page.jsx
@@ -55,7 +55,7 @@ const Order = () => {
               height={30}
               alt=''
             />
-            <Status>Preparing</Status>
+            <Status>On the way</Status>
             <Image
               src='/img/checked.png'
               width={20}
