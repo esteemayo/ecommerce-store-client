7ce32ace5c1b1e7dab4bf76ef7ee46095506b91a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-10 18:06:34 +0100

    fix: modify

--- src/app/orders/[id]/page.jsx
+++ src/app/orders/[id]/page.jsx
@@ -245,7 +245,7 @@ const Button = styled.button`
   display: inline-block;
   border: none;
   text-transform: capitalize;
-  padding: 1.5rem 3rem;
+  padding: 1.35rem 3rem;
 `;
 
 export default Order;
