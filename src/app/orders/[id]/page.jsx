5abb7c2aaa33adad3c0aff6db0cd954cf81bba7f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-11 05:40:21 +0100

    feat: add css properties

--- src/app/orders/[id]/page.jsx
+++ src/app/orders/[id]/page.jsx
@@ -250,6 +250,13 @@ const Button = styled.button`
   padding: 1.35rem 3rem;
   background-color: var(--clr-black);
   color: var(--clr-white);
+  background-image: linear-gradient(
+    120 deg,
+    transparent 0%,
+    transparent 50%,
+    #333 50%
+  );
+  background-size: 220%;
 `;
 
 export default Order;
