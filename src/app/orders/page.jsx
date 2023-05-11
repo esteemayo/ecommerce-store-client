1fd4a51963d17462b3fda0a5bfdcd76697ca7bac
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-11 07:28:22 +0100

    feat: add css property

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -43,6 +43,7 @@ const OrderContainer = styled.div`
 const OrderWrapper = styled.div`
   width: 25%;
   padding: 2rem;
+  border-radius: 1rem;
   box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
   -webkit-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
   -moz-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
