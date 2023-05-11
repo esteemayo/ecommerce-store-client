ce7775ce43ea9823ca281313f116a15026727288
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-11 15:38:14 +0100

    feat: add css properties

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -78,6 +78,11 @@ const OrderWrapper = styled.div`
   box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
   -webkit-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
   -moz-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
+
+  &::before {
+    content: '';
+    display: block;
+  }
 `;
 
 const OrderId = styled.h2`
