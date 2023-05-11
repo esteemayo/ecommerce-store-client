6f1cd7a58685cc00c57f7587026a3fab705716a5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-11 23:55:40 +0100

    fix: refactor code

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -49,26 +49,6 @@ const OrderContainer = styled.div`
   gap: 3rem;
 `;
 
-const OrderWrapper = styled.div`
-  width: 30%;
-  border-radius: 1rem;
-  box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
-  -webkit-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
-  -moz-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
-
-  &::before {
-    content: '';
-    display: block;
-    width: 95%;
-    height: 2px;
-    margin: 0 auto;
-    background-color: ${({ status }) => status === 0 && 'var(--clr-primary-red)'};
-    background-color: ${({ status }) => status === 1 && '#ffb900'};
-    background-color: ${({ status }) => status === 2 && 'var(--clr-secondary-green)'};
-    border-radius: 1rem;
-  }
-`;
-
 const OrderDateIconContainer = styled.div`
   display: flex;
   align-items: flex-start;
