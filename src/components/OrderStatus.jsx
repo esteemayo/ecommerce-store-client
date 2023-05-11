248a77e31f4d269c328efb737b9acb4cd07c1ee8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-11 06:30:24 +0100

    fix: refactor code

--- src/components/OrderStatus.jsx
+++ src/components/OrderStatus.jsx
@@ -1,9 +1,74 @@
 import styled from 'styled-components';
+import Image from 'next/image';
+import { NumericFormat } from 'react-number-format';
 
 const OrderStatus = ({ statusClass }) => {
   return (
     <Container>
-      OrderStatus
+      <StatusWrapper className={statusClass(0)}>
+        <Image
+          src='/img/paid.png'
+          width={30}
+          height={30}
+          alt=''
+        />
+        <Status>Payment</Status>
+        <Image
+          src='/img/checked.png'
+          width={20}
+          height={20}
+          alt=''
+          className='checkedIcon'
+        />
+      </StatusWrapper>
+      <StatusWrapper className={statusClass(1)}>
+        <Image
+          src='/img/bake.png'
+          width={30}
+          height={30}
+          alt=''
+        />
+        <Status>Preparing</Status>
+        <Image
+          src='/img/checked.png'
+          width={20}
+          height={20}
+          alt=''
+          className='checkedIcon'
+        />
+      </StatusWrapper>
+      <StatusWrapper className={statusClass(2)}>
+        <Image
+          src='/img/bike.png'
+          width={30}
+          height={30}
+          alt=''
+        />
+        <Status>On the way</Status>
+        <Image
+          src='/img/checked.png'
+          width={20}
+          height={20}
+          alt=''
+          className='checkedIcon'
+        />
+      </StatusWrapper>
+      <StatusWrapper className={statusClass(3)}>
+        <Image
+          src='/img/delivered.png'
+          width={30}
+          height={30}
+          alt=''
+        />
+        <Status>Delivered</Status>
+        <Image
+          src='/img/checked.png'
+          width={20}
+          height={20}
+          alt=''
+          className='checkedIcon'
+        />
+      </StatusWrapper>
     </Container>
   );
 }
