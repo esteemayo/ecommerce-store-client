236b5bbcc0fe5eb080c29d2540eff76160051af1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-11 16:26:10 +0100

    feat: add MoreVertIcon

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -2,6 +2,7 @@
 import styled from 'styled-components';
 import { NumericFormat } from 'react-number-format';
 import Link from 'next/link';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useDispatch } from 'react-redux';
 
 import { orders } from '@/data';
