665d27e7a4e5709bdf1f789081bbf850b52db080
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-11 23:46:48 +0100

    feat: add css properties

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -9,7 +9,11 @@ const OrderCard = () => {
 }
 
 const Container = styled.article`
-  
+  width: 30%;
+  border-radius: 1rem;
+  box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
+  -webkit-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
+  -moz-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
 `;
 
 export default OrderCard;
