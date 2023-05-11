9a8d0cf393e2b800ad69e32e7c1f21287bda7c02
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-11 07:31:22 +0100

    fix: modify

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -41,7 +41,7 @@ const OrderContainer = styled.div`
 `;
 
 const OrderWrapper = styled.div`
-  width: 25%;
+  width: 30%;
   padding: 2rem;
   border-radius: 1rem;
   box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
