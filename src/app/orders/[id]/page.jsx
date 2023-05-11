42acfaf265aa3b19b3f6ac6565e4bb2eb07d27ce
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-11 05:55:02 +0100

    feat: addreact-number-format module

--- src/app/orders/[id]/page.jsx
+++ src/app/orders/[id]/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 import styled from 'styled-components';
 import Image from 'next/image';
+import { NumericFormat } from 'react-number-format';
 
 const Order = () => {
   const status = 0;
