c1776c2fecf69df88ec7f0e20b542b88db182e23
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-13 00:44:35 +0100

    fix: modify

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
 import {
   persistStore,
   persistReducer,
@@ -9,7 +10,6 @@ import {
   PURGE,
   REGISTER,
 } from 'redux-persist'
-import storage from 'redux-persist/lib/storage';
 
 import cartReducer from '@/features/cart/cartSlice';
 import subMenuReducer from '@/features/submenu/submenuSlice';
