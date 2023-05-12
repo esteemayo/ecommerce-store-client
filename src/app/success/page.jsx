4ab7bfef4a6588facfbb81631880c18e4c771ac9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-12 01:18:44 +0100

    feat: add faLocationDot icon

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 const Success = () => {
   return (
