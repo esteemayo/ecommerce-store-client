14f022ffaa754ec2e8833971712922c68332ac31
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-13 00:28:51 +0100

    feat: add redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 import cartReducer from '@/features/cart/cartSlice';
 import subMenuReducer from '@/features/submenu/submenuSlice';
