7f31e4d88e53ff23c8632bc15298ff8018a7917c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-13 20:29:13 +0100

    fix: add FaFacebookF icon

--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import PinterestIcon from '@mui/icons-material/Pinterest';
+import { FaFacebookF } from 'react-icons/fa';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBagShopping,
