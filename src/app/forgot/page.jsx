c24db496d4dfff3c05545e5cdc97aed6b23e4101
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-14 00:39:48 +0100

    fix: add css property

--- src/app/forgot/page.jsx
+++ src/app/forgot/page.jsx
@@ -62,7 +62,7 @@ const Forgot = () => {
 
 const Container = styled.main`
   width: 100vw;
-  height: 100vh;
+  padding: 10rem 0;
   display: flex;
   align-items: center;
   justify-content: center;
