07d939c6cf1e8a665f8c16ff45dc0fbf3caecdb8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-14 00:57:16 +0100

    fix: modify

--- src/app/reset/[token]/page.jsx
+++ src/app/reset/[token]/page.jsx
@@ -11,7 +11,7 @@ const ResetPassword = () => {
   const passwordRef = useRef();
   const [errors, setErrors] = useState({});
   const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const validateForm = () => {
     const tempErrors = {};
