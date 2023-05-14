634691e9a2d6908d640ebdb5dbbdcaf8f2609976
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-14 17:27:16 +0100

    fix: add media query

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -75,6 +75,11 @@ const Container = styled.main`
     padding-top: 6rem;
     padding-bottom: 6rem;
   }
+
+  @media only screen and (max-width: 25em) {
+    padding-left: 3rem;
+    padding-right: 3rem;
+  }
 `;
 
 const Wrapper = styled.div`
