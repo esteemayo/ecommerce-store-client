eedb6c470f1f2a9d0398595c40958783e83714b8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-16 00:13:13 +0100

    fix: add media query

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -144,6 +144,10 @@ const Title = styled.p`
   text-transform: capitalize;
   font-weight: 500;
   font-size: 1.6rem;
+
+  @media only screen and (max-width: 43.75em) {
+    font-size: 1.5rem;
+  }
 `;
 
 const ProductColor = styled.span`
