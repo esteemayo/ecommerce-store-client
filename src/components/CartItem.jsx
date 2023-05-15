e13b077c1b0d0940c805811fe712636f0627e066
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-16 00:15:24 +0100

    fix: add media query

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -183,6 +183,10 @@ const ProductSize = styled.span`
   display: block;
   font-size: 1.5rem;
   color: #999;
+
+  @media only screen and (max-width: 43.75em) {
+    font-size: 1.45rem;
+  }
 `;
 
 const RemoveButton = styled.button`
