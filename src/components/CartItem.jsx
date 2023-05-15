1b8aa24491ee585262658cc6a563bd147a8f6670
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-16 00:18:21 +0100

    fix: modify

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -67,10 +67,6 @@ const CartItem = ({ id, name, size, color, images, price, quantity }) => {
 
 const Container = styled.article`
   line-height: 1.12;
-
-  @media only screen and (max-width: 43.75em) {
-    line-height: 1.08;
-  }
   
   &:not(:last-of-type) {
     border-bottom: 1px solid #e2e9e7;
