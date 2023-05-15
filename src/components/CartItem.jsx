51b7a43a4f04a59bf699f95d919230adff920a92
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-16 00:12:29 +0100

    fix: add media query

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -116,6 +116,10 @@ const ProductName = styled.h2`
   font-weight: 500;
   font-size: 2rem;
   margin-bottom: 0.5rem;
+
+  @media only screen and (max-width: 43.75em) {
+    font-size: 1.8rem;
+  }
 `;
 
 const PriceContainer = styled.div`
