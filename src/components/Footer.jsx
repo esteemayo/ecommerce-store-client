68cce690a2350755f1af398db0d1a43395fdcbee
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-17 03:19:46 +0100

    fix: format code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -94,7 +94,8 @@ const Footer = () => {
       </FooterLinkContainer>
       <FooterText>
         &copy; {year} by <Author>Emmanuel Adebayo&trade;</Author>. All rights reserved.
-        We are a young company always looking for new and creative ideas to help you with our products in your everyday work.
+        We are a young company always looking for new and creative ideas to help you 
+        with our products in your everyday work.
       </FooterText>
     </Container>
   );
