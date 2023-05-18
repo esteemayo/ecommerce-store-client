4db898744277613b285b79731aff47c5bfd2b974
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-18 18:10:55 +0100

    fix: add media query

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -62,6 +62,10 @@ const Container = styled.article`
   @media only screen and (max-width: 37.5em) {
     gap: 2rem;
   }
+
+  @media only screen and (max-width: 31.25em) {
+    gap: 1.5rem;
+  }
 `;
 
 const Button = styled.button`
