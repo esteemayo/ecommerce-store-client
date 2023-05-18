9ec9bcd96cdd65f0ee361bd66e7de73ba262bafa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-18 18:39:18 +0100

    fix: add media query

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -40,6 +40,10 @@ const Container = styled.section`
     padding-left: 3rem;
     padding-right: 3rem;
   }
+
+  @media only screen and (max-width: 23.75em) {
+    padding-left: 2rem;
+  }
 `;
 
 export default Categories;
