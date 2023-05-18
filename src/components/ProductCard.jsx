12adea185649962f57ae27400d6024da496322a6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-19 00:52:01 +0100

    feat: create ProductCard component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const ProductCard = () => {
+  return (
+    <Container>
+      ProductCard
+    </Container>
+  );
+}
+
+const Container = styled.article`
+  
+`;
+
+export default ProductCard;
