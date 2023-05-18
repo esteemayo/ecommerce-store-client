991c553922543e8a543035e50cd7b6f77d1e4167
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-18 20:18:49 +0100

    fix: modify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,7 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const Header = ({ title }) => {
-  return (
-    <Heading>{title}</Heading>
-  );
+  return <Heading>{title}</Heading>;
 }
 
 const Heading = styled.h1`
