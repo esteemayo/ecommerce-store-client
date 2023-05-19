0028df8fbc5a584c49964bcc9375a04b9ef5c91c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-19 02:06:27 +0100

    fix: add css properties

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -207,6 +207,10 @@ const Button = styled.button`
   outline-color: #85beae;
   border-radius: 0.5rem;
   cursor: pointer;
+
+  &:hover {
+    background-position: 100%;
+  }
 `;
 
 const FavWrapper = styled.div`
