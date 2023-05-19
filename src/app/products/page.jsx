b7817cffc2d7558c147ea2c16f90818629ce2187
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-19 15:42:28 +0100

    fix: add media query

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -94,6 +94,11 @@ const Container = styled.main`
   @media only screen and (max-width: 50em) {
     max-width: 70rem;
   }
+
+  @media only screen and (max-width: 37.5em) {
+    padding-left: 5rem;
+    padding-right: 5rem;
+  }
 `;
 
 export default Products;
