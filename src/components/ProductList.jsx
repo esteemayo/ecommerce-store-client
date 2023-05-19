7d9f8df3be46eef86b50d4fb262b2b1ae56118e9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-19 15:45:25 +0100

    feat: add media query

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -61,6 +61,10 @@ const ProductsContainer = styled.div`
   @media only screen and (max-width: 37.5em) {
     gap: 2rem;
   }
+
+  @media only screen and (max-width: 31.25em) {
+    gap: 1.5rem;
+  }
 `;
 
 export default ProductList;
