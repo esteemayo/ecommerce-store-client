174aec59ea9f11503ae01c38cab12f2d6c12701f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-19 02:00:32 +0100

    fix: add css properties

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -146,6 +146,9 @@ const Reviews = styled.div`
 
 const PriceContainer = styled.div`
   margin: 0.5rem 0 1.5rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const Prices = styled.div`
