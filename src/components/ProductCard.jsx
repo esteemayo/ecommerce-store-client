3d689ae3d8b07fb4f9aa1a445c396b4f7fc1d40f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-19 02:32:56 +0100

    fix: modify

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -78,6 +78,7 @@ const Container = styled.article`
   box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
   -webkit-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
   -moz-box-shadow: 2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.47);
+`;
 
   img {
     width: 35rem;
