3f323a1680613fa017c31ee1da6a47cf10cd25cf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-20 06:41:32 +0100

    fix: add media query

--- src/app/products/category/[category]/page.jsx
+++ src/app/products/category/[category]/page.jsx
@@ -132,6 +132,11 @@ const Container = styled.main`
     padding-left: 5rem;
     padding-right: 5rem;
   }
+
+  @media only screen and (max-width: 25em) {
+    padding-left: 3rem;
+    padding-right: 3rem;
+  }
 `;
 
 const FilterContainer = styled.section`
