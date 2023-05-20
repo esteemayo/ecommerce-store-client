596e9f0098689f575b44aa020328413f954e8c8f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-20 07:01:30 +0100

    fix: modify

--- src/app/products/category/[category]/page.jsx
+++ src/app/products/category/[category]/page.jsx
@@ -249,15 +249,4 @@ const Option = styled.option`
   color: inherit;
 `;
 
-const ProductWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: flex-start;
-
-  @media only screen and (max-width: 37.5em) {
-    padding: 4rem 0;
-  }
-`;
-
 export default ProductCategory;
