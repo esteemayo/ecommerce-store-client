c882652873e23a27c8e856518ae2b5bfbef25567
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-20 09:22:11 +0100

    fix: add media query

--- src/components/TopReviews.jsx
+++ src/components/TopReviews.jsx
@@ -77,6 +77,10 @@ const HeadingContainer = styled.div`
   @media only screen and (max-width: 59.375em) {
     max-width: 80rem;
   }
+
+  @media only screen and (max-width: 50em) {
+    max-width: 70rem;
+  }
 `;
 
 const Wrapper = styled.div`
