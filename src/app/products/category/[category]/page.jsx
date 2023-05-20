1016d4a99845d19c65f3b7560dbbd1024b1ea46e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-20 06:41:08 +0100

    fix: add media query

--- src/app/products/category/[category]/page.jsx
+++ src/app/products/category/[category]/page.jsx
@@ -127,6 +127,11 @@ const Container = styled.main`
   @media only screen and (max-width: 50em) {
     max-width: 70rem;
   }
+
+  @media only screen and (max-width: 37.5em) {
+    padding-left: 5rem;
+    padding-right: 5rem;
+  }
 `;
 
 const FilterContainer = styled.section`
