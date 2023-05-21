81a984239bd58b51735bdce958e21be763d47988
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-21 10:30:54 +0100

    fix: add media query

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -95,6 +95,10 @@ const Container = styled.section`
   @media only screen and (max-width: 59.375em) {
     max-width: 80rem;
   }
+
+  @media only screen and (max-width: 50em) {
+    max-width: 70rem;
+  }
 `;
 
 const Heading = styled.h1`
