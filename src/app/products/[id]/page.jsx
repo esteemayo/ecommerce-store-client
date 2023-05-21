0b7b8d125fa43ec1548d2c53fa9fb759aa9dea96
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-21 10:18:46 +0100

    fix: add media query

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -635,6 +635,10 @@ const Button = styled.button`
   cursor: pointer;
   transition: all 0.3s;
 
+  @media only screen and (max-width: 59.375em) {
+    padding-top: 1.75rem;
+  }
+
   &:hover {
     background-position: 100%;
   }
