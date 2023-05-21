9c8fbfa35201e970f28b6c411ac92107a5efc2fb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-21 10:25:39 +0100

    fix: add media query

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -149,6 +149,10 @@ const Heading = styled.h1`
   font-weight: 500;
   font-size: 3.2rem;
   line-height: 3.2rem;
+
+  @media only screen and (max-width: 59.375em) {
+    font-size: 3.15rem;
+  }
 `;
 
 const ReviewContainer = styled.div`
