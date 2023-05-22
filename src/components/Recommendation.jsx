132df4e96283777d4c1cdb6367dee6bba961c66e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-22 13:06:38 +0100

    fix: modify

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -107,10 +107,6 @@ const Heading = styled.h1`
   font-weight: 500;
   font-size: 3.2rem;
   margin: 5rem 0;
-
-  @media only screen and (max-width: 59.375em) {
-    font-size: 3.15rem;
-  }
 `;
 
 const Wrapper = styled.div`
