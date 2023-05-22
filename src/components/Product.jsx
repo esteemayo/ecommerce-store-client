ee107062005674917f605adaca94d2a916476e3f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-23 00:48:48 +0100

    feat: add react-number-format module

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
+import { NumericFormat } from 'react-number-format';
 
 const Product = () => {
   return (
