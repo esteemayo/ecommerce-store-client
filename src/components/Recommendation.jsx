8a6885285c1f045e80e060d1c02322910089ce67
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-22 14:00:26 +0100

    fix: add media query

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -104,6 +104,11 @@ const Container = styled.section`
     padding-left: 5rem;
     padding-right: 5rem;
   }
+
+  @media only screen and (max-width: 25em) {
+    padding-left: 3rem;
+    padding-right: 3rem;
+  }
 `;
 
 const Heading = styled.h1`
