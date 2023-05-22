6eba52bf54483b1b2eb661f7605efd7d48f7569a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-23 00:30:30 +0100

    fix: add media query

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -654,6 +654,10 @@ const ColorWrapper = styled.div`
   @media only screen and (max-width: 37.5em) {
     margin-top: 1.3rem;
   }
+
+  @media only screen and (max-width: 18.75em) {
+    gap: 2rem;
+  }
 `;
 
 const Color = styled.button`
