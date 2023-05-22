dbf5040ba725dcfe2a023dea25f9cd147a42bce5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-23 00:48:11 +0100

    feat: add next/image module

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
 
 const Product = () => {
   return (
