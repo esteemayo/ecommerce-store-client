f9f89acc2463176cb8c6ea0a2e52371e6a7457e7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-24 00:30:30 +0100

    fix: add styling

--- src/app/reset/[token]/page.jsx
+++ src/app/reset/[token]/page.jsx
@@ -84,6 +84,7 @@ const ResetPassword = () => {
 const Container = styled.main`
   width: 100vw;
   max-width: 120rem;
+  margin: 0 auto;
   padding: 10rem 0;
   display: flex;
   align-items: center;
