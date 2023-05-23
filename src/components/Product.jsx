bb865abf913e0fd4c5a921a8f59dac011585c391
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-23 02:21:52 +0100

    fix: modify

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,6 +5,8 @@ import CloseIcon from '@mui/icons-material/Close';
 import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import { faArrowLeft, faArrowRight, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { NumericFormat } from 'react-number-format';
 
