35b1eb308f12f9d51f699dacb439d520956e7b1a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-23 15:23:34 +0100

    fix: modify

--- src/components/ProductImageModal.jsx
+++ src/components/ProductImageModal.jsx
@@ -133,10 +133,6 @@ const ArrowButton = styled.button`
     left: ${({ direction }) => direction === 'left' && '2rem'};
   }
 
-  @media only screen and (max-width: 33.125em) {
-    left: ${({ direction }) => direction === 'left' && '3rem'};
-  }
-
   &:hover {
     background-color: hsla(0, 0%, 100%, 0.05);
   }
