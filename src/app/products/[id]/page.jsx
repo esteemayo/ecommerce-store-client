744ae1f7ebb79aa4d9545b42777d5c74cca94ee3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-23 01:33:28 +0100

    fix: modify

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 import styled from 'styled-components';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import Recommendation from '@/components/Recommendation';
