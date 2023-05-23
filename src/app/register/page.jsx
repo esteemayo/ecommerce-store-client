f9eb68aff18405a08362e3bc6536c5a03cff05b4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-24 00:53:49 +0100

    fix: handle form input validation

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -37,6 +37,11 @@ const Register = () => {
 
     if (email.trim() === '') {
       errors.email = 'Email must not be empty';
+    } else {
+      const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)*[a-zA-Z]{2,9})$/;
+      if (!email.match(regEx)) {
+        errors.email = 'Email must be a valid email address';
+      }
     }
 
     if (username.trim() === '') {
