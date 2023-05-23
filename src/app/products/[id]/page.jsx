6f51e724b713d7bc1924ece94a32c21ba9284c1d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-23 01:32:58 +0100

    fix: modify

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,7 +1,6 @@
 'use client';
 import styled from 'styled-components';
 import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
