3cbaf92899e801023b99bb7eba87ba84ba3739ff
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-23 01:46:33 +0100

    fix: refactor code

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -192,35 +192,6 @@ const Right = styled.div`
   }
 `;
 
-const BreadCrumbs = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-  color: #888;
-
-  a {
-    &:link,
-    &:visited {
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-start;
-      text-decoration: underline;
-      text-decoration-color: #89a8a0;
-      text-underline-offset: 3px;
-      text-transform: capitalize;
-      font-size: 1.6rem;
-      color: currentColor;
-      outline-color: #ccc;
-      line-height: 1.2;
-    }
-
-    &:hover,
-    &:active {
-      color: currentColor;
-    }
-  }
-`;
-
 const ProductWrapper = styled.div`
   margin-top: 2rem;
 
