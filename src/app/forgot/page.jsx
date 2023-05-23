3b57d76e60f7ec1c4ccb143804c1430aacfe0d02
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-24 00:58:32 +0100

    fix: handle form input validation

--- src/app/forgot/page.jsx
+++ src/app/forgot/page.jsx
@@ -17,6 +17,11 @@ const Forgot = () => {
 
     if (!email) {
       errors.email = 'Please enter your email address';
+    } else {
+      const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)*[a-zA-Z]{2,9})$/;
+      if (!email.match(regEx)) {
+        errors.email = 'Email must be a valid email address';
+      }
     }
 
     return errors;
