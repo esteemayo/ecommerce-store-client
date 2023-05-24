529ac2d9c42b8e0cf67c23ca09188b44c265efc7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-24 05:23:03 +0100

    fix: add media query

--- src/app/wishlists/page.jsx
+++ src/app/wishlists/page.jsx
@@ -100,6 +100,10 @@ const Text = styled.p`
   @media only screen and (max-width: 25em) {
     font-size: 2.8rem;
   }
+
+  @media only screen and (max-width: 18.75em) {
+    font-size: 2rem;
+  }
 `;
 
 export default WishLists;
