47ec8e9390183e9fbec604ed473d13e58b8c7911
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-24 03:45:02 +0100

    fix: add media query

--- src/app/orders/[id]/page.jsx
+++ src/app/orders/[id]/page.jsx
@@ -119,6 +119,11 @@ const Container = styled.main`
     padding-right: 5rem;
   }
 
+  @media only screen and (max-width: 25em) {
+    padding-left: 3rem;
+    padding-right: 3rem;
+  }
+
   /* @media only screen and (max-width: 50em) {
     padding-left: 9rem;
     padding-right: 9rem;
