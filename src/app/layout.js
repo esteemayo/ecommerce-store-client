f6972174ed21e589723a4a7000745eb700c70eab
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-24 15:06:02 +0100

    feat: add useSelector react redux hook

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 'use client';
 import { ThemeProvider } from 'styled-components';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import Navbar from '@/components/Navbar';
