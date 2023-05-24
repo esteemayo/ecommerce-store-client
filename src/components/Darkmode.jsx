39f4e728df201878cef20d179e17a14ed60e342b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-24 15:16:53 +0100

    fix: modify

--- src/components/Darkmode.jsx
+++ src/components/Darkmode.jsx
@@ -1,4 +1,6 @@
 import { ThemeProvider } from 'styled-components';
+import { useSelector } from 'react-redux';
+import { darkTheme, lightTheme } from '@/utils/Theme';
 
 const Darkmode = () => {
   return (
