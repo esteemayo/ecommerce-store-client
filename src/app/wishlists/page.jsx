d7ab252c10bf883e6f1889114078dbae5b4b7011
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-24 04:21:59 +0100

    fix: modify

--- src/app/wishlists/page.jsx
+++ src/app/wishlists/page.jsx
@@ -73,11 +73,6 @@ const Wrapper = styled.div`
     max-width: 70rem;
   }
 
-  @media only screen and (max-width: 37.5em) {
-    padding-left: 5rem;
-    padding-right: 5rem;
-  }
-
   @media only screen and (max-width: 25em) {
     padding-left: 3rem;
     padding-right: 3rem;
