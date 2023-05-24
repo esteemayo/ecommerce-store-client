564cd3c0feaab676b00ad15a516b43d339e38702
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-24 05:10:28 +0100

    fix: modify

--- src/components/WishlistCard.jsx
+++ src/components/WishlistCard.jsx
@@ -147,6 +147,11 @@ const ImageContainer = styled.div`
     width: 8rem;
     height: 8rem;
     object-fit: cover;
+
+    @media only screen and (max-width: 18.75em) {
+      width: 6.5rem;
+      height: 6.5rem;
+    }
   }
 `;
 
