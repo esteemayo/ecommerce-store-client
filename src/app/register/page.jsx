516d2e8858eef88033a73617ce4c0c6aa5a6091e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-25 22:08:01 +0100

    fix: modify

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -235,7 +235,7 @@ const FormInput = styled.input`
   transition: all 0.3s ease;
 
   &:focus {
-    background-color: var(--clr-white);
+    background-color: transparent;
   }
 
   &::-webkit-input-placeholder {
