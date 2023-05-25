63654c045009427be7cc1143cde4059ce5ccb6d2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-25 17:34:01 +0100

    fix: add dark/light mode

--- src/components/ProductFilter.jsx
+++ src/components/ProductFilter.jsx
@@ -153,6 +153,7 @@ const Label = styled.label`
   display: inline-block;
   text-transform: capitalize;
   font-size: 1.6rem;
+  color: ${({ theme }) => theme.textFilterLabel};
   letter-spacing: 3px;
   margin-bottom: 0.5rem;
 `;
