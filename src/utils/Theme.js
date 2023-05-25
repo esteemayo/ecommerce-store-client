1086bf86fed3e05e3126546372a822515d9c0408
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-25 05:17:43 +0100

    fix: modify

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -21,7 +21,7 @@ export const darkTheme = {
   textRevArrowBtn: '#e2e4e4',
   bgScroll: '#121212',
   textScroll: '#d6d9db',
-  boxScroll: '0 0.4rem 1.2rem rgba(0, 0, 0, 0.04)',
+  boxScroll: '0 0.4rem 1.2rem rgba(136, 135, 135, 0.7)',
 };
 
 export const lightTheme = {
