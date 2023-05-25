61d539402bd332d5109b4fc73b74efb3227125f5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-25 21:53:07 +0100

    fix: add dark/light mode

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -162,7 +162,7 @@ const Wrapper = styled.div`
   width: 40rem;
   padding: 2rem;
   border-radius: 1rem;
-  box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
+  box-shadow: ${({ theme }) => theme.boxForm};
   -webkit-box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
   -moz-box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.25);
 
