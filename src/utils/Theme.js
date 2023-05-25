8a582c4a326ebb00a73c369525dd674a3c4e834c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-25 04:08:04 +0100

    feat: change color value

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -10,7 +10,7 @@ export const darkTheme = {
   textProdCard: '#d6d9db',
   star: '#89898a',
   textBtn: '#cfd0d1',
-  textPrice: '#cfd0d1',
+  textPrice: '#9a9b9c',
 };
 
 export const lightTheme = {
