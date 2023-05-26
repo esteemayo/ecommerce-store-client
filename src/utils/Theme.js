06ee43da3de88d17647ffce8668206edcf4dd9b3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-26 03:44:43 +0100

    fix: modify

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -59,7 +59,7 @@ export const darkTheme = {
   textRemoveCart: '#797979',
   cartBorder: '#111111',
   bgCartBtn: '#121212',
-  boxOverlay: 'rgba(0, 0, 0, 0.5)',
+  boxOverlay: 'rgba(47, 46, 46, 0.5)',
 };
 
 export const lightTheme = {
