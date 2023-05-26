fc316f11255caf679ede83b958261ffdb71b6669
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-26 03:19:18 +0100

    fix: add dark/light mode

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -69,7 +69,7 @@ const Container = styled.article`
   line-height: 1.12;
   
   &:not(:last-of-type) {
-    border-bottom: 1px solid #e2e9e7;
+    border-bottom: 1px solid ${({ theme }) => theme.cartBorder};
     margin-bottom: 1.25rem;
   }
 `;
