65feb63c58e7861c082f424f5c4d5c090190e785
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-26 01:26:50 +0100

    fix: modify

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -98,6 +98,6 @@ export const lightTheme = {
   inputOut: '#eeeeee',
   boxForm: '0 2rem 4rem rgba(145, 143, 143, 0.25)',
   inputCaret: 'var(--caret-light)',
-  uploadInput: '#89a8a0',
+  fileInput: '#89a8a0',
   textFile: '#89a8a0',
 };
