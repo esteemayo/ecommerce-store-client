72fb541488fed49cb6963c03665c98a1d943f98e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-26 05:02:33 +0100

    fix: add dark/light mode

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -166,7 +166,7 @@ const Button = styled.button`
   font-size: 1.5rem;
   padding: 1rem 0.5rem;
   background-color: transparent;
-  color: var(--clr-secondary-green);
+  color: ${({ theme }) => theme.textEmptyCartBtn};
   border: 2px solid var(--clr-primary-green);
   border-radius: 0.5rem;
   outline-color: #85beae;
