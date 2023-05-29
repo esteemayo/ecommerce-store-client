d57662fab8818bcd7f023d72bb0f821ad87d4a49
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-29 16:07:04 +0100

    fix: add dark mode

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -200,7 +200,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
   width: 40rem;
   padding: 2rem 4rem;
-  background-color: var(--clr-white);
+  background-color: ${({ theme }) => theme.bgModal};
   border-radius: 1.5rem;
   box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.1);
   -webkit-box-shadow: 0 2rem 4rem rgba(145, 143, 143, 0.1);
