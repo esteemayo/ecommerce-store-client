274d594322e5a70f9e39fe2d0b97fc30b6432451
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-29 18:06:18 +0100

    fix: add dark/light mode

--- src/components/FileUploadModal.jsx
+++ src/components/FileUploadModal.jsx
@@ -199,6 +199,7 @@ const Input = styled.input`
 const UploadProgress = styled.span`
   text-transform: capitalize;
   font-size: 1.4rem;
+  color: ${({ theme }) => theme.text};
 `;
 
 const ButtonContainer = styled.div`
