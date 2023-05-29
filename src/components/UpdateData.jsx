bba0d88c47bd557cae07789ba2c9af1e334351a7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-29 19:13:27 +0100

    fix: handle input validation

--- src/components/UpdateData.jsx
+++ src/components/UpdateData.jsx
@@ -23,6 +23,11 @@ const UpdateData = ({ onCancel }) => {
 
     if (email.trim() === '') {
       errors.email = 'Please enter your new email address';
+    } else {
+      const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)*[a-zA-Z]{2,9})$/;
+      if (!email.match(regEx)) {
+        errors.email = 'Email must be a valid email address';
+      }
     }
 
     if (currentPassword === '') {
