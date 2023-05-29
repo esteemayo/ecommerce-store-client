146626bd341707ff9b0c5e522b317a871c49e1c7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-29 17:48:05 +0100

    feat: add color value

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -159,4 +159,5 @@ export const lightTheme = {
   textWlCard: '#999999',
   boxWlCard: '2px 0.4rem 1rem 1px rgba(201, 201, 201, 0.25)',
   cartSelected: '#89a8a0',
+  textAccBtn: 'var(--clr-primary-green)',
 };
