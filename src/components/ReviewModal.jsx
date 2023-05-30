d36c6142a31f789c1bbbe7f62e6524aa082acfc7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-30 18:27:00 +0100

    fix: add dark/light mode

--- src/components/ReviewModal.jsx
+++ src/components/ReviewModal.jsx
@@ -104,7 +104,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
   width: 40rem;
   padding: 2rem 4rem;
-  background-color: var(--clr-white);
+  background-color: ${({ theme }) => theme.bgModal};
   border-radius: 1.5rem;
 
   @media only screen and (max-width: 25em) {
