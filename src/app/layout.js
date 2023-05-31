c78353cc7a9412ee10d790d13b419fe1b942f166
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-31 02:02:24 +0100

    fix: change component' name

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ import Footer from '@/components/Footer';
 import { persistor, store } from '@/store/store';
 import Sidebar from '@/components/Sidebar';
 import ScrollToTop from '@/components/ScrollToTop';
-import Darkmode from '@/components/Darkmode';
+import AppTheme from '@/components/Darkmode';
 
 import './globals.scss';
 
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
       <body>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
-            <Darkmode>
+            <AppTheme>
               <div className='container'>
                 <Navbar />
                 <Submenu />
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
                 <Footer />
                 <ScrollToTop />
               </div>
-            </Darkmode>
+            </AppTheme>
           </PersistGate>
         </Provider>
       </body>
