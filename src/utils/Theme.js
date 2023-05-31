73a4f88384596825f8f7feafa4ba8014cdd4ab19
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-31 01:33:22 +0100

    fix: add color value

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -79,6 +79,7 @@ export const darkTheme = {
   boxWlCard: '2px 0.4rem 1rem 1px rgba(27, 27, 27, 0.25)',
   cartSelected: '#252424',
   sizeHover: '#131313',
+  sizeHovModal: '#131313',
   textAccBtn: '#d6d9db',
   textDelHeader: '#e2e4e4',
   textProdHeader: '#e2e4e4',
