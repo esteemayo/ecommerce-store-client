9246494b30dcc50116221e1e0f0bce70913a09cf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-05-31 16:12:43 +0100

    fix: add environment variables

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   images: {
     domains: ['images.pexels.com', 'images.unsplash.com']
   },
+  env: {
+    NEXT_PUBLIC_DEV_API_URL: process.env.NEXT_PUBLIC_DEV_API_URL,
+    NEXT_PUBLIC_PROD_API_URL: process.env.NEXT_PUBLIC_PROD_API_URL,
+  }
 }
 
 module.exports = nextConfig
