f9fd9381197b3c7acd08d34980dd6a3a9319f33f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-02 18:36:01 +0100

    fix: modify

--- src/app/admin/products/new/page.jsx
+++ src/app/admin/products/new/page.jsx
@@ -20,6 +20,7 @@ const NewProduct = () => {
   const [size, setSize] = useState([]);
   const [files, setFiles] = useState(null);
   const [tags, setTags] = useState([]);
+  const [color, setColor] = useState([]);
 
   const handleChange = ({ target: input }) => {
     const { name, value } = input;
