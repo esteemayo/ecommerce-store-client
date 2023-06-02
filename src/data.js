287ddf44e4f5564b8483330871bac9e3de7b2e96
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-02 01:59:40 +0100

    fix: modify data

--- src/data.js
+++ src/data.js
@@ -749,7 +749,7 @@ export const storeProducts = [
     id: 11,
     name: 'Native wears',
     desc: 'Let the rain fall, you\'ve come prepared. Introducing The Stanley, our newest Smart Layer rain jacket. Made with recycled polyester, it features a regular fit and a waist length. This coat has a 10k/10k water resistant and breathability rating, protecting you from heavy rainfalls while keeping your body temperature on point. It achieves this rating because of its 2-layer fabric with different capabilities Better yet, this coat has an extra layer of protection made with a PFC-free C0 (carbon-free) DWR (Durable Water Repellent) with an 80/10 rating that ensures efficiency even after multiple washes. 2-way mechanical stretch fabric. Hand pockets with recycled zip closure. Recycled zip closure at the centre front. Adjustable cord locks at the hood and hem for the perfect fit. 2-tone drawcord. Inside chest pocket. Inside mesh storage pocket. Inside pocket is made with shell fabric. Flag label at side seam. Small logo branding at the chest. Machine washable. Compatible with all Smart Layer base jackets.',
-    price: 120,
+    price: 115.2,
     priceDiscount: 4,
     numberInStock: 25,
     inStock: true,
@@ -787,6 +787,7 @@ export const storeProducts = [
     createdAt: '2023-02-02T16:43:48.757Z',
     updatedAt: '2023-02-02T16:43:48.757Z',
     slug: 'native-wears',
+    discount: 4.8,
   },
   {
     id: 12,
