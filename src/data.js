59818141d1a50013a55889b8d4d87346144e6d9b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-06 03:27:22 +0100

    fix: change item title

--- src/data.js
+++ src/data.js
@@ -175,7 +175,7 @@ export const sliderItems = [
     id: 3,
     url: '/products',
     img: '/img/hero-2.jpg',
-    title: 'LOUNGEWEAR LOVE',
+    title: 'SUMMER SALE',
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: 'fbf0f4',
   },
