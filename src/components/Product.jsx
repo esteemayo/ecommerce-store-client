34714c99d81798d53bd0f77dde4bbdfe10f529c0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-07 16:00:17 +0100

    fix: modify

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -626,8 +626,8 @@ const Color = styled.button`
   display: block;
   width: 2.2rem;
   height: 2.2rem;
-  background-color: ${({ color, darkMode }) => darkMode && color === 'black' ? '#111' : color};
-  background-color: ${({ color, darkMode }) => !darkMode && color === 'white' && '#f9f9f9'};
+  background-color: ${({ color, mode }) => mode && color === 'black' ? '#111' : color};
+  background-color: ${({ color, mode }) => !mode && color === 'white' && '#f9f9f9'};
   border-radius: 50%;
   outline: 1px solid ${({ theme, selected }) => selected ? theme.cartSelected : 'transparent'};
   outline-offset: 2px;
