1cb55dd1b66980a7ff34dc21c692243762a8bb91
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-07 03:23:33 +0100

    fix: modify

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,7 +5,6 @@ import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useDispatch, useSelector } from 'react-redux';
 import { useCallback, useEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { NumericFormat } from 'react-number-format';
 
 import StarRating from '@/components/StarRating';
