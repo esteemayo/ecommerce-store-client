17855c441b47b85504de94809b45400eaf3852f0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-07 16:25:47 +0100

    fix: modify

--- src/app/wishlists/page.jsx
+++ src/app/wishlists/page.jsx
@@ -7,7 +7,7 @@ import { wishlists } from '@/data';
 import { closeSubmenu } from '@/features/submenu/submenuSlice';
 import WishlistCard from '@/components/WishlistCard';
 import WishlistHeader from '@/components/WishlistHeader';
-import CartModal from '@/components/CartModal';
+import CartModal from '@/components/modals/CartModal';
 
 const WishLists = () => {
   const dispatch = useDispatch();
