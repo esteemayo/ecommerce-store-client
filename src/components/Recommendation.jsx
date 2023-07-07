0fd522b62e302ed8d58b777627b4020fa180c699
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-07 04:14:13 +0100

    fix: modify

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -34,7 +34,7 @@ const Recommendation = () => {
                 </ProductContainer>
               </Link>
             </ProductWrapper>
-          )
+          );
         })}
       </Wrapper>
     </Container>
