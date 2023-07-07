20c943da76782548029ac3bf82aaf17374d639df
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-07 16:55:15 +0100

    fix: change file directory

---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+import Header from '../Header';
+import Category from './Category';
+
+const Categories = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Header title='Shop by category' />
+        <Category />
+      </Wrapper>
+    </Container>
+  );
+}
+
+const Container = styled.section`
+  width: 100vw;
+  background-color: ${({ theme }) => theme.bg};
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  max-width: 120rem;
+  margin: 0 auto;
+  padding: 10rem 0;
+
+  @media only screen and (max-width: 64em) {
+    max-width: 100rem;
+    padding: 8rem 0;
+  }
+
+  @media only screen and (max-width: 59.375em) {
+    max-width: 80rem;
+  }
+
+  @media only screen and (max-width: 50em) {
+    max-width: 70rem;
+  }
+
+  @media only screen and (max-width: 37.5em) {
+    padding-left: 5rem;
+    padding-right: 5rem;
+  }
+
+  @media only screen and (max-width: 25em) {
+    padding-left: 3rem;
+    padding-right: 3rem;
+  }
+
+  @media only screen and (max-width: 23.75em) {
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+`;
+
+export default Categories;
