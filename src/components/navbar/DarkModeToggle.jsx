30f0c77c3fe4c1da6d9549cd695bdf7a84171864
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-08 03:15:41 +0100

    fix: rename file

---
+++ src/components/navbar/DarkModeToggle.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const DarkModeToggle = () => {
+  return (
+    <Container>DarkModeToggle</Container>
+  );
+}
+
+const Container = styled.div`
+  
+`;
+
+export default DarkModeToggle;
