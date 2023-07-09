3f583399f9ccb6a6072e6fe828477248ae6618b6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-10 00:07:48 +0100

    fix: modify

--- src/components/navbar/CartQuantity.jsx
+++ src/components/navbar/CartQuantity.jsx
@@ -2,6 +2,7 @@
 
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
 import { faBagShopping } from '@fortawesome/free-solid-svg-icons';
 
 const CartQuantity = () => {
