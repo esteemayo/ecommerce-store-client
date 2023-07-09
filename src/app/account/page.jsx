00d803724234a37b31c99df5c651cd0bf713afaa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-09 17:46:15 +0100

    fix: add css variable

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -420,7 +420,7 @@ const FileButton = styled.button`
     120deg,
     transparent 0%,
     transparent 50%,
-    #89a8a0 50%
+    var(--clr-tertiary-green) 50%
   );
   background-size: 220%;
   border-radius: 0.5rem;
