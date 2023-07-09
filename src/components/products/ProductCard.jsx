5146ec6f12798cc1be2989c52d07d6135f2407b2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-09 17:51:15 +0100

    fix: add css variable

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -268,7 +268,7 @@ const Button = styled.button`
     120deg,
     transparent 0%,
     transparent 50%,
-    #89a8a0 50%
+    var(--clr-tertiary-green) 50%
   );
   background-size: 220%;
   outline-color: #85beae;
