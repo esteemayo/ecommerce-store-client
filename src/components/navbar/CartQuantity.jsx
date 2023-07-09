8ec5afd5c00b6b4d7102f1bd65156570f8a79754
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-10 00:09:37 +0100

    fix: add useSelector react-redux hook

--- src/components/navbar/CartQuantity.jsx
+++ src/components/navbar/CartQuantity.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 import { faBagShopping } from '@fortawesome/free-solid-svg-icons';
+import { useSelector } from 'react-redux';
 
 const CartQuantity = () => {
   return (
