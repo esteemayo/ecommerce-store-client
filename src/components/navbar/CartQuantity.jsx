7876de7ac9d1448b19c81fe6712a156210bc5f9c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-10 00:07:12 +0100

    fix: add faBagShopping icon

--- src/components/navbar/CartQuantity.jsx
+++ src/components/navbar/CartQuantity.jsx
@@ -3,6 +3,7 @@
 import styled from 'styled-components';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBagShopping } from '@fortawesome/free-solid-svg-icons';
 
 const CartQuantity = () => {
   return (
