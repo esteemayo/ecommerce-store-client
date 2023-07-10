6271bbe1c52c95b40f52c97026af0cbfcb8e3c5a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-10 15:58:30 +0100

    fix: modify

--- src/components/navbar/ToggleButton.jsx
+++ src/components/navbar/ToggleButton.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useDispatch } from 'react-redux';
 
 const ToggleButton = ({ icon: Icon, onClick }) => {
