4d49a8f9937b8951e09a4343794f51f5738b9e94
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-10 17:20:16 +0100

    fix: add media query

--- src/components/footer/SocialIcon.jsx
+++ src/components/footer/SocialIcon.jsx
@@ -14,6 +14,10 @@ const Container = styled.div`
   justify-content: flex-start;
   gap: 1rem;
   margin-top: 1rem;
+
+  @media only screen and (max-width: 26.875em) {
+    margin-top: 0.7rem;
+  }
 `;
 
 export default SocialIcon;
