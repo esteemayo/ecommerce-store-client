12edef633da3ae2060f052ceaa5a8e6018159951
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-10 15:55:38 +0100

    fix: add useDispatch react-redux hook

--- src/components/navbar/ToggleButton.jsx
+++ src/components/navbar/ToggleButton.jsx
@@ -2,6 +2,7 @@
 
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useDispatch } from 'react-redux';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const ToggleButton = () => {
