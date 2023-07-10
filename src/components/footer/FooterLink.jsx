8da4eeec20eb58f9e6593c7b3c5505709b1219c3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-11 00:37:20 +0100

    fix: add media query

--- src/components/footer/FooterLink.jsx
+++ src/components/footer/FooterLink.jsx
@@ -41,6 +41,10 @@ const LinkItem = styled.li`
   font-size: 1.4rem;
   color: var(--clr-white);
   outline-color: var(--clr-primary-green);
+
+  @media only screen and (max-width: 37.5rem) {
+    font-size: 1.57rem;
+  }
 `;
 
 export default FooterLink;
