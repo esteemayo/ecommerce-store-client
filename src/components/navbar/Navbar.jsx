033055bbb61ffe9a3b94bb009206e81e368d074c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-10 16:03:01 +0100

    fix: refactor code

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -123,31 +123,6 @@ const LogoBox = styled.div`
   justify-content: center;
 `;
 
-const ToggleButton = styled.button`
-  display: inline-block;
-  border: none;
-  width: 4rem;
-  height: 4rem;
-  background-color: transparent;
-  color: var(--clr-white);
-  margin-right: 2rem;
-  transition: all 0.5s ease;
-
-  &:hover {
-    transform: rotate(90deg);
-  }
-
-  svg {
-    width: 3rem;
-    height: 3rem;
-    fill: inherit;
-  }
-
-  @media only screen and (min-width:50em){
-    display: none;
-  }
-`;
-
 const ListContainer = styled.ul`
   display: none;
 
