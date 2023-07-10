faa26766b399dcd4c86334c1e948ba3e1091bfda
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-10 15:54:44 +0100

    fix: add faBars icon

--- src/components/navbar/ToggleButton.jsx
+++ src/components/navbar/ToggleButton.jsx
@@ -6,7 +6,9 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const ToggleButton = () => {
   return (
-    <Button type='button'>ToggleButton</Button>
+    <Button type='button'>
+      <FontAwesomeIcon icon={faBars} />
+    </Button>
   );
 }
 
