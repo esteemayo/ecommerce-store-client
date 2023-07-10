5e5693865b701c228b8f76a15b6a1b511bc3613f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-10 17:19:00 +0100

    fix: add a div element tag

--- src/components/footer/SocialIcon.jsx
+++ src/components/footer/SocialIcon.jsx
@@ -2,10 +2,14 @@
 
 import styled from 'styled-components';
 
-const SocialIcon = () => {
+const SocialIcon = ({ data }) => {
   return (
     <div>SocialIcon</div>
   );
 }
+
+const Container = styled.div`
+  
+`;
 
 export default SocialIcon;
