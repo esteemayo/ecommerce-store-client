29714bb2fc718de70f6e96b8b1b635e0d0f0e3e8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-10 14:39:16 +0100

    fix: modify

--- src/components/navbar/LoginButton.jsx
+++ src/components/navbar/LoginButton.jsx
@@ -52,7 +52,7 @@ const Button = styled.button`
   svg {
     position: absolute;
     top: 3px;
-    right: -1rem;
+    right: 1rem;
     font-size: 1.25rem;
     fill: currentColor;
     transition: 150ms cubic-bezier(0.22, 0.61, 0.36, 1);
