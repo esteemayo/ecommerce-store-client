feedbaa62d8da0b897d670d1f1e27fbc168b1031
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-10 16:54:51 +0100

    fix: refactor code

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -46,9 +46,11 @@ const Footer = () => {
                 {discoverLinks.map((link) => {
                   const { id, url, text } = link;
                   return (
-                    <ListItem key={id}>
-                      <Link href={url} passHref>{text}</Link>
-                    </ListItem>
+                    <MenuItem
+                      key={id}
+                      url={url}
+                      label={text}
+                    />
                   );
                 })}
               </ListContainer>
