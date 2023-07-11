c416db42146c02c800c22b0fe14b8bc5949238c4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-11 02:16:44 +0100

    fix: add ClientOnly component

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,7 @@ import AppThemeProvider from '@/providers/ThemeProvider';
 import { persistor, store } from '@/store/store';
 
 import './globals.scss';
+import ClientOnly from '@/components/ClientOnly';
 
 export default function RootLayout({ children }) {
   return (
