f8db95ffc19bee35c3edb353b45d5294a54e6f43
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-11 02:20:23 +0100

    fix: modify

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,6 +38,7 @@ export default function RootLayout({ children }) {
                   <Sidebar />
                 </ClientOnly>
                 {children}
+                <ClientOnly>
                   <Footer />
                   <ScrollToTop />
                   <Darkmode />
