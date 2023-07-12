6140489195fad9b8b42c867626b9baab64bfe254
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-12 02:16:55 +0100

    fix: refactor code

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -2,7 +2,7 @@
 
 import styled from 'styled-components';
 import Link from 'next/link';
-import { faArrowLeft, faArrowRight, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useDispatch, useSelector } from 'react-redux';
