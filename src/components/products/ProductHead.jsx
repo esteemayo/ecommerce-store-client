8129591b54936361051a3f6a97db2f0f97760622
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-12 14:23:21 +0100

    fix: add media query

--- src/components/products/ProductHead.jsx
+++ src/components/products/ProductHead.jsx
@@ -75,6 +75,10 @@ const Price = styled.p`
   text-decoration: line-through;
   font-size: 1.6rem;
   color: ${({ theme }) => theme.text};
+
+  @media only screen and (max-width: 18.75em) {
+    font-size: 1.5rem;
+  }
 `;
 
 const Discount = styled.p`
