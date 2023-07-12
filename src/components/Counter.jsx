9201f2d96be078ddd3d9175ae550a458b284e584
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-12 01:10:29 +0100

    fix: add h3 element tag

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,7 +4,9 @@ import styled from 'styled-components';
 
 const Counter = () => {
   return (
-    <Container>Counter</Container>
+    <Container>
+      <Heading>Quantity</Heading>
+    </Container>
   );
 }
 
