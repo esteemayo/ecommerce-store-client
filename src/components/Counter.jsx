482b59f82e7a4d8af95664ed434c054adf44862c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-12 01:16:15 +0100

    fix: add media query

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -28,6 +28,10 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   gap: 2rem;
+
+  @media only screen and (max-width: 37.5em) {
+    gap: 1.5rem;
+  }
 `;
 
 export default Counter;
