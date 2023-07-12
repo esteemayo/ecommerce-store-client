a7579c09bfb9aac002a511e8e73dcb223c30d924
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-12 14:27:46 +0100

    fix: add css styling

--- src/components/products/ProductHead.jsx
+++ src/components/products/ProductHead.jsx
@@ -129,6 +129,9 @@ const Message = styled.p`
 
 const Rating = styled.div`
   margin: 1rem 0 2rem 0;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
 `;
 
 const ReviewQuantity = styled.span`
