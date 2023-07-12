96d3ba40cd3facc94b7d25c1489af25f06e3472a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-12 17:10:16 +0100

    fix: add media query

--- src/components/products/ProductImage.jsx
+++ src/components/products/ProductImage.jsx
@@ -61,6 +61,10 @@ const Wrapper = styled.figure`
     object-fit: cover;
     border-radius: 0.5rem;
     cursor: pointer;
+
+    @media only screen and (max-width: 64em) {
+      width: 30rem;
+    }
   }
 `;
 
