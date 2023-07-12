8874a53cd5d626822a633c4d67255a353ce6c87f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-12 14:42:36 +0100

    fix: refactor code

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -400,17 +400,6 @@ const ProductWrapper = styled.div`
   }
 `;
 
-const SalePrice = styled.p`
-  font-weight: 500;
-  font-size: 1.6rem;
-  color: #fa4b21;
-  line-height: 1.3;
-
-  @media only screen and (max-width: 18.75em) {
-    font-size: 1.5rem;
-  }
-`;
-
 const Message = styled.p`
   color: ${({ theme }) => theme.text};
   margin-top: 1rem;
