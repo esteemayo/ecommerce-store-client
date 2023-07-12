fe9566c5740560595ceac72b06defe9183f15b6b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-12 16:57:13 +0100

    fix: add useCallback react hook

--- src/components/products/ProductImage.jsx
+++ src/components/products/ProductImage.jsx
@@ -3,6 +3,7 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { useCallback } from 'react';
 
 const ProductImage = () => {
   return (
