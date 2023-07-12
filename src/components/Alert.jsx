ebff7eccd614390513776bc2d652e4cb65b93bec
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-12 15:54:03 +0100

    fix: add a span element tag

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -6,9 +6,21 @@ import { useEffect, useState } from 'react';
 const Alert = () => {
   const [alert, setAlert] = useState(false);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
   return (
     <div>Alert</div>
   );
 }
+
+const Message = styled.span`
+  
+`;
 
 export default Alert;
