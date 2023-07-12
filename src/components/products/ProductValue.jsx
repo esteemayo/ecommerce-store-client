571f868c4ebc2fcb875a643306e6d901b2f1d6cf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-12 16:22:59 +0100

    fix: modify

--- src/components/products/ProductValue.jsx
+++ src/components/products/ProductValue.jsx
@@ -3,10 +3,23 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 
-const ProductValue = () => {
+const ProductValue = ({ items, mode }) => {
   return (
     <Container>
-      <ValueAdd></ValueAdd>
+      <ValueAdd mode={mode}>
+        {items.map((item) => {
+          const { id, desc, img } = item;
+          return (
+            <Image
+              key={id}
+              src={img}
+              width={24}
+              height={24}
+              alt=''
+            />
+          );
+        })}
+      </ValueAdd>
     </Container>
   );
 }
