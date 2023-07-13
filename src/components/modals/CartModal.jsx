4846956a4781ab2a0f0f76dbf6b3a542b5f17e0c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-13 02:37:38 +0100

    fix: refactor code

--- src/components/modals/CartModal.jsx
+++ src/components/modals/CartModal.jsx
@@ -462,11 +462,4 @@ const Button = styled.button`
   }
 `;
 
-const SuccessMessage = styled.span`
-  display: block;
-  font-size: 1.4rem;
-  color: #409384;
-  text-align: center;
-`;
-
 export default CartModal;
