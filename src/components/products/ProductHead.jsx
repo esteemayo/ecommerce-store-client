d7fdb4cb6e9627c2000259c4e71f96337e42009e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-13 16:34:58 +0100

    fix: modify

--- src/components/products/ProductHead.jsx
+++ src/components/products/ProductHead.jsx
@@ -110,7 +110,7 @@ const Discount = styled.p`
 `;
 
 const TotalPrice = styled.p`
-  font-weight: 500;
+  font-weight: ${({ modal }) => modal ? '400' : '500'};
   font-size: 1.6rem;
   color: #fa4b21;
   line-height: 1.3;
