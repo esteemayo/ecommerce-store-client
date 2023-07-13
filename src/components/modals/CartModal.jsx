afbcaa5144fc2c7b54e9cb0474afffe0e5bccff9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-13 02:44:31 +0100

    fix: refactor code

--- src/components/modals/CartModal.jsx
+++ src/components/modals/CartModal.jsx
@@ -46,11 +46,6 @@ const CartModal = ({ product, isModalOpen, closeModal, setIsSelectedProduct }) =
     setSelectedSize(size);
   }, []);
 
-  const handleQuantity = useCallback((type) => {
-    type === 'dec' && quantity > 1 && setQuantity((prev) => prev - 1);
-    type === 'inc' && setQuantity((prev) => prev + 1);
-  }, [quantity]);
-
   const handleReset = useCallback(() => {
     setSize(null);
     setColor(null);
