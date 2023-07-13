502fc499e43d880fd94921e34d7737fead8fc0de
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-13 01:59:07 +0100

    fix: refactor code

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -181,49 +181,4 @@ const Hr = styled.hr`
   background-color: ${({ theme }) => theme.cartModalBorder};
 `;
 
-const Button = styled.button`
-  display: inline-block;
-  text-align: center;
-  font-size: 1.8rem;
-  width: 100%;
-  padding: 2rem 1rem;
-  background-color: ${({ theme }) => theme.bgProdBtn};
-  color: var(--clr-white);
-  background-image: linear-gradient(
-    120deg,
-    transparent 0%,
-    transparent 50%,
-    #333 50%
-  );
-  background-size: 220%;
-  border: 1px solid var(--clr-black);
-  border-radius: 0.5rem;
-  outline-color: #777;
-  line-height: 1.3em;
-  margin: 3rem 0;
-  cursor: pointer;
-  transition: all 0.3s;
-
-  @media only screen and (max-width: 59.375em) {
-    padding-top: 1.75rem;
-    padding-bottom: 1.75rem;
-  }
-
-  @media only screen and (max-width: 37.5em), only screen and (hover: none) {
-    font-size: 1.7rem;
-    padding-top: 1.6rem;
-    padding-bottom: 1.6rem;
-  }
-
-  @media only screen and (max-width: 18.75em) {
-    font-size: 1.6rem;
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-  }
-
-  &:hover {
-    background-position: 100%;
-  }
-`;
-
 export default Product;
