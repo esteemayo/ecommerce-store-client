f2002276899737b7f51342494d0539424861ae27
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-14 02:08:31 +0100

    fix: add css styling

--- src/components/submenu/MenuItem.jsx
+++ src/components/submenu/MenuItem.jsx
@@ -35,6 +35,13 @@ const ListItem = styled.li`
       color: ${({ theme }) => theme.textHeader};
     }
   }
+
+  & > * {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    outline-color: #f5f5f5;
+  }
 `;
 
 export default MenuItem;
