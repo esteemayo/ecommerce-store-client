ce99c84c9ea2600fee6a4971ae27d0c0ca081927
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-14 02:30:23 +0100

    fix: add state variables

--- src/hooks/useAddToCart.js
+++ src/hooks/useAddToCart.js
@@ -6,6 +6,13 @@ import { useCallback, useState } from 'react';
 const useAddToCart = () => {
   const dispatch = useDispatch();
 
+  const [color, setColor] = useState(null);
+  const [size, setSize] = useState(null);
+  const [isSelected, setIsSelected] = useState(null);
+  const [quantity, setQuantity] = useState(1);
+  const [selectedSize, setSelectedSize] = useState(null);
+  const [alert, setAlert] = useState(false);
+
   return (
     <div>useAddToCart</div>
   );
