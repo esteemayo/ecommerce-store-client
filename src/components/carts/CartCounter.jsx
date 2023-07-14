c04bedd7e61220452efe663b671817d935aff852
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-14 16:50:51 +0100

    fix: add icons

--- src/components/carts/CartCounter.jsx
+++ src/components/carts/CartCounter.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import styled from 'styled-components';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const CartCounter = () => {
