8fd20cc22a78be925cbecdb035f510c69807f456
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-14 16:23:21 +0100

    fix: modify

--- src/components/carts/CartInfo.jsx
+++ src/components/carts/CartInfo.jsx
@@ -4,7 +4,14 @@ import styled from 'styled-components';
 import Image from 'next/image';
 import { NumericFormat } from 'react-number-format';
 
-const CartInfo = ({ color, name, images, price, size, onAction }) => {
+const CartInfo = ({
+  color,
+  name,
+  images,
+  price,
+  size,
+  onAction,
+}) => {
   return (
     <Container>
       <ImageContainer>
