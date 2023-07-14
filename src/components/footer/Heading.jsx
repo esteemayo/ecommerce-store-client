d3f9f1d09bae451a571d93b744a188ba9512ae26
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-14 01:26:29 +0100

    fix: add type checking

--- src/components/footer/Heading.jsx
+++ src/components/footer/Heading.jsx
@@ -7,6 +7,11 @@ const Heading = ({ title, small }) => {
   return <Header small={small}>{title}</Header>;
 }
 
+Heading.propTypes = {
+  title: PropTypes.string.isRequired,
+  small: PropTypes.bool,
+};
+
 const Header = styled.h4`
   font-weight: ${({ small }) => small ? '300' : '500'};
   font-size: ${({ small }) => small ? '1.4rem' : '2rem'};
