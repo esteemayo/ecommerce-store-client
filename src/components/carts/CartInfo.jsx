2535e5a862a33bfefb676c29b4732a3ddb5522cb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-14 04:01:22 +0100

    fix: add markup

--- src/components/carts/CartInfo.jsx
+++ src/components/carts/CartInfo.jsx
@@ -4,10 +4,17 @@ import styled from 'styled-components';
 import Image from 'next/image';
 import { NumericFormat } from 'react-number-format';
 
-const CartInfo = () => {
+const CartInfo = ({ images }) => {
   return (
     <Container>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image
+          src={images[0]}
+          width={120}
+          height={120}
+          alt=''
+        />
+      </ImageContainer>
     </Container>
   );
 }
