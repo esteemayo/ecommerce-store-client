74a65bc1043c9abda5392f649914df512c149d25
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-14 16:17:00 +0100

    fix: add a div element tag

--- src/components/carts/CartInfo.jsx
+++ src/components/carts/CartInfo.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import Image from 'next/image';
 import { NumericFormat } from 'react-number-format';
 
-const CartInfo = ({ color, name, images, price }) => {
+const CartInfo = ({ color, name, images, price, size }) => {
   return (
     <Container>
       <ImageContainer>
@@ -32,6 +32,11 @@ const CartInfo = ({ color, name, images, price }) => {
           <Label>Color:</Label>
           <Color color={color}>{color}</Color>
         </ColorContainer>
+        {size && (
+          <SizeContainer>
+            <Label>Size:</Label>
+          </SizeContainer>
+        )}
       </CartDetails>
     </Container>
   );
