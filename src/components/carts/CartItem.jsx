ff343f33684a627d0490ec4d0ecef3dd6a7ddec6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-14 17:06:39 +0100

    fix: refactor

--- src/components/carts/CartItem.jsx
+++ src/components/carts/CartItem.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import styled from 'styled-components';
-import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { useCallback } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useDispatch } from 'react-redux';
