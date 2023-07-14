10083012b12a9774661ca4b5024a79c041f7a0a2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-14 02:14:50 +0100

    fix: refactor code

--- src/components/submenu/Submenu.jsx
+++ src/components/submenu/Submenu.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import styled from 'styled-components';
-import Link from 'next/link';
 import { useEffect, useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
