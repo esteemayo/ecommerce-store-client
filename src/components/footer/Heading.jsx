ba069097157c95404d9ccedaa19ff23a637db6cc
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-14 01:30:11 +0100

    fix: modify

--- src/components/footer/Heading.jsx
+++ src/components/footer/Heading.jsx
@@ -7,11 +7,6 @@ const Heading = ({ title, small }) => {
   return <Header small={small}>{title}</Header>;
 }
 
-Heading.propTypes = {
-  title: PropTypes.string.isRequired,
-  small: PropTypes.bool,
-};
-
 const Header = styled.h4`
   font-weight: ${({ small }) => small ? '300' : '500'};
   font-size: ${({ small }) => small ? '1.4rem' : '2rem'};
@@ -25,5 +20,10 @@ const Header = styled.h4`
     margin-bottom: ${({ small }) => !small && '1.5rem'};
   }
 `;
+
+Heading.propTypes = {
+  title: PropTypes.string.isRequired,
+  small: PropTypes.bool,
+};
 
 export default Heading;
