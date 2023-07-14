e5b6297f434ef90b6b7e9c0ab90630dc61cfd4e7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-14 17:06:54 +0100

    fix: refactor

--- src/components/carts/CartItem.jsx
+++ src/components/carts/CartItem.jsx
@@ -2,7 +2,6 @@
 
 import styled from 'styled-components';
 import { useCallback } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useDispatch } from 'react-redux';
 
 import CartInfo from './CartInfo';
