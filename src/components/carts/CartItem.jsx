a5b9792c946c23d55427859e1a6bae89b6ca97a4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-14 16:39:58 +0100

    fix: refactor

--- src/components/carts/CartItem.jsx
+++ src/components/carts/CartItem.jsx
@@ -71,41 +71,6 @@ const Left = styled.div`
   gap: 1rem;
 `;
 
-const ImageContainer = styled.div`
-  width: 12rem;
-  height: 12rem;
-  background-color: #ede9e6;
-  border-radius: 3px;
-
-  @media only screen and (max-width: 25em) {
-    width: 10rem;
-    height: 10rem;
-  }
-
-  @media only screen and (max-width: 18.75em) {
-    width: 8rem;
-    height: 8rem;
-  }
-
-  img {
-    width: 12rem;
-    height: 12rem;
-    display: block;
-    object-fit: cover;
-    border-radius: 3px;
-
-    @media only screen and (max-width: 25em) {
-      width: 10rem;
-      height: 10rem;
-    }
-
-    @media only screen and (max-width: 18.75em) {
-      width: 8rem;
-      height: 8rem;
-    }
-  }
-`;
-
 const Right = styled.div`
   flex: 1;
   display: flex;
