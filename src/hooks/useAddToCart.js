fe2f15252255872700e2c6efad00aa08edc4f5b4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-14 02:28:34 +0100

    fix: modify

--- src/hooks/useAddToCart.js
+++ src/hooks/useAddToCart.js
@@ -4,6 +4,8 @@ import { useDispatch } from 'react-redux';
 import { useCallback, useState } from 'react';
 
 const useAddToCart = () => {
+  const dispatch = useDispatch();
+
   return (
     <div>useAddToCart</div>
   );
