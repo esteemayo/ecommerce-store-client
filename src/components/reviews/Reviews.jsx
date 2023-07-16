a082c54f38729666b9630c8bf3f2c6ced135cc8a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-17 00:28:32 +0100

    fix: refactor

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -155,11 +155,4 @@ const ReviewsWrapper = styled.div`
   }
 `;
 
-const ReviewCardContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 1rem;
-`;
-
 export default Reviews;
