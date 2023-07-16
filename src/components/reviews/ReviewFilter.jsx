eca6fa36f4956169e7a045f6336d28a9c69a3b55
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-16 14:54:04 +0100

    fix: add a div element tag

--- src/components/reviews/ReviewFilter.jsx
+++ src/components/reviews/ReviewFilter.jsx
@@ -4,7 +4,9 @@ import styled from 'styled-components';
 
 const ReviewFilter = () => {
   return (
-    <Container>ReviewFilter</Container>
+    <Container>
+      <Wrapper></Wrapper>
+    </Container>
   );
 }
 
