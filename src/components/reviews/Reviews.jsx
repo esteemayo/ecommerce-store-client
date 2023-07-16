54e29d928188b4f091a349ca14aab8e8c5c16064
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-17 00:05:09 +0100

    fix: refactor

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -153,25 +153,6 @@ const Heading = styled.h1`
   line-height: 3.2rem;
 `;
 
-const ReviewContainer = styled.div`
-  padding: 3rem 0 0.5rem 0;
-  display: flex;
-  flex-direction: column;
-
-  svg {
-    font-size: 3rem;
-    color: ${({ theme }) => theme.star};
-
-    @media only screen and (max-width: 26.875em) {
-      font-size: 2.65rem;
-    }
-
-    @media only screen and (max-width: 18.75em) {
-      font-size: 2.45rem;
-    }
-  }
-`;
-
 const ReviewsWrapper = styled.div`
   padding: 5rem 0 0.5rem 0;
   border-top: 1px solid ${({ theme }) => theme.cartBorder};
