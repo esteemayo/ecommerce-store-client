ee464b4682e94687b26071c4f094cb53084f7895
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-16 15:43:58 +0100

    fix: add ReviewFilter component

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -12,6 +12,7 @@ import StarRating from '../StarRating';
 import ReviewModal from '../modals/ReviewModal';
 
 import { reviewItems } from '@/data';
+import ReviewFilter from './ReviewFilter';
 
 const Reviews = () => {
   const [sort, setSort] = useState(null);
