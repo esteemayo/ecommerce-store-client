e29a972933216d12c2b98d0167df773ea119ef2f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-16 16:28:41 +0100

    fix: add css styling

--- src/components/reviews/ReviewHead.jsx
+++ src/components/reviews/ReviewHead.jsx
@@ -12,6 +12,11 @@ const Container = styled.div`
   padding: 3rem 0 0.5rem 0;
   display: flex;
   flex-direction: column;
+
+  svg {
+    font-size: 3rem;
+    color: ${({ theme }) => theme.star};
+  }
 `;
 
 export default ReviewHead;
