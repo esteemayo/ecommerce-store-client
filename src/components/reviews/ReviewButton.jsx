07672c361d330f3768b18348faf305bae3fe2176
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-16 14:09:01 +0100

    fix: style button element

--- src/components/reviews/ReviewButton.jsx
+++ src/components/reviews/ReviewButton.jsx
@@ -12,6 +12,8 @@ const Button = styled.button`
   border: none;
   display: inline-block;
   padding: 1rem;
+  background-color: ${({ theme }) => theme.bgRevBtn};
+  color:  ${({ theme }) => theme.textRevBtn};
 `;
 
 export default ReviewButton;
