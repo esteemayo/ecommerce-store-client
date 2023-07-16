6e5cfbebc2a249d0f2bd76a8202484bdb47742e2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-16 14:40:24 +0100

    fix: refactor

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -362,28 +362,6 @@ const ReviewCardContainer = styled.div`
   padding-top: 1rem;
 `;
 
-const EmptyReview = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 1.3rem;
-
-  svg {
-    font-size: 2.5rem;
-    fill: ${({ theme }) => theme.star};
-    margin-right: 0.5rem;
-
-    @media only screen and (max-width: 26.875em) {
-      font-size: 2.25rem;
-    }
-
-    @media only screen and (max-width: 18.75em) {
-      font-size: 2rem;
-    }
-  }
-`;
-
 const Message = styled.span`
   color: ${({ theme }) => theme.text};
 
