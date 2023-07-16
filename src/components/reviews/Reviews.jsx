37556ba01ccfc5c35ac4f205d587859df53afbad
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-16 15:57:44 +0100

    fix: modify

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -11,7 +11,6 @@ import StarRating from '../StarRating';
 import ReviewModal from '../modals/ReviewModal';
 
 import { reviewItems } from '@/data';
-import ReviewFilter from './ReviewFilter';
 
 const Reviews = () => {
   const [sort, setSort] = useState(null);
