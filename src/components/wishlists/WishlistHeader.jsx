777a486f857600ce6de3ab38447742cd0b2ad1b6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-17 02:33:01 +0100

    fix: refactor

--- src/components/wishlists/WishlistHeader.jsx
+++ src/components/wishlists/WishlistHeader.jsx
@@ -47,19 +47,6 @@ const Heading = styled.h1`
   }
 `;
 
-const DateTime = styled.time`
-  font-size: 1.5rem;
-  color: ${({ theme }) => theme.textWlHeader};
-
-  @media only screen and (max-width: 56.25em) {
-    font-size: 1.3rem;
-  }
-
-  @media only screen and (max-width: 18.75em) {
-    font-size: 1.2rem;
-  }
-`;
-
 const SendButton = styled.button`
   border: none;
   display: inline-block;
