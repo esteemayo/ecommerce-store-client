b2f1b41709944c257533b3c09062c083b963e680
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-17 18:03:57 +0100

    fix: add ClientOnly component

--- src/app/products/category/[category]/page.jsx
+++ src/app/products/category/[category]/page.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { useCallback, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
+import ClientOnly from '@/components/ClientOnly';
 import ProductList from '@/components/products/ProductList';
 import { closeSubmenu } from '@/features/submenu/submenuSlice';
 
