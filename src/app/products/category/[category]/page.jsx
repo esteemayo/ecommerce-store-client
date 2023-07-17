88b825f6015ee0dcb15cdb1ff00dcd65fa9bfab9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-17 18:04:16 +0100

    fix: add ClientOnly component

--- src/app/products/category/[category]/page.jsx
+++ src/app/products/category/[category]/page.jsx
@@ -6,6 +6,7 @@ import { useDispatch } from 'react-redux';
 
 import ClientOnly from '@/components/ClientOnly';
 import ProductList from '@/components/products/ProductList';
+
 import { closeSubmenu } from '@/features/submenu/submenuSlice';
 
 import { getUnique } from '@/utils';
@@ -73,44 +74,46 @@ const ProductCategory = ({ params }) => {
   });
 
   return (
-    <Container onMouseOver={() => dispatch(closeSubmenu())}>
-      <Wrapper>
-        <FilterContainer>
-          <FilterHeading>Filter by</FilterHeading>
-          <FilterWrapper>
-            <FilterLeft>
-              <Filter>
-                <Label htmlFor='color'>Color</Label>
-                <Select id='color' name='color' onChange={handleFilter}>
-                  {colors}
-                </Select>
-              </Filter>
-              <Filter>
-                <Label htmlFor='size'>Product size</Label>
-                <Select id='size' name='size' onChange={handleFilter}>
-                  {sizes}
-                </Select>
-              </Filter>
-            </FilterLeft>
-            <FilterRight>
-              <Filter>
-                <Label htmlFor='price'>Product price</Label>
-                <Select id='price' name='price' onChange={(e) => setSort(e.target.value)}>
-                  <Option value='newest'>Newest</Option>
-                  <Option value='asc'>Price (asc)</Option>
-                  <Option value='desc'>Price (desc)</Option>
-                </Select>
-              </Filter>
-            </FilterRight>
-          </FilterWrapper>
-        </FilterContainer>
-        {category ? (
-          <ProductList products={sortedProducts} />
-        ) : (
-          <ProductList products={products} />
-        )}
-      </Wrapper>
-    </Container>
+    <ClientOnly>
+      <Container onMouseOver={() => dispatch(closeSubmenu())}>
+        <Wrapper>
+          <FilterContainer>
+            <FilterHeading>Filter by</FilterHeading>
+            <FilterWrapper>
+              <FilterLeft>
+                <Filter>
+                  <Label htmlFor='color'>Color</Label>
+                  <Select id='color' name='color' onChange={handleFilter}>
+                    {colors}
+                  </Select>
+                </Filter>
+                <Filter>
+                  <Label htmlFor='size'>Product size</Label>
+                  <Select id='size' name='size' onChange={handleFilter}>
+                    {sizes}
+                  </Select>
+                </Filter>
+              </FilterLeft>
+              <FilterRight>
+                <Filter>
+                  <Label htmlFor='price'>Product price</Label>
+                  <Select id='price' name='price' onChange={(e) => setSort(e.target.value)}>
+                    <Option value='newest'>Newest</Option>
+                    <Option value='asc'>Price (asc)</Option>
+                    <Option value='desc'>Price (desc)</Option>
+                  </Select>
+                </Filter>
+              </FilterRight>
+            </FilterWrapper>
+          </FilterContainer>
+          {category ? (
+            <ProductList products={sortedProducts} />
+          ) : (
+            <ProductList products={products} />
+          )}
+        </Wrapper>
+      </Container>
+    </ClientOnly>
   );
 }
 
