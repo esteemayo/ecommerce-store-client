44412585f34ea0f2aee11540e81dfbed31156170
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-17 16:52:39 +0100

    fix: add useCallback react hook

--- src/components/products/FeaturedProducts.jsx
+++ src/components/products/FeaturedProducts.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import styled from 'styled-components';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import Header from '../Header';
 import ProductCard from './ProductCard';
