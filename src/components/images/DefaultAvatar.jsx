fc2bb6e42008667d55b91fe6a59794304a17ed12
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-17 01:04:04 +0100

    fix: modify

--- src/components/images/DefaultAvatar.jsx
+++ src/components/images/DefaultAvatar.jsx
@@ -2,9 +2,9 @@
 
 import styled from 'styled-components';
 
-const DefaultAvatar = () => {
+const DefaultAvatar = ({ name }) => {
   return (
-    <Container>DefaultAvatar</Container>
+    <Container>{name.charAt(0)}</Container>
   );
 }
 
