051dab69562afb5aba27ce3fb305cb8ff14a3919
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-18 01:25:27 +0100

    fix: add ClientOnly component

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -7,6 +7,7 @@ import Image from 'next/image';
 import { FaFacebookF } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 
+import ClientOnly from '@/components/ClientOnly';
 import { closeSubmenu } from '@/features/submenu/submenuSlice';
 
 const Login = () => {
