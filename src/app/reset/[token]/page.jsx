d514a2a41ff55e4b14b98902f10e9683ac7b9194
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-18 01:28:49 +0100

    fix: add ClientOnly component

--- src/app/reset/[token]/page.jsx
+++ src/app/reset/[token]/page.jsx
@@ -49,39 +49,41 @@ const ResetPassword = () => {
   }, []);
 
   return (
-    <Container onMouseOver={() => dispatch(closeSubmenu())}>
-      <ResetContainer>
-        <Wrapper>
-          <Header>Reset your password</Header>
-          <Form onSubmit={handleSubmit}>
-            <FormGroup>
-              <FormLabel htmlFor='password'>Password</FormLabel>
-              <FormInput
-                id='password'
-                type='password'
-                name='password'
-                ref={passwordRef}
-                placeholder='Enter your password'
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {errors.password && <ErrorMsg>{errors.password}</ErrorMsg>}
-            </FormGroup>
-            <FormGroup>
-              <FormLabel htmlFor='confirmPassword'>Confirm password</FormLabel>
-              <FormInput
-                id='confirmPassword'
-                type='password'
-                name='confirmPassword'
-                placeholder='Confirm your password'
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-              {errors.confirmPassword && <ErrorMsg>{errors.confirmPassword}</ErrorMsg>}
-            </FormGroup>
-            <FormButton type='submit'>Reset password</FormButton>
-          </Form>
-        </Wrapper>
-      </ResetContainer>
-    </Container>
+    <ClientOnly>
+      <Container onMouseOver={() => dispatch(closeSubmenu())}>
+        <ResetContainer>
+          <Wrapper>
+            <Header>Reset your password</Header>
+            <Form onSubmit={handleSubmit}>
+              <FormGroup>
+                <FormLabel htmlFor='password'>Password</FormLabel>
+                <FormInput
+                  id='password'
+                  type='password'
+                  name='password'
+                  ref={passwordRef}
+                  placeholder='Enter your password'
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {errors.password && <ErrorMsg>{errors.password}</ErrorMsg>}
+              </FormGroup>
+              <FormGroup>
+                <FormLabel htmlFor='confirmPassword'>Confirm password</FormLabel>
+                <FormInput
+                  id='confirmPassword'
+                  type='password'
+                  name='confirmPassword'
+                  placeholder='Confirm your password'
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                {errors.confirmPassword && <ErrorMsg>{errors.confirmPassword}</ErrorMsg>}
+              </FormGroup>
+              <FormButton type='submit'>Reset password</FormButton>
+            </Form>
+          </Wrapper>
+        </ResetContainer>
+      </Container>
+    </ClientOnly>
   );
 }
 
