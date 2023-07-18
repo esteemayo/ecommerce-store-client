e40e6c1665d3310df027c8ea5c551231a7246811
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-18 15:36:23 +0100

    fix: add useSelector react-redux hook

--- src/providers/CartTotalProvider.js
+++ src/providers/CartTotalProvider.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 const CartTotalProvider = () => {
   const dispatch = useDispatch();
+  const { cart } = useSelector((state) => ({ ...state.cart }));
 
   return (
     <div>CartTotalProvider</div>
