4083357f07306968fdb219899ecf9a6353cfe7a2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-18 16:25:11 +0100

    fix: import FontAwesomeIcon

--- src/components/submenu/MenuItem.jsx
+++ src/components/submenu/MenuItem.jsx
@@ -2,6 +2,7 @@
 
 import styled from 'styled-components';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 
 const MenuItem = ({ url, icon, label }) => {
