2dd49672ce3dac400634e94eed7dbd753e07ea1d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-18 01:15:23 +0100

    fix: add styling

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -21,6 +21,9 @@ const Container = styled.main`
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default EmptyState;
