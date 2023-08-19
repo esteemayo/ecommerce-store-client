ab449b00f090d3b6ab3b8756330540f4bb58bb72
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-19 02:24:17 +0100

    fix: modify

--- src/providers/ThemeProvider.js
+++ src/providers/ThemeProvider.js
@@ -4,10 +4,10 @@ import { useSelector } from 'react-redux';
 import { darkTheme, lightTheme } from '@/utils/Theme';
 
 const AppThemeProvider = ({ children }) => {
-  const { darkMode } = useSelector((state) => ({ ...state.darkMode }));
+  const { mode } = useSelector((state) => ({ ...state.darkMode }));
 
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+    <ThemeProvider theme={mode ? darkTheme : lightTheme}>
       {children}
     </ThemeProvider>
   );
