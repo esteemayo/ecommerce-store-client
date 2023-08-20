101ac0fa03adeb1b9b16fd35790ee7bc687be896
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-20 16:43:58 +0100

    fix: add prop-types module

--- src/components/reviews/ReviewCard.jsx
+++ src/components/reviews/ReviewCard.jsx
@@ -2,6 +2,7 @@
 
 import styled from 'styled-components';
 import { faArrowUpFromBracket } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Avatar from '../images/Avatar';
