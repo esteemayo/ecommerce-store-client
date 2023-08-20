4773528baf8a44dd4f4fc664b3beb7632c81f426
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-20 15:22:44 +0100

    fix: add prop-types module

--- src/components/modals/FileUploadModal.jsx
+++ src/components/modals/FileUploadModal.jsx
@@ -2,6 +2,7 @@
 
 import styled from 'styled-components';
 import CloseIcon from '@mui/icons-material/Close';
+import PropTypes from 'prop-types';
 import { useCallback, useState } from 'react';
 
 const FileUploadModal = ({ openModal, closeModal }) => {
