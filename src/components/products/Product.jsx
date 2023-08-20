d3bb16883086946379dfe6ca54aceea0a701b3cd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-20 16:15:26 +0100

    fix: add prop-types module

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -2,6 +2,7 @@
 
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import useAddToCart from '@/hooks/useAddToCart';
 import BreadCrumbs from '@/app/products/[id]/BreadCrumbs';
