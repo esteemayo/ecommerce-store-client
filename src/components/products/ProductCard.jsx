1e1937cd4f2346483abe6ec2ce1736a29c3709cd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-21 14:54:17 +0100

    fix: add css styled-component method

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -271,12 +271,12 @@ const Button = styled.button`
   text-transform: uppercase;
   background-color: ${({ theme }) => theme.bgBtn};
   color: ${({ theme }) => theme.textBtn};
-  background-image: linear-gradient(
+  background-image: ${({ theme }) => css`linear-gradient(
     120deg,
     transparent 0%,
     transparent 50%,
-    ${({ theme }) => theme.bgImgBtn} 50%
-  );
+    ${theme.bgImgBtn} 50%
+  )`};
   background-size: 220%;
   outline-color: ${({ theme }) => theme.btnOut};
   border-radius: 0.5rem;
