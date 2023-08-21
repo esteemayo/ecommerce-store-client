2b97f85015a9b9c54e71199aa76889ee19a1c71c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-21 16:55:21 +0100

    fix: handle dark/light mode

--- src/components/modals/PaymentModal.jsx
+++ src/components/modals/PaymentModal.jsx
@@ -297,12 +297,14 @@ const FormButton = styled.button`
   padding: 1.5rem;
   background-color: ${({ theme }) => theme.bgBtn};
   color: ${({ theme }) => theme.textBtn};
-  background-image: linear-gradient(
-    120deg,
-    transparent 0%,
-    transparent 50%,
-    var(--clr-tertiary-green) 50%
-  );
+  background-image: ${({ theme }) => css`
+    linear-gradient(
+      120deg,
+      transparent 0%,
+      transparent 50%,
+      ${theme.bgImgBtn} 50%
+    );
+  `};
   background-size: 220%;
   border-radius: 0.5rem;
   outline-color: #85beae;
