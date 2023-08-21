f4adeec8e42eedb352555e2a3a18621782f5dbbb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-21 15:05:48 +0100

    fix: modify

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -357,12 +357,14 @@ const FormButton = styled.button`
   padding: 1.5rem;
   background-color: ${({ theme }) => theme.bgBtn};
   color: ${({ theme }) => theme.textBtn};
-  background-image: linear-gradient(
-    120deg,
-    transparent 0%,
-    transparent 50%,
-    ${({ theme }) => theme.bgImgBtn} 50%
-  );
+  background-image: ${({ theme }) => css`
+    linear-gradient(
+      120deg,
+      transparent 0%,
+      transparent 50%,
+      ${theme.bgImgBtn} 50%
+    );
+  `};
   background-size: 220%;
   border-radius: 0.5rem;
   outline-color:  ${({ theme }) => theme.btnOut};
