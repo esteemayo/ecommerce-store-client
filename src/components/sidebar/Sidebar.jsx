26fd327fd54e514e100213f54ac5bfa1134e7366
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-21 01:12:22 +0100

    fix: add useMemo react hook

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@
 
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useMemo } from 'react';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch, useSelector } from 'react-redux';
 
