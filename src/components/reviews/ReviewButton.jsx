919c1610b8fe2d959929d5b2adec422f45656beb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-21 15:07:01 +0100

    fix: format

--- src/components/reviews/ReviewButton.jsx
+++ src/components/reviews/ReviewButton.jsx
@@ -17,12 +17,14 @@ const Button = styled.button`
   padding: 1rem;
   background-color: ${({ theme }) => theme.bgRevBtn};
   color:  ${({ theme }) => theme.textRevBtn};
-  background-image: ${({ theme }) => css`linear-gradient(
-    120deg,
-    transparent 0%,
-    transparent 50%,
-    ${theme.bgImgRevBtn} 50%
-  )`};
+  background-image: ${({ theme }) => css`
+    linear-gradient(
+      120deg,
+      transparent 0%,
+      transparent 50%,
+      ${theme.bgImgRevBtn} 50%
+    );
+  `};
   background-size: 220%;
   border-radius: 0.5rem;
   outline-color: ${({ theme }) => theme.textRevBtn};
