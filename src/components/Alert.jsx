64ab92ec4881334516409f5ee0b77eed169818fc
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-21 01:19:56 +0100

    fix: add prop-types module

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,6 +2,7 @@
 
 import styled from 'styled-components';
 import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 const Alert = ({ alert, center, message, onChange }) => {
   useEffect(() => {
