62f97ec99e44060f43b85773773372e08e750a93
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-22 01:24:45 +0100

    feat: add icons

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const LikeButton = () => {
   return (
