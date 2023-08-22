0e1d3c24e66490bf9643282f0762d98a5546a407
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-22 01:34:05 +0100

    fix: add icon

--- src/components/WishlistButton.jsx
+++ src/components/WishlistButton.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const WishlistButton = () => {
   return (
