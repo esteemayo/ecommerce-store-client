74bee077b79e84465bdd7d2820cbe09b913193b9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-22 02:00:56 +0100

    fix: refactor

--- src/app/orders/[id]/page.jsx
+++ src/app/orders/[id]/page.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import styled from 'styled-components';
-import { NumericFormat } from 'react-number-format';
 import { useDispatch } from 'react-redux';
 
 import ClientOnly from '@/components/ClientOnly';
