66e6c86bed21eba4bd83d2ee1ee4ed6869619852
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-22 14:16:16 +0100

    fix: modify

--- src/components/navbar/SearchIcon.jsx
+++ src/components/navbar/SearchIcon.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const SearchIcon = () => {
   return <FontAwesomeIcon icon={faMagnifyingGlass} />;
