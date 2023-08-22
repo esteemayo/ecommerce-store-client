c1406388b5781a1706cc4cf7e62a062e97477a44
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-22 02:53:52 +0100

    fix: import prop-types module

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -2,6 +2,7 @@
 
 import styled from 'styled-components';
 import { useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
 
 import ReviewHead from './ReviewHead';
 import ReviewCards from './ReviewCards';
