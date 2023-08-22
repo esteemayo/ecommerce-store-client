8bf5cc46f5e4c6dbe3fb6531ce194d20428340e2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-22 15:03:25 +0100

    fix: receives data via props

--- src/components/navbar/SearchIcon.jsx
+++ src/components/navbar/SearchIcon.jsx
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const SearchIcon = () => {
+const SearchIcon = ({ onOpen }) => {
   return (
-    <Container>
+    <Container onClick={onOpen}>
       <FontAwesomeIcon icon={faMagnifyingGlass} />
     </Container>
   );
