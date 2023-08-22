d9d750efeb54cd7dc8b28a5396b830b47497f468
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-22 15:06:52 +0100

    fix: add SearchModal component

--- src/app/layout.js
+++ src/app/layout.js
@@ -45,6 +45,7 @@ export default function RootLayout({ children }) {
                     <ToastProvider />
                     <Submenu />
                     <Sidebar />
+                    <SearchModal />
                   </ClientOnly>
                   {children}
                   <ClientOnly>
