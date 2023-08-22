82802d7b1e16b2b5bdc7111fdc9514f20b8c541c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-22 01:30:04 +0100

    fix: refactor

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -351,37 +351,6 @@ const WishlistButton = styled.button`
   }
 `;
 
-// const LikeButton = styled.button`
-//   border: none;
-//   font-weight: 500;
-//   font-size: 1.4rem;
-//   background-color: transparent;
-//   color: #858585;
-//   outline-color: #b8b6b6;
-//   outline-offset: 3px;
-//   cursor: pointer;
-
-//   display: flex;
-//   align-items: center;
-//   gap: 0.5rem;
-
-//   @media only screen and (max-width: 64em) {
-//     align-items: self-end;
-//   }
-
-//   @media only screen and (max-width: 35em) {
-//     font-size: 1.37rem;
-//   }
-
-//   svg {
-//     font-size: 1.7rem;
-
-//     @media only screen and (max-width: 35em) {
-//       font-size: 1.6rem;
-//     }
-//   }
-// `;
-
 ProductCard.propTypes = {
   product: PropTypes.object.isRequired,
   onOpen: PropTypes.func.isRequired,
