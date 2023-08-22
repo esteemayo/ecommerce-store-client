c8905f55b49e598db8ae589b161b7dbaa0146dd9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-22 01:25:43 +0100

    fix: modify

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 const LikeButton = () => {
   return (
