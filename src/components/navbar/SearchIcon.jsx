318788debd18d876d513519874f46129eda3fcbf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-22 14:08:33 +0100

    fix: add icon

--- src/components/navbar/SearchIcon.jsx
+++ src/components/navbar/SearchIcon.jsx
@@ -2,9 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const SearchIcon = () => {
-  return (
-    <div>SearchIcon</div>
-  );
+  return <FontAwesomeIcon icon={faMagnifyingGlass} />;
 }
 
 export default SearchIcon;
