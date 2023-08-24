2b11b2fcd4afbdb1e9edd28e472ee878ef0f820b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-24 04:26:58 +0100

    fix: modify

--- src/components/SearchHistory.jsx
+++ src/components/SearchHistory.jsx
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
-import { faClock, faMagnifyingGlass, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { Container } from '@mui/material';
+import { faClock, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const SearchHistory = ({ id, query }) => {
   return (
