4278ceda0df6c4c3b4615a13bcc27f9df6f7ee5d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-24 21:46:04 +0100

    fix: refactor

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -1,9 +1,6 @@
 'use client';
 
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Image from 'next/image';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch } from 'react-redux';
 
 import Modal from '@/components/modals/Modal';
