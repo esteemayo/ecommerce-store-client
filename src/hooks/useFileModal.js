f0701fde3a63bad5e8d8bf60136cdc200f167516
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-24 16:36:35 +0100

    feat: create useFileModal custom hook

---
+++ src/hooks/useFileModal.js
@@ -0,0 +1,9 @@
+import { create } from 'zustand';
+
+const useFileModal = create((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useFileModal;
