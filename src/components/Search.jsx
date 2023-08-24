5907ed4f071f5029c44b38e0589a651747800d6f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-24 04:36:00 +0100

    fix: add useRef react hook

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 import SearchHistory from './SearchHistory';
