60473ba19a4906a8c53713237a380a4029adb210
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-24 17:20:21 +0100

    fix: refactor

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -331,18 +331,6 @@ const DeleteContainer = styled.div`
   margin-top: 3rem;
 `;
 
-const DeleteHeader = styled.h2`
-  display: inline-block;
-  text-transform: capitalize;
-  font-weight: 500;
-  font-size: 1.385rem;
-  color: ${({ theme }) => theme.textDelHeader};
-
-  @media only screen and (max-width: 43.75em) {
-    font-size: 1.58rem;
-  }
-`;
-
 const Right = styled.div`
   flex: 1;
   display: flex;
