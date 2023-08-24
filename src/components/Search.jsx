7566cb5119bd43aebc6889360c81f7128269d7f4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-24 04:07:38 +0100

    fix: modify

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -57,7 +57,7 @@ const Search = () => {
         </FormGroup>
         <Button type='submit'>Search</Button>
       </Form>
-      {!!histories.length > 0 && !!histories.length <= 5 && histories.map((item) => {
+      {!!histories.length > 0 && histories.splice(0, 5).map((item) => {
         const { id, query } = item;
         return (
           <Wrapper key={id}>
