7ee6a7752b3e04d89d259fcae9b871c1b07dac0a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-24 17:18:04 +0100

    feat: create DeactivateAccount component

---
+++ src/app/account/DeactivateAccount.jsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const DeactivateAccount = ({ onOpen }) => {
+  return (
+    <Container>
+      <Header>Delete your account</Header>
+      <Warning>
+        You will receive an email to confirm your decision.
+        Please note, that all your data will be permanently erased.
+      </Warning>
+      <Button type='button' onClick={onOpen}>
+        Delete account
+      </Button>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  margin-top: 3rem;
+`;
+
+const Header = styled.h2`
+  display: inline-block;
+  text-transform: capitalize;
+  font-weight: 500;
+  font-size: 1.385rem;
+  color: ${({ theme }) => theme.textDelHeader};
+
+  @media only screen and (max-width: 43.75em) {
+    font-size: 1.58rem;
+  }
+`;
+
+const Warning = styled.p`
+  font-size: 1.3rem;
+  color: #999;
+  line-height: 1.2;
+  word-wrap: break-word;
+
+  @media only screen and (max-width: 43.75em) {
+    font-size: 1.5rem;
+  }
+`;
+
+const Button = styled.button`
+  display: inline-block;
+  border: none;
+  padding: 0.5rem 2rem;
+  background-color: transparent;
+  color: var(--clr-primary-red);
+  border: 1px solid var(--clr-primary-red);
+  border-radius: 0.5rem;
+  outline-color: #e92222;
+  margin-top: 2rem;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  @media only screen and (max-width: 43.75em) {
+    font-size: 1.5rem;
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+  }
+
+  &:hover {
+    background-color: rgba(222, 10, 10, 0.1);
+  }
+`;
+
+DeactivateAccount.propTypes = {
+  onOpen: PropTypes.func.isRequired,
+};
+
+export default DeactivateAccount;
