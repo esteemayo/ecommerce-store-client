bf328bdb88e31c7c5de88bb036543428b51e0be2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-25 15:03:35 +0100

    feat: create useImageModal custom hook

---
+++ src/hooks/useImageModal.js
@@ -0,0 +1,9 @@
+import { create } from 'zustand';
+
+const useImageModal = create((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useImageModal;
