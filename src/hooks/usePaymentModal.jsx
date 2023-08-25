24727f045d46f725b927a827c51904355d230850
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-25 16:44:10 +0100

    feat: create usePaymentModal custom hook

---
+++ src/hooks/usePaymentModal.jsx
@@ -0,0 +1,9 @@
+import { create } from 'zustand';
+
+const usePaymentModal = create((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default usePaymentModal;
