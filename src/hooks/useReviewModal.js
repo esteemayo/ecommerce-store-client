3214b32f5af1443b6f2298f3f671c6b572759e2f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-25 14:33:29 +0100

    feat: create useReviewModal custom hook

---
+++ src/hooks/useReviewModal.js
@@ -0,0 +1,9 @@
+import { create } from 'zustand';
+
+const useReviewModal = create((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useReviewModal;
