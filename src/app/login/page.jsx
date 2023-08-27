d08195d694ee84f6cf8cd43f8707a295454456b6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-27 15:09:25 +0100

    fix: refactor

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -130,7 +130,7 @@ const Login = () => {
                 <CheckMark className={checkmarkClasses} />
                 <CheckBoxLabel htmlFor='rememberMe'>Remember me</CheckBoxLabel>
               </CheckBoxWrapper>
-              <FormButton type='submit'>Log in</FormButton>
+              {/* <FormButton type='submit'>Log in</FormButton> */}
               <ForgotPassword>
                 <Link href='/forgot' passHref>
                   Forgot your password?
@@ -290,44 +290,6 @@ const CheckBoxLabel = styled.label`
 `;
 
 const CheckMark = styled.span``;
-
-const FormButton = styled.button`
-  margin-top: 2rem;
-  border: none;
-  display: inline-block;
-  font-weight: 500;
-  font-size: 1.4rem;
-  width: 100%;
-  padding: 1.5rem;
-  background-color: ${({ theme }) => theme.bgBtn};
-  color: ${({ theme }) => theme.textBtn};
-  background-image: ${({ theme }) => css`
-    linear-gradient(
-      120deg,
-      transparent 0%,
-      transparent 50%,
-      ${theme.bgImgBtn} 50%
-    );
-  `};
-  background-size: 220%;
-  border-radius: 0.5rem;
-  outline-color:  ${({ theme }) => theme.btnOut};
-  cursor: pointer;
-  transition: all 0.3s;
-
-  @media only screen and (max-width: 56.25em), only screen and (hover: none) {
-    font-size: 1.398rem;
-  }
-
-  &:hover {
-    background-position: 100%;
-  }
-
-  &:disabled {
-    opacity: 0.85;
-    cursor: not-allowed;
-  }
-`;
 
 const ForgotPassword = styled.p`
   font-size: 1.65rem;
