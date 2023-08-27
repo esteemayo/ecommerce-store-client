3ebac35891a4c7351ae3fbc5b5c9a717dec4b3b3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-28 00:30:55 +0100

    fix: add markup

--- src/components/form/FormUpload.jsx
+++ src/components/form/FormUpload.jsx
@@ -1,9 +1,63 @@
 import styled from 'styled-components';
 
-const FormUpload = () => {
+const FormUpload = ({ id, label, ...rest }) => {
   return (
-    <div>FormUpload</div>
-  )
+    <FormGroup>
+      <Input {...rest} id={id} type='file' />
+      <Label htmlFor={id}>{label}</Label>
+    </FormGroup>
+  );
 }
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+
+  &:not(:last-of-type) {
+    margin-bottom: 2rem;
+  }
+`;
+
+const Label = styled.label`
+  display: inline-block;
+  font-weight: 600;
+  font-size: 1.6rem;
+  color:  ${({ theme }) => theme.textLabel};
+`;
+
+const Input = styled.input`
+  width: 0.1px;
+  height: 0.1px;
+  opacity: 0;
+  overflow: hidden;
+  position: absolute;
+  z-index: -1;
+
+  &:focus + ${Label} {
+    outline: 3px solid ${({ theme }) => theme.fileInput};
+    outline-offset: 3px;
+  }
+
+  & + ${Label} {
+    color: ${({ theme }) => theme.textFile};
+    display: inline-block;
+    text-decoration: none;
+    padding: 3px;
+    border-bottom: 1px solid ${({ theme }) => theme.fileInput};
+    cursor: pointer;
+    transition: all 0.3s;
+
+    &:hover {
+      background-color: ${({ theme }) => theme.fileInput};
+      color: ${({ theme }) => theme.textCat};
+      box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.15);
+      -webkit-box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.15);
+      -moz-box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.15);
+      transform: translateY(-2px);
+    }
+  }
+`;
 
 export default FormUpload;
