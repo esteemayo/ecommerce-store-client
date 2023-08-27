d863ebf9b518bd02ebe8659f3e957876b90374ec
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-27 15:18:04 +0100

    fix: refactor

--- src/app/forgot/page.jsx
+++ src/app/forgot/page.jsx
@@ -4,8 +4,9 @@ import styled, { css } from 'styled-components';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
-import ClientOnly from '@/components/ClientOnly';
 import AuthError from '@/components/form/FormError';
+import ClientOnly from '@/components/ClientOnly';
+import FormButton from '@/components/form/FormButton';
 
 import { closeSubmenu } from '@/features/submenu/submenuSlice';
 
@@ -64,7 +65,7 @@ const Forgot = () => {
                 />
                 {errors.email && <AuthError message={errors.email} />}
               </FormGroup>
-              <FormButton type='submit'>Reset password</FormButton>
+              <FormButton label='Reset password' />
             </Form>
           </Wrapper>
         </ForgotContainer>
@@ -190,43 +191,43 @@ const FormInput = styled.input`
   }
 `;
 
-const FormButton = styled.button`
-  margin-top: 2rem;
-  border: none;
-  display: inline-block;
-  text-transform: capitalize;
-  font-weight: 500;
-  font-size: 1.4rem;
-  width: 100%;
-  padding: 1.5rem;
-  background-color: ${({ theme }) => theme.bgBtn};
-  color: ${({ theme }) => theme.textBtn};
-  background-image: ${({ theme }) => css`
-    linear-gradient(
-      120deg,
-      transparent 0%,
-      transparent 50%,
-      ${theme.bgImgBtn} 50%
-    );
-  `};
-  background-size: 220%;
-  border-radius: 0.5rem;
-  outline-color: ${({ theme }) => theme.btnOut};
-  cursor: pointer;
-  transition: all 0.3s;
+// const FormButton = styled.button`
+//   margin-top: 2rem;
+//   border: none;
+//   display: inline-block;
+//   text-transform: capitalize;
+//   font-weight: 500;
+//   font-size: 1.4rem;
+//   width: 100%;
+//   padding: 1.5rem;
+//   background-color: ${({ theme }) => theme.bgBtn};
+//   color: ${({ theme }) => theme.textBtn};
+//   background-image: ${({ theme }) => css`
+//     linear-gradient(
+//       120deg,
+//       transparent 0%,
+//       transparent 50%,
+//       ${theme.bgImgBtn} 50%
+//     );
+//   `};
+//   background-size: 220%;
+//   border-radius: 0.5rem;
+//   outline-color: ${({ theme }) => theme.btnOut};
+//   cursor: pointer;
+//   transition: all 0.3s;
 
-  @media only screen and (max-width: 56.25em), only screen and (hover: none) {
-    font-size: 1.398rem;
-  }
+//   @media only screen and (max-width: 56.25em), only screen and (hover: none) {
+//     font-size: 1.398rem;
+//   }
 
-  &:hover {
-    background-position: 100%;
-  }
+//   &:hover {
+//     background-position: 100%;
+//   }
 
-  &:disabled {
-    opacity: 0.85;
-    cursor: not-allowed;
-  }
-`;
+//   &:disabled {
+//     opacity: 0.85;
+//     cursor: not-allowed;
+//   }
+// `;
 
 export default Forgot;
