06c899d08c1e1c37fb1874342493c340a8f4a17a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-27 03:13:28 +0100

    fix: refactor

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -235,49 +235,6 @@ const GoogleContainer = styled.div`
   }
 `;
 
-const GoogleButton = styled.button`
-  width: 50%;
-  padding: 1rem 4rem;
-  font-weight: 400;
-  font-size: 1.5rem;
-  background-color: transparent;
-  color: ${({ theme }) => theme.text};
-  border: 1px solid ${({ theme }) => theme.inputBorder};
-  border-radius: 0.5rem;
-  outline-color: #ccc;
-  cursor: pointer;
-  transition: all 0.3s ease;
-
-  display: flex;
-  align-items: center;
-  gap: 0.8rem;
-
-  @media only screen and (max-width: 56.25em), only screen and (hover: none) {
-    font-size: 1.495rem;
-  }
-
-  @media only screen and (max-width: 26.25em) {
-    width: 100%;
-    justify-content: center;
-  }
-
-  &:hover {
-    opacity: 0.7;
-  }
-
-  img {
-    width: 2.3rem;
-    height: 2.3rem;
-    display: block;
-    object-fit: cover;
-
-    @media only screen and (max-width: 26.25em) {
-      width: 2rem;
-      height: 2rem;
-    }
-  }
-`;
-
 const Text = styled.p`
   display: block;
   text-align: center;
