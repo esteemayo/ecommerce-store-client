494a3110b91f4510ab1ee0d5cb379647428c87e9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-28 00:15:16 +0100

    fix: modify

--- src/app/admin/products/new/page.jsx
+++ src/app/admin/products/new/page.jsx
@@ -190,6 +190,7 @@ const NewProduct = () => {
               <FormGroup>
                 <FormLabel htmlFor='category'>Select category</FormLabel>
                 <FormSelect id='category' name='category' onChange={handleChange}>
+                  <Option value=''>Uncategorized</Option>
                   <Option value='jeans'>Jeans</Option>
                   <Option value='electronics'>Electronics</Option>
                   <Option value='gadgets'>Gadgets</Option>
