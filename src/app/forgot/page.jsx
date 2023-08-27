1d1a1f597d559dd919fb1d66ba900e0adeeda433
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-27 15:18:21 +0100

    fix: refactor

--- src/app/forgot/page.jsx
+++ src/app/forgot/page.jsx
@@ -191,43 +191,4 @@ const FormInput = styled.input`
   }
 `;
 
-// const FormButton = styled.button`
-//   margin-top: 2rem;
-//   border: none;
-//   display: inline-block;
-//   text-transform: capitalize;
-//   font-weight: 500;
-//   font-size: 1.4rem;
-//   width: 100%;
-//   padding: 1.5rem;
-//   background-color: ${({ theme }) => theme.bgBtn};
-//   color: ${({ theme }) => theme.textBtn};
-//   background-image: ${({ theme }) => css`
-//     linear-gradient(
-//       120deg,
-//       transparent 0%,
-//       transparent 50%,
-//       ${theme.bgImgBtn} 50%
-//     );
-//   `};
-//   background-size: 220%;
-//   border-radius: 0.5rem;
-//   outline-color: ${({ theme }) => theme.btnOut};
-//   cursor: pointer;
-//   transition: all 0.3s;
-
-//   @media only screen and (max-width: 56.25em), only screen and (hover: none) {
-//     font-size: 1.398rem;
-//   }
-
-//   &:hover {
-//     background-position: 100%;
-//   }
-
-//   &:disabled {
-//     opacity: 0.85;
-//     cursor: not-allowed;
-//   }
-// `;
-
 export default Forgot;
