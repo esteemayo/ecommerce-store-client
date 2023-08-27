c228d6f169344bc2bb300859c7679e6f7cdba47d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-27 03:08:22 +0100

    fix: modify

--- src/app/login/SocialLogin.jsx
+++ src/app/login/SocialLogin.jsx
@@ -12,4 +12,112 @@ const Container = styled.div`
   
 `;
 
+const Header = styled.h1`
+  display: block;
+  font-weight: 400;
+  font-size: 2rem;
+  text-align: center;
+  color: ${({ theme }) => theme.textFormHeader};
+`;
+
+const GoogleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  gap: 1.5rem;
+  margin: 2rem 0;
+
+  @media only screen and (max-width: 64em) {
+    margin: 1.8rem 0;
+  }
+
+  @media only screen and (max-width: 26.25em) {
+    flex-direction: column;
+    gap: 1rem;
+  }
+`;
+
+const GoogleButton = styled.button`
+  width: 50%;
+  padding: 1rem 4rem;
+  font-weight: 400;
+  font-size: 1.5rem;
+  background-color: transparent;
+  color: ${({ theme }) => theme.text};
+  border: 1px solid ${({ theme }) => theme.inputBorder};
+  border-radius: 0.5rem;
+  outline-color: #ccc;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+
+  @media only screen and (max-width: 56.25em), only screen and (hover: none) {
+    font-size: 1.495rem;
+  }
+
+  @media only screen and (max-width: 26.25em) {
+    width: 100%;
+    justify-content: center;
+  }
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  img {
+    width: 2.3rem;
+    height: 2.3rem;
+    display: block;
+    object-fit: cover;
+
+    @media only screen and (max-width: 26.25em) {
+      width: 2rem;
+      height: 2rem;
+    }
+  }
+`;
+
+const FacebookButton = styled.button`
+  width: 50%;
+  padding: 1rem 4rem;
+  font-weight: 400;
+  font-size: 1.5rem;
+  background-color: transparent;
+  color: ${({ theme }) => theme.text};
+  border: 1px solid ${({ theme }) => theme.inputBorder};
+  border-radius: 0.5rem;
+  outline-color: #ccc;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+
+  @media only screen and (max-width: 56.25em), only screen and (hover: none) {
+    font-size: 1.495rem;
+  }
+
+  @media only screen and (max-width: 26.25em) {
+    width: 100%;
+    justify-content: center;
+  }
+
+  svg {
+    font-size: 2.3rem;
+    color: #3b5999;
+
+    @media only screen and (max-width: 26.25em) {
+      font-size: 2rem;
+    }
+  }
+`;
+
 export default SocialLogin;
