5bbdd5f4a718141cb9fa3af6449a990a3a9e4fb0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-27 03:33:27 +0100

    fix: refactor

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -331,29 +331,6 @@ const RegisterContainer = styled.div`
   margin-top: 3rem;
 `;
 
-const RegisterText = styled.p`
-  font-size: 1.7rem;
-  color: ${({ theme }) => theme.textLabel};
-
-  a {
-    &:link,
-    &:visited {
-      display: inline-block;
-      text-decoration: none;
-      text-decoration-color: var(--clr-tertiary-green);
-      color: var(--clr-tertiary-green);
-      outline-color: #ddd;
-      transition: all 0.3s ease;
-    }
-
-    &:hover {
-      text-decoration: underline;
-      text-decoration-color: var(--clr-tertiary-green);
-      text-underline-offset: 0.4rem;
-    }
-  }
-`;
-
 const ForgotPassword = styled.p`
   font-size: 1.65rem;
   color: ${({ theme }) => theme.textLabel};
