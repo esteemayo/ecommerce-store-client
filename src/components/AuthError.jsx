ef12a8c68942d667dc1fdf0a8cc71abdd2804bb0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-27 03:46:13 +0100

    fix: modify

--- src/components/AuthError.jsx
+++ src/components/AuthError.jsx
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
-const AuthError = () => {
-  return (
-    <div>AuthError</div>
-  )
+const AuthError = ({ message }) => {
+  return <ErrorMessage>{message}</ErrorMessage>;
 }
+
+const ErrorMessage = styled.span`
+  font-size: 1.3rem;
+  color: var(--clr-secondary-red);
+  margin-top: 1px;
+`;
 
 export default AuthError;
