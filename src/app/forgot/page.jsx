552d8d93ae8713ff181bbbf7aae5df80b2fc2e3f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-27 03:56:40 +0100

    fix: refactor

--- src/app/forgot/page.jsx
+++ src/app/forgot/page.jsx
@@ -229,10 +229,4 @@ const FormButton = styled.button`
   }
 `;
 
-const ErrorMsg = styled.span`
-  font-size: 1.3rem;
-  color: var(--clr-secondary-red);
-  margin-top: 1px;
-`;
-
 export default Forgot;
