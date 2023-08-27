d66d89b4126d55c4ce5f141b38f0e1a2de044c79
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-27 03:27:38 +0100

    fix: import next/link module

--- src/components/AuthInfo.jsx
+++ src/components/AuthInfo.jsx
@@ -1,9 +1,42 @@
 import styled from 'styled-components';
+import Link from 'next/link';
 
 const AuthInfo = () => {
   return (
-    <div>AuthInfo</div>
-  )
+    <RegisterContainer>
+      <RegisterText>
+        Don&apos;t have an account? {' '}
+        <Link href='/register' passHref>Sign up</Link>
+      </RegisterText>
+    </RegisterContainer>
+  );
 }
+
+const RegisterContainer = styled.div`
+  margin-top: 3rem;
+`;
+
+const RegisterText = styled.p`
+  font-size: 1.7rem;
+  color: ${({ theme }) => theme.textLabel};
+
+  a {
+    &:link,
+    &:visited {
+      display: inline-block;
+      text-decoration: none;
+      text-decoration-color: var(--clr-tertiary-green);
+      color: var(--clr-tertiary-green);
+      outline-color: #ddd;
+      transition: all 0.3s ease;
+    }
+
+    &:hover {
+      text-decoration: underline;
+      text-decoration-color: var(--clr-tertiary-green);
+      text-underline-offset: 0.4rem;
+    }
+  }
+`;
 
 export default AuthInfo;
