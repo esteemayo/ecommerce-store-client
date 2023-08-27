62ac0565b40f63ae48a539a23b3f46e5b6bf214a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-27 03:51:26 +0100

    fix: refactor

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -352,10 +352,4 @@ const ForgotPassword = styled.p`
   }
 `;
 
-const ErrorMsg = styled.span`
-  font-size: 1.3rem;
-  color: var(--clr-secondary-red);
-  margin-top: 1px;
-`;
-
 export default Login;
