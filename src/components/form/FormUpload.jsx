2993ecd5d3964ae946afaf2bdbbc4f36f36b4391
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-29 08:50:10 +0100

    fix: modify

--- src/components/form/FormUpload.jsx
+++ src/components/form/FormUpload.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const FormUpload = ({ id, label, ...rest }) => {
   return (
     <FormGroup>
-      <input {...rest} id={id} type='file' />
+      <Input {...rest} id={id} type='file' />
       <Label htmlFor={id}>{label}</Label>
     </FormGroup>
   );
@@ -18,39 +18,6 @@ const FormGroup = styled.div`
 
   &:not(:last-of-type) {
     margin-bottom: 2rem;
-  }
-
-  input {
-    width: 0.1px;
-    height: 0.1px;
-    opacity: 0;
-    overflow: hidden;
-    position: absolute;
-    z-index: -1;
-
-    &:focus + ${Label} {
-      outline: 3px solid ${({ theme }) => theme.fileInput};
-      outline-offset: 3px;
-    }
-
-    & + ${Label} {
-      color: ${({ theme }) => theme.textFile};
-      display: inline-block;
-      text-decoration: none;
-      padding: 3px;
-      border-bottom: 1px solid ${({ theme }) => theme.fileInput};
-      cursor: pointer;
-      transition: all 0.3s;
-
-      &:hover {
-        background-color: ${({ theme }) => theme.fileInput};
-        color: ${({ theme }) => theme.textCat};
-        box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.15);
-        -webkit-box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.15);
-        -moz-box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.15);
-        transform: translateY(-2px);
-      }
-    }
   }
 `;
 
