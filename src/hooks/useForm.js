0f0a39dbafac465ccb571297849667921f14a3fe
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-30 01:32:25 +0100

    fix: add errors state variable

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,6 +2,7 @@ import { useCallback, useState } from 'react';
 
 const useForm = (callback, initialState = {}, validate) => {
   const [formData, setFormData] = useState(initialState);
+  const [errors, setErrors] = useState({});
 
   return {
     // 
