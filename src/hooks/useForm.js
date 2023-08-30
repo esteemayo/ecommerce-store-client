79fa07a4cbf5edf82cd3236a3e408da18aeae9d7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-30 01:34:10 +0100

    fix: adds handleChange function

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -4,6 +4,11 @@ const useForm = (callback, initialState = {}, validate) => {
   const [formData, setFormData] = useState(initialState);
   const [errors, setErrors] = useState({});
 
+  const handleChange = useCallback(({ target: input }) => {
+    const { name, value } = input;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  }, []);
+
   return {
     // 
   };
