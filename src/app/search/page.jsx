de3b4055f97da4f4847b5a197274acb6f3855381
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-05 16:25:17 +0100

    feat: add search product page

---
+++ src/app/search/page.jsx
@@ -0,0 +1,18 @@
+'use client';
+
+import styled from 'styled-components';
+
+import ProductList from '@/components/products/ProductList';
+import { closeSubmenu } from '@/features/submenu/submenuSlice';
+
+import { storeProducts } from '@/data';
+
+const Search = () => {
+  return (
+    <div>
+      Search
+    </div>
+  );
+}
+
+export default Search;
