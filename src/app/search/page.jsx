8ae7d8356e1f482302eba7fcb33497fe445a65bf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-05 16:50:50 +0100

    fix: add media query

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -49,6 +49,11 @@ const Wrapper = styled.div`
   @media only screen and (max-width: 50em) {
     max-width: 70rem;
   }
+
+  @media only screen and (max-width: 37.5em) {
+    padding-left: 5rem;
+    padding-right: 5rem;
+  }
 `;
 
 export default Search;
