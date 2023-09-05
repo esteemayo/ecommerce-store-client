a9058c8e255a4a43559c812385c2c66a6e2a8653
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-06 00:21:50 +0100

    fix: refactor

--- src/components/reviews/TopReviews.jsx
+++ src/components/reviews/TopReviews.jsx
@@ -75,34 +75,6 @@ const Container = styled.section`
   }
 `;
 
-const HeadingContainer = styled.div`
-  width: 100%;
-  max-width: 120rem;
-  margin: 0 auto;
-
-  @media only screen and (max-width: 64em) {
-    max-width: 100rem;
-  }
-
-  @media only screen and (max-width: 59.375em) {
-    max-width: 80rem;
-  }
-
-  @media only screen and (max-width: 50em) {
-    max-width: 70rem;
-  }
-
-  @media only screen and (max-width: 37.5em) {
-    padding-left: 5rem;
-    padding-right: 5rem;
-  }
-
-  @media only screen and (max-width: 25em) {
-    padding-left: 3rem;
-    padding-right: 3rem;
-  }
-`;
-
 const Wrapper = styled.div`
   position: relative;
 `;
