0cbcf349f88e4b24b6e8c5068f7525736f96483c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-05 14:05:31 +0100

    fix: modify

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -102,9 +102,7 @@ const Navbar = () => {
               onMouseLeave={handleMouseLeave}
             />
           ) : (
-            <>
-              <CartQuantity amount={qty} />
-            </>
+            <CartQuantity amount={qty} />
           )}
         </ButtonContainer>
       </Wrapper>
