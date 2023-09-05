5b74ef82a1bba75e250de7214b70aac559b4eba4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-06 00:37:34 +0100

    fix: handle not found display message

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -7,14 +7,28 @@ import ClientOnly from '@/components/ClientOnly';
 import ProductList from '@/components/products/ProductList';
 
 import { storeProducts } from '@/data';
+import EmptyProduct from '@/components/products/EmptyProduct';
 
 const Search = () => {
   const [products, setProducts] = useState(storeProducts);
 
+  let bodyContent;
+
+  if (products.length < 1) {
+    bodyContent = (
+      <EmptyProduct
+        src='/img/no-result.png'
+        title='No results matches your search criteria'
+      />
+    );
+  } else {
+    bodyContent = <ProductList products={products} />;
+  }
+
   return (
     <ClientOnly>
       <ProductBox>
-        <ProductList products={products} />
+        {bodyContent}
       </ProductBox>
     </ClientOnly>
   );
