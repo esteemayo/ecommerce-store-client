bdf8441542a31d76ba38682e351564a529c5a115
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-05 17:29:54 +0100

    fix: add ProductBox component

--- src/app/products/category/[category]/page.jsx
+++ src/app/products/category/[category]/page.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { useCallback, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
+import ProductBox from '@/components/products/ProductBox';
 import ClientOnly from '@/components/ClientOnly';
 import ProductList from '@/components/products/ProductList';
 
