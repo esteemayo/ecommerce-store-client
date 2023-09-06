e536d614e220eed2d6a3a3fba6fee3d57f5aef19
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-06 14:46:43 +0100

    fix: add FormBox component

--- src/app/forgot/page.jsx
+++ src/app/forgot/page.jsx
@@ -7,6 +7,7 @@ import { useDispatch } from 'react-redux';
 import FormButton from '@/components/form/FormButton';
 import ClientOnly from '@/components/ClientOnly';
 import FormInput from '@/components/form/FormInput';
+import FormBox from '@/components/form/FormBox';
 
 import { closeSubmenu } from '@/features/submenu/submenuSlice';
 
