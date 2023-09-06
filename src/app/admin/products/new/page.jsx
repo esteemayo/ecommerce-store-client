d187af8afbad754b50b52827263273fd4ff1f74b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-06 16:09:47 +0100

    fix: refactor

--- src/app/admin/products/new/page.jsx
+++ src/app/admin/products/new/page.jsx
@@ -205,45 +205,6 @@ const NewProduct = () => {
   );
 }
 
-const Wrapper = styled.div`
-  width: 100%;
-  max-width: 120rem;
-  margin: 0 auto;
-  padding: 10rem 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  @media only screen and (max-width: 64em) {
-    max-width: 100rem;
-    padding: 8rem 0;
-  }
-
-  @media only screen and (max-width: 59.375em) {
-    max-width: 80rem;
-  }
-
-  @media only screen and (max-width: 50em) {
-    max-width: 70rem;
-  }
-
-  @media only screen and (max-width: 37.5em) {
-    padding-left: 5rem;
-    padding-right: 5rem;
-  }
-
-  @media only screen and (max-width: 25em) {
-    padding-left: 3rem;
-    padding-right: 3rem;
-  }
-
-  @media only screen and (max-width: 23.75em) {
-    padding-left: 2rem;
-    padding-right: 2rem;
-  }
-`;
-
 const FormWrapper = styled.div`
   width: 40rem;
   padding: 2rem;
