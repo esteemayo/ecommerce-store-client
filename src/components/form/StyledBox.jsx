b028a93735c4f86ca6f9c289c7f24025e90c62eb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-06 03:22:46 +0100

    fix: format code

--- src/components/form/StyledBox.jsx
+++ src/components/form/StyledBox.jsx
@@ -1,20 +1,20 @@
 import styled from 'styled-components';
 
 export const StyledBox = styled.div`
-width: 40rem;
-padding: 2rem;
-border-radius: 1rem;
-box-shadow: ${({ theme }) => theme.boxForm};
--webkit-box-shadow: ${({ theme }) => theme.boxForm};
--moz-box-shadow: ${({ theme }) => theme.boxForm};
+  width: 40rem;
+  padding: 2rem;
+  border-radius: 1rem;
+  box-shadow: ${({ theme }) => theme.boxForm};
+  -webkit-box-shadow: ${({ theme }) => theme.boxForm};
+  -moz-box-shadow: ${({ theme }) => theme.boxForm};
 
-@media only screen and (max-width: 21.25em) {
-  width: 35rem;
-}
+  @media only screen and (max-width: 21.25em) {
+    width: 35rem;
+  }
 
-@media only screen and (max-width: 18.75em) {
-  width: 32rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
-}
+  @media only screen and (max-width: 18.75em) {
+    width: 32rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
 `;
