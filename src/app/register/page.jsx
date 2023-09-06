8421f25ccb5245dec1da79ef757b293be12858dc
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-06 16:02:10 +0100

    fix: refactor

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -131,14 +131,6 @@ const Register = () => {
   );
 }
 
-const Header = styled.h1`
-  display: block;
-  font-weight: 500;
-  font-size: 2.5rem;
-  text-align: center;
-  color: ${({ theme }) => theme.textFormHeader};
-`;
-
 const Form = styled.form`
   margin: 2rem 0;
 `;
