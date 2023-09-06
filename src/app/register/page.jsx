f0bec234855f77f7193696d979c76ca8732775de
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-06 03:02:30 +0100

    fix: refactor

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -132,25 +132,6 @@ const Register = () => {
   );
 }
 
-const Wrapper = styled.div`
-  width: 40rem;
-  padding: 2rem;
-  border-radius: 1rem;
-  box-shadow: ${({ theme }) => theme.boxForm};
-  -webkit-box-shadow: ${({ theme }) => theme.boxForm};
-  -moz-box-shadow: ${({ theme }) => theme.boxForm};
-
-  @media only screen and (max-width: 21.25em) {
-    width: 35rem;
-  }
-
-  @media only screen and (max-width: 18.75em) {
-    width: 32rem;
-    padding-left: 1rem;
-    padding-right: 1rem;
-  }
-`;
-
 const Header = styled.h1`
   display: block;
   font-weight: 500;
