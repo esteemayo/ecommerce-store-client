78cf6a3e5c4731207ed387070ec6917b9ea59252
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-06 03:11:43 +0100

    fix: refactor

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -195,25 +195,6 @@ const LoginContainer = styled.div`
   }
 `;
 
-const Wrapper = styled.div`
-  width: 40rem;
-  padding: 2rem;
-  border-radius: 1rem;
-  box-shadow: ${({ theme }) => theme.boxForm};
-  -webkit-box-shadow: ${({ theme }) => theme.boxForm};
-  -moz-box-shadow: ${({ theme }) => theme.boxForm};
-
-  @media only screen and (max-width: 21.25em) {
-    width: 35rem;
-  }
-
-  @media only screen and (max-width: 18.75em) {
-    width: 32rem;
-    padding-left: 1rem;
-    padding-right: 1rem;
-  }
-`;
-
 const Text = styled.p`
   display: block;
   text-align: center;
