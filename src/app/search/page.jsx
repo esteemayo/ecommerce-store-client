0838b8eb7c8553e34b5ac61b0eda37eb72191224
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-06 01:08:12 +0100

    fix: modify

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,45 +1,26 @@
 'use client';
 
-import styled from 'styled-components';
 import { useState } from 'react';
 
 import ProductBox from '@/components/products/ProductBox';
-import EmptyProduct from '@/components/products/EmptyProduct';
-import ProductList from '@/components/products/ProductList';
-
 import ClientOnly from '@/components/ClientOnly';
+import ProductList from '@/components/products/ProductList';
 
 import { storeProducts } from '@/data';
 
 const Search = () => {
   const [products, setProducts] = useState([]);
 
-  let bodyContent;
-
-  if (products.length < 1) {
-    bodyContent = (
-      <Container>
-        <EmptyProduct
-          src='/img/no-result.png'
-          title='No result matches your search criteria'
-        />
-      </Container>
-    );
-  } else {
-    bodyContent = <ProductList products={products} />;
-  }
-
   return (
     <ClientOnly>
       <ProductBox>
-        {bodyContent}
+        <ProductList
+          products={products}
+          title='No result matches your search criteria'
+        />
       </ProductBox>
     </ClientOnly>
   );
 }
-
-const Container = styled.div`
-  padding: 10rem 0;
-`;
 
 export default Search;
