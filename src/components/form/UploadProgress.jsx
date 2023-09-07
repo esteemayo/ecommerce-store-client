3ef7e95602ad16ba13abbd091cf0daaba20ba9b7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-07 18:30:06 +0100

    fix: format code

--- src/components/form/UploadProgress.jsx
+++ src/components/form/UploadProgress.jsx
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 
 const UploadProgress = ({ perecentage }) => {
   return (
-    <Container>Uploading: {perecentage}%</Container>
+    <Container>
+      Uploading: {perecentage}%
+    </Container>
   );
 }
 
