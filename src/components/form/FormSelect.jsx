ab2559b2b6611da670343a833ce52efba6578e82
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-08 01:03:47 +0100

    feat: import prop-types module

--- src/components/form/FormSelect.jsx
+++ src/components/form/FormSelect.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import FormError from './FormError';
 
 const FormSelect = ({ name, label, error, value, defaultText, ...rest }) => {
