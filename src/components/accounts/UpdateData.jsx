387e709fdd03da0565334e92104c1dfa20a73684
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-08 15:05:30 +0100

    fix: refactor

--- src/components/accounts/UpdateData.jsx
+++ src/components/accounts/UpdateData.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { useCallback, useState } from 'react';
 
