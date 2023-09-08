85c8b82e34e35a90e90f91b13fe705cdc055fd1e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-08 14:17:28 +0100

    fix: modify

--- src/components/accounts/Input.jsx
+++ src/components/accounts/Input.jsx
@@ -1,9 +1,61 @@
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-const Input = () => {
+import FormError from '../form/FormError';
+import { FormGroup } from '../form/FormGroup';
+
+const Input = ({ name, label, error, ...rest }) => {
   return (
-    <div>Input</div>
+    <FormGroup>
+      <Label htmlFor={id}>{label}</Label>
+      <StyledInput
+        {...rest}
+        id={name}
+        name={name}
+      />
+      {error && error && <FormError message={error} />}
+    </FormGroup>
   );
 }
+
+const Label = styled.label`
+  display: inline-block;
+  width: 15rem;
+  font-weight: 600;
+  font-size: 1.4rem;
+  color: ${({ theme }) => theme.textLabel};
+`;
+
+const StyledInput = styled.input`
+  display: inline-block;
+  font-family: inherit;
+  font-size: 1.5rem;
+  width: 100%;
+  padding: 1.25rem 1rem;
+  background-color: transparent;
+  color: ${({ theme }) => theme.textInput};
+  border: 2px solid #f3f4f6;
+  border-radius: 0.5rem;
+  outline-color: ${({ theme }) => theme.inputOut};
+  caret-color: ${({ theme }) => theme.inputCaret};
+
+  @media only screen and (max-width: 25em) {
+    padding-top: 1.2rem;
+    padding-bottom: 1.2rem;
+  }
+
+  &::placeholder {
+    font-weight: 300;
+    font-size: 1.5rem;
+    color: #bbb;
+  }
+`;
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  rest: PropTypes.any.isRequired,
+};
 
 export default Input;
