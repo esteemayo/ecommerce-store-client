0145d60ba1392d5fdc5dff4e55d5bcadb1ffe927
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-08 02:12:29 +0100

    fix: add styling

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Form = ({ children, onSubmit }) => {
+const Form = ({ type, children, onSubmit }) => {
   return (
-    <StyledForm onSubmit={onSubmit}>
+    <StyledForm type={type} onSubmit={onSubmit}>
       {children}
     </StyledForm>
   );
 }
 
 const StyledForm = styled.form`
-  
+  margin: 2rem 0;
+
+  @media only screen and (max-width: 64em) {
+    margin: ${({ type }) => type === 'login' && '1.8rem 0'};
+  }
 `;
 
 Form.propTypes = {
