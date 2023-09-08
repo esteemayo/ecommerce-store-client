e4d78c125e9530350f0eb60fd2c42703dd6d36f5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-08 14:36:17 +0100

    fix: refactor

--- src/components/accounts/UpdatePassword.jsx
+++ src/components/accounts/UpdatePassword.jsx
@@ -122,44 +122,6 @@ const CancelButton = styled.button`
   cursor: pointer;
 `;
 
-const FormButton = styled.button`
-  display: inline-block;
-  border: none;
-  text-transform: uppercase;
-  font-weight: 500;
-  font-size: 1.4rem;
-  width: 50%;
-  padding: 1rem;
-  background-color: ${({ theme }) => theme.bgBtn};
-  color: ${({ theme }) => theme.textModalDelBtn};
-  background-image: ${({ theme }) => css`
-    linear-gradient(
-      120deg,
-      transparent 0%,
-      transparent 50%,
-      ${theme.bgImgBtn} 50%
-    );
-  `};
-  background-size: 220%;
-  border-radius: 0.5rem;
-  outline-color: ${({ theme }) => theme.btnOut};
-  cursor: pointer;
-  transition: all 0.3s;
-
-  @media only screen and (max-width: 56.25em), only screen and (hover: none) {
-    font-size: 1.398rem;
-  }
-
-  &:hover {
-    background-position: 100%;
-  }
-
-  &:disabled {
-    opacity: 0.85;
-    cursor: not-allowed;
-  }
-`;
-
 UpdatePassword.propTypes = {
   onCancel: PropTypes.func.isRequired,
 };
