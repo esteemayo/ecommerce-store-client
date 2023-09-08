1796e22d273cc68e1f14adafa421cba915add598
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-08 15:13:30 +0100

    fix: refactor

--- src/components/accounts/UpdateData.jsx
+++ src/components/accounts/UpdateData.jsx
@@ -117,13 +117,6 @@ const EmailText = styled.span`
   font-weight: 600;
 `;
 
-const FormButtonContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 2rem;
-`;
-
 UpdateData.propTypes = {
   onCancel: PropTypes.func.isRequired,
 };
