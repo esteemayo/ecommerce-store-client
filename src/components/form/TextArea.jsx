7341ce634132c9d55b0704427c602b83ff26a57f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-08 01:37:20 +0100

    fix: add a textarea element tag

--- src/components/form/TextArea.jsx
+++ src/components/form/TextArea.jsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const TextArea = ({ name, label }) => {
+const TextArea = ({ name, label, error, ...rest }) => {
   return (
     <FormGroup>
       <Label htmlFor={name}>{label}</Label>
+      <StyledTextArea
+        {...rest}
+        id={name}
+        name={name}
+      />
     </FormGroup>
   );
 }
