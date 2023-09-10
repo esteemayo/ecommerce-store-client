6109ff6ca2dea73a87c900339d27e41c44ca4e21
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-10 15:55:35 +0100

    fix: add an option element tag

--- src/components/inputs/CountrySelect.jsx
+++ src/components/inputs/CountrySelect.jsx
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
-import { Label } from '../form/Select';
+import { Label, StyledSelect } from '../form/Select';
 import { FormGroup } from '../form/FormGroup';
 
 const CountrySelect = ({ name, label, error, ...rest }) => {
   return (
     <FormGroup>
       <Label htmlFor={name}>{label}</Label>
+      <StyledSelect {...rest} id={name} name={name}></StyledSelect>
     </FormGroup>
   );
 }
