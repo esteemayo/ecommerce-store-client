f7fd55e55274e2757e73e806bc9b0deb6303427a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-11 00:38:25 +0100

    fix: modify

--- src/hooks/useCountries.js
+++ src/hooks/useCountries.js
@@ -3,7 +3,6 @@ import countries from 'world-countries';
 const formatCountries = countries.map((item) => ({
   flag: item.flag,
   label: item.name.common,
-  value: item.cca2,
 }));
 
 export const useCountries = () => {
