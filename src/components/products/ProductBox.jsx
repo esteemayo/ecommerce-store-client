052d9558dfb9d0941b0ad4f3241119f7222cf1ad
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-11 01:34:16 +0100

    fix: refactor

--- src/components/products/ProductBox.jsx
+++ src/components/products/ProductBox.jsx
@@ -11,10 +11,6 @@ const ProductBox = ({ children }) => {
   const dispatch = useDispatch();
   const { handleSubmenu } = useCloseSubmenu();
 
-  const handleClose = useCallback(() => {
-    dispatch(closeSubmenu());
-  }, [dispatch]);
-
   return (
     <Container onMouseOver={handleSubmenu}>
       <StyledWrapper>{children}</StyledWrapper>
