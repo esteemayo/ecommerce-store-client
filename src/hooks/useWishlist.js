3063b4f8f34d8525feef45102e0cbbadf401d873
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-11 15:25:53 +0100

    fix: add isWished

--- src/hooks/useWishlist.js
+++ src/hooks/useWishlist.js
@@ -1,8 +1,14 @@
 import { useDispatch } from 'react-redux';
 import { useCallback, useMemo } from 'react';
 
-export const useWishlist = ({ product }) => {
+export const useWishlist = ({ actionId, products }) => {
   const dispatch = useDispatch();
+
+  const isWished = useMemo(() => {
+    const wished = products.includes(actionId)
+    return !!wished;
+  }, [actionId, products]);
+
   return (
     <div>useWishlist</div>
   );
