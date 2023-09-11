14bbab7699afa8ab4235283832f0e1b632e0d319
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-11 13:15:00 +0100

    fix: modify

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,11 @@ import styled from 'styled-components';
 import dynamic from 'next/dynamic';
 
 import ClientOnly from '@/components/ClientOnly';
+import CarouselSlider from '@/components/sliders/CarouselSlider';
 
 const Categories = dynamic(() => import('@/components/categories/Categories'), { ssr: false });
 const FeaturedProducts = dynamic(() => import('@/components/products/FeaturedProducts'), { ssr: false });
 const TopReviews = dynamic(() => import('@/components/reviews/TopReviews'), { ssr: false });
-const CarouselSlider = dynamic(() => import('@/components/sliders/CarouselSlider'), { ssr: false });
 
 const Home = () => {
   return (
