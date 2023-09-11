0394fc3abccb35efa78ab238730f339719f90e9c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-11 19:54:25 +0100

    fix: add IconButton component

---
+++ src/components/buttons/IconButton.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const IconButton = styled.button`
+  border: none;
+  display: inline-block;
+  font-weight: 500;
+  font-size: 1.4rem;
+  background-color: transparent;
+  color: #858585;
+  outline-color: #b8b6b6;
+  outline-offset: 3px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  @media only screen and (max-width: 64em) {
+    align-items: self-end;
+  }
+
+  @media only screen and (max-width: 35em) {
+    font-size: 1.37rem;
+  }
+
+  &:hover {
+    opacity: 0.75;
+  }
+
+  svg {
+    font-size: 1.7rem;
+
+    @media only screen and (max-width: 35em) {
+      font-size: 1.6rem;
+    }
+  }
+`;
