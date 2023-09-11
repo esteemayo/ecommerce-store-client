0e01ca3b15ba2600635f716bf162778f25afed4d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-11 18:38:59 +0100

    fix: refactor

--- src/components/buttons/WishlistButton.jsx
+++ src/components/buttons/WishlistButton.jsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
 
 import WishIcon from '../icons/WishIcon';
 import useWishlist from '@/hooks/useWishlist';
