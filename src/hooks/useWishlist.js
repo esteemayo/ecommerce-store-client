b85987b23f7238f4c5bc4b28dbfc743bfa611e98
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-11 15:23:27 +0100

    fix: import useDispatch react-redux hook

--- src/hooks/useWishlist.js
+++ src/hooks/useWishlist.js
@@ -2,6 +2,7 @@ import { useDispatch } from 'react-redux';
 import { useCallback, useMemo } from 'react';
 
 export const useWishlist = ({ product }) => {
+  const dispatch = useDispatch();
   return (
     <div>useWishlist</div>
   );
