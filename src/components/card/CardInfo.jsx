02e70929e72de46bae371e461d537af7272700b1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-12 01:51:04 +0100

    fix: add media query

--- src/components/card/CardInfo.jsx
+++ src/components/card/CardInfo.jsx
@@ -12,6 +12,10 @@ const Container = styled.div`
   padding: 2rem;
   background-color: inherit;
   border-radius: 0.5rem;
+
+  @media only screen and (max-width: 35em) {
+    padding: 1rem;
+  }
 `;
 
 export default CardInfo;
