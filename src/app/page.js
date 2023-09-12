afc46ced31ed01b429db9d9f0aca0fc5db65e641
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-13 00:23:38 +0100

    fix: refactor

--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,18 @@ import dynamic from 'next/dynamic';
 import ClientOnly from '@/components/ClientOnly';
 import CarouselSlider from '@/components/sliders/CarouselSlider';
 
+import { useSubmenu } from '@/hooks/useSubmenu';
+
 const Categories = dynamic(() => import('@/components/categories/Categories'), { ssr: false });
 const FeaturedProducts = dynamic(() => import('@/components/products/FeaturedProducts'), { ssr: false });
 const TopReviews = dynamic(() => import('@/components/reviews/TopReviews'), { ssr: false });
 
 const Home = () => {
+  const { closeSubmenuHandler } = useSubmenu();
+
   return (
     <ClientOnly>
-      <Container>
+      <Container onMouseOver={closeSubmenuHandler}>
         <CarouselSlider />
         <Categories />
         <FeaturedProducts />
