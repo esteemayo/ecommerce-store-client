123538c0430670353408b338ffb50c0299f72e2c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-12 02:11:04 +0100

    fix: refactor

--- src/components/card/ProductCard.jsx
+++ src/components/card/ProductCard.jsx
@@ -9,9 +9,6 @@ import PropTypes from 'prop-types';
 import { useCallback, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
-import FavoriteButton from '../buttons/FavoriteButton';
-import WishlistButton from '../buttons/WishlistButton';
-
 import StarRating from '../StarRating';
 import CardInfo from './CardInfo';
 import CardImage from './CardImage';
