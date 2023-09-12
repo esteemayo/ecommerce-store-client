c9a281b2bb99a2987722d64db0f0b2ec407d293c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-12 02:12:42 +0100

    fix: refactor

--- src/components/card/ProductCard.jsx
+++ src/components/card/ProductCard.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { useCallback, useMemo } from 'react';
 import { useSelector } from 'react-redux';
