c01d88c224cb76993b48ad77d7d3142b5c5bdcd9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-12 02:54:52 +0100

    feat: create CardReview component

---
+++ src/components/card/CardReview.jsx
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import StarRating from '../StarRating';
+
+const CardReview = ({ reviewLabel, ratingsAverage, ratingsQuantity }) => {
+  return (
+    <ReviewContainer>
+      <StarRating
+        readOnly
+        value={ratingsAverage}
+        name='read-only'
+      />
+      <Reviews>
+        <Rating>({ratingsQuantity} {reviewLabel})</Rating>
+      </Reviews>
+    </ReviewContainer>
+  );
+}
+
+const ReviewContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom: 0.5rem;
+
+  svg {
+    font-size: 2.3rem;
+    fill: ${({ theme }) => theme.star};
+
+    @media only screen and (max-width: 35em) {
+      font-size: 2rem;
+    }
+  }
+`;
+
+const Reviews = styled.div`
+  span {
+    color: ${({ theme }) => theme.star};
+
+    @media only screen and (max-width: 35em) {
+      font-size: 1.4rem;
+    }
+  }
+`;
+
+const Rating = styled.span``;
+
+CardReview.propTypes = {
+  reviewLabel: PropTypes.string,
+  ratingsAverage: PropTypes.number,
+  ratingsQuantity: PropTypes.number,
+};
+
+export default CardReview;
