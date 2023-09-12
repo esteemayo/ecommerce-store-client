dd7e9b8f16a31706fb5ed9e7fa457a9850e98cfb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-12 02:21:12 +0100

    fix: refactor

--- src/components/card/CardInfo.jsx
+++ src/components/card/CardInfo.jsx
@@ -191,51 +191,6 @@ const InStock = styled.div`
   }
 `;
 
-// const Button = styled.button`
-//   border: none;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   width: 100%;
-//   padding: 1.5rem 1rem;
-//   font-size: 1.5rem;
-//   text-transform: uppercase;
-//   background-color: ${({ theme }) => theme.bgBtn};
-//   color: ${({ theme }) => theme.textBtn};
-//   background-image: ${({ theme }) => css`
-//     linear-gradient(
-//       120deg,
-//       transparent 0%,
-//       transparent 50%,
-//       ${theme.bgImgBtn} 50%
-//     );
-//   `};
-//   background-size: 220%;
-//   outline-color: ${({ theme }) => theme.btnOut};
-//   border-radius: 0.5rem;
-//   cursor: pointer;
-//   transition: all 0.3s;
-
-//   @media only screen and (max-width: 64em) {
-//     padding: 1.3rem 1rem;
-//     font-size: 1.4rem;
-//   }
-
-//   @media only screen and (max-width: 56.25em), only screen and (hover: none) {
-//     font-size: 1.498rem;
-//   }
-
-//   @media only screen and (max-width: 37.5em) {
-//     padding-top: 1rem;
-//     padding-bottom: 1rem;
-//     font-size: 1.4rem;
-//   }
-
-//   &:hover {
-//     background-position: 100%;
-//   }
-// `;
-
 const FavWrapper = styled.div`
   padding-top: 2rem;
   display: flex;
