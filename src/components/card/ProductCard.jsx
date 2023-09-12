437b8a118ca1627475a0e48f770dc5ddacc6842a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-12 02:07:38 +0100

    fix: refactor

--- src/components/card/ProductCard.jsx
+++ src/components/card/ProductCard.jsx
@@ -86,43 +86,6 @@ const Container = styled.article`
   }
 `;
 
-const ProductName = styled.h3`
-  text-transform: capitalize;
-  font-weight: 500;
-  font-size: 1.8rem;
-  color: ${({ theme }) => theme.textProdCard};
-  margin-bottom: 0.5rem;
-  line-height: 1;
-
-  @media only screen and (max-width: 64em) {
-    font-size: 1.7rem;
-  }
-
-  @media only screen and (max-width: 35em) {
-    font-size: 1.63rem;
-  }
-
-  a {
-    &:link,
-    &:visited {
-      display: inline-block;
-      text-decoration: none;
-      text-decoration-color: #e2e9e7;
-      color: inherit;
-      outline-color: #eee;
-      outline-offset: 3px;
-      transition: all 0.3s ease;
-    }
-
-    &:hover,
-    &:active {
-      text-decoration: underline;
-      text-decoration-color: #e2e9e7;
-      text-underline-offset: 3px;
-    }
-  }
-`;
-
 const ReviewContainer = styled.div`
   display: flex;
   align-items: center;
