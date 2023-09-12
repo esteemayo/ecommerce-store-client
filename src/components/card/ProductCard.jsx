950820f54f2c3c338dda5e6026219a8279ba9a35
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-12 02:08:30 +0100

    fix: refactor

--- src/components/card/ProductCard.jsx
+++ src/components/card/ProductCard.jsx
@@ -86,13 +86,6 @@ const Container = styled.article`
   }
 `;
 
-const PriceContainer = styled.div`
-  margin: 0.5rem 0 1.5rem 0;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
 const Prices = styled.div`
   display: flex;
   flex-direction: column;
