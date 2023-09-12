a0776869c58d0b16655c999491b1e648dd9dd8d1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-12 01:40:00 +0100

    fix: add an img element tag

--- src/components/card/CardImage.jsx
+++ src/components/card/CardImage.jsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 
-const CardImage = () => {
+const CardImage = ({ image }) => {
   return (
     <Container>
-      CardImage
+      <StyledImage
+        src={image}
+        width={300}
+        height={200}
+        alt=''
+      />
     </Container>
   );
 }
