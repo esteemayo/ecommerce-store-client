57476657248e73d7f048754d9292d797f4b7893d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-12 02:19:23 +0100

    fix: add markup

--- src/components/card/Button.jsx
+++ src/components/card/Button.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import styled, { css } from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
-const Button = () => {
+const Button = ({ onClick }) => {
   return (
-    <div>Button</div>
+    <StyledButton type='button' onClick={onClick}>
+      <FontAwesomeIcon icon={faShoppingCart} />
+      &nbsp;
+      add to cart
+    </StyledButton>
   );
 }
+
+const StyledButton = styled.button`
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 1.5rem 1rem;
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  background-color: ${({ theme }) => theme.bgBtn};
+  color: ${({ theme }) => theme.textBtn};
+  background-image: ${({ theme }) => css`
+    linear-gradient(
+      120deg,
+      transparent 0%,
+      transparent 50%,
+      ${theme.bgImgBtn} 50%
+    );
+  `};
+  background-size: 220%;
+  outline-color: ${({ theme }) => theme.btnOut};
+  border-radius: 0.5rem;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  @media only screen and (max-width: 64em) {
+    padding: 1.3rem 1rem;
+    font-size: 1.4rem;
+  }
+
+  @media only screen and (max-width: 56.25em), only screen and (hover: none) {
+    font-size: 1.498rem;
+  }
+
+  @media only screen and (max-width: 37.5em) {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    font-size: 1.4rem;
+  }
+
+  &:hover {
+    background-position: 100%;
+  }
+`;
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default Button;
