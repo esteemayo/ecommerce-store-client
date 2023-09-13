790862a984664b4b3f61976f49dd71bb7c959ac7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-13 01:47:03 +0100

    fix: modify

--- src/components/filters/Select.jsx
+++ src/components/filters/Select.jsx
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 import { Label } from './Label';
 import { Filter } from './Filter';
 
-const Select = ({ name, type, label, value, ...rest }) => {
+const Select = ({ name, type, label, options, ...rest }) => {
   return (
     <Filter>
       <Label htmlFor={name}>{label}</Label>
       <StyledSelect {...rest} id={name} name={name} type={type}>
-        {value}
+        {options}
       </StyledSelect>
     </Filter>
   );
@@ -58,7 +58,7 @@ Select.propTypes = {
   name: PropTypes.string,
   type: PropTypes.string,
   label: PropTypes.string,
-  value: PropTypes.any,
+  options: PropTypes.any,
   rest: PropTypes.any,
 };
 
