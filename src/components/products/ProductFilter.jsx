f891fa3fe346de6a21f30134b7b3cd558fe2ac8a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-13 01:22:07 +0100

    fix: refactor

--- src/components/products/ProductFilter.jsx
+++ src/components/products/ProductFilter.jsx
@@ -185,39 +185,6 @@ const Option = styled.option`
   color: inherit;
 `;
 
-const Input = styled.input`
-  display: inline-block;
-  width: 20rem;
-  padding: 3px;
-  font-family: inherit;
-  font-size: 1.4rem;
-  background-color: transparent;
-  color: ${({ theme }) => theme.textFilterSelect};
-  border: 1px solid #ddd;
-  border-radius: 3px;
-  outline-color: ${({ theme }) => theme.filterOut};
-
-  @media only screen and (max-width: 37.5em) {
-    width: 26rem;
-  }
-
-  @media only screen and (max-width: 31.25em) {
-    width: 40rem;
-  }
-
-  @media only screen and (max-width: 25em) {
-    width: 39rem;
-  }
-
-  @media only screen and (max-width: 21.875em) {
-    width: 37rem;
-  }
-
-  @media only screen and (max-width: 18.75em) {
-    width: 31rem;
-  }
-`;
-
 ProductFilter.propTypes = {
   products: PropTypes.array,
   price: PropTypes.number,
