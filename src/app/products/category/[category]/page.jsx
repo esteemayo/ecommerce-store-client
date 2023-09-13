33e7a88664dcfc6fe6034e53c1bf99252e62e351
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-13 02:42:13 +0100

    fix: refactor

--- src/app/products/category/[category]/page.jsx
+++ src/app/products/category/[category]/page.jsx
@@ -177,43 +177,4 @@ const FilterRight = styled.div`
   }
 `;
 
-// const Select = styled.select`
-//   display: inline-block;
-//   text-transform: capitalize;
-//   width: 20rem;
-//   padding: 3px;
-//   font-family: inherit;
-//   font-size: 1.4rem;
-//   background-color: transparent;
-//   color: ${({ theme }) => theme.textFilterSelect};
-//   border: 1px solid #ddd;
-//   border-radius: 3px;
-//   outline-color: ${({ theme }) => theme.filterOut};
-
-//   @media only screen and (max-width: 37.5em) {
-//     width: 60rem;
-//     padding: 0.7rem;
-//   }
-
-//   @media only screen and (max-width: 35em) {
-//     width: 54rem;
-//   }
-
-//   @media only screen and (max-width: 31.25em) {
-//     width: 40rem;
-//   }
-
-//   @media only screen and (max-width: 25em) {
-//     width: 39rem;
-//   }
-
-//   @media only screen and (max-width: 21.875em) {
-//     width: 37rem;
-//   }
-
-//   @media only screen and (max-width: 18.75em) {
-//     width: 31rem;
-//   }
-// `;
-
 export default ProductCategory;
