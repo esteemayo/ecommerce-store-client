9310f93a223389f4fce8af8f92f9d02272d9b0d2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-13 02:46:39 +0100

    fix: refactor

--- src/components/filters/SelectColor.jsx
+++ src/components/filters/SelectColor.jsx
@@ -24,45 +24,6 @@ const SelectColor = ({ name, label, options, ...rest }) => {
   );
 }
 
-const Select = styled.select`
-  display: inline-block;
-  text-transform: capitalize;
-  width: 20rem;
-  padding: 3px;
-  font-family: inherit;
-  font-size: 1.4rem;
-  background-color: transparent;
-  color: ${({ theme }) => theme.textFilterSelect};
-  border: 1px solid #ddd;
-  border-radius: 3px;
-  outline-color: ${({ theme }) => theme.filterOut};
-
-  @media only screen and (max-width: 37.5em) {
-    width: 60rem;
-    padding: 0.7rem;
-  }
-
-  @media only screen and (max-width: 35em) {
-    width: 54rem;
-  }
-
-  @media only screen and (max-width: 31.25em) {
-    width: 40rem;
-  }
-
-  @media only screen and (max-width: 25em) {
-    width: 39rem;
-  }
-
-  @media only screen and (max-width: 21.875em) {
-    width: 37rem;
-  }
-
-  @media only screen and (max-width: 18.75em) {
-    width: 31rem;
-  }
-`;
-
 SelectColor.propTypes = {
   name: PropTypes.string,
   label: PropTypes.string,
