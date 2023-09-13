785207a4c219432cf9e2c721b853831620b2c09e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-13 01:44:49 +0100

    fix: refactor

--- src/components/products/ProductFilter.jsx
+++ src/components/products/ProductFilter.jsx
@@ -133,40 +133,6 @@ const FilterWrapper = styled.div`
   }
 `;
 
-// const Select = styled.select`
-//   display: inline-block;
-//   text-transform: capitalize;
-//   width: 20rem;
-//   padding: 3px;
-//   font-family: inherit;
-//   font-size: 1.4rem;
-//   background-color: transparent;
-//   color: ${({ theme }) => theme.textFilterSelect};
-//   border: 1px solid #ddd;
-//   border-radius: 3px;
-//   outline-color: ${({ theme }) => theme.filterOut};
-
-//   @media only screen and (max-width: 37.5em) {
-//     width: 26rem;
-//   }
-
-//   @media only screen and (max-width: 31.25em) {
-//     width: 40rem;
-//   }
-
-//   @media only screen and (max-width: 25em) {
-//     width: 39rem;
-//   }
-
-//   @media only screen and (max-width: 21.875em) {
-//     width: 37rem;
-//   }
-
-//   @media only screen and (max-width: 18.75em) {
-//     width: 31rem;
-//   }
-// `;
-
 const Option = styled.option`
   text-transform: capitalize;
   background-color: ${({ theme }) => theme.bgProdCard};
