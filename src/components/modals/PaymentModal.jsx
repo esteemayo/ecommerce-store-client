1fc130f81b5b5a3141f4f00acee03e64540d96b8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-14 17:51:01 +0100

    fix: refactor

--- src/components/modals/PaymentModal.jsx
+++ src/components/modals/PaymentModal.jsx
@@ -199,15 +199,6 @@ const Form = styled.form`
   margin: 2rem 0;
 `;
 
-// const FormGroup = styled.div`
-//   display: flex;
-//   flex-direction: column;
-
-//   &:not(:last-of-type) {
-//     margin-bottom: 2rem;
-//   }
-// `;
-
 const FormLabel = styled.label`
   display: inline-block;
   width: 5rem;
