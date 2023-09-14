f14c3e081083e051bb909a24c0d087685db245ab
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-14 17:15:22 +0100

    fix: add a div element tag

--- src/components/modals/Overlay.jsx
+++ src/components/modals/Overlay.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Overlay = ({ type, mode, children, onClick }) => {
   return (
     <StyledOverlay>
-      {children}
+      <Container>{children}</Container>
     </StyledOverlay>
   );
 }
