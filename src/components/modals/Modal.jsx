22817a99230ce202e6d78900506fc5e100f3f25b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-14 17:40:15 +0100

    fix: refactor

--- src/components/modals/Modal.jsx
+++ src/components/modals/Modal.jsx
@@ -51,13 +51,6 @@ const Modal = ({ title, children, isOpen, onClose }) => {
   );
 }
 
-const Container = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
-
 const Wrapper = styled.div`
   width: 40rem;
   padding: 2rem 4rem;
