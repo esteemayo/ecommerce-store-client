2232b0204c6841260fdf64081ee16ab9251dc057
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-14 18:06:16 +0100

    fix: refactor

--- src/components/modals/DeleteModal.jsx
+++ src/components/modals/DeleteModal.jsx
@@ -75,13 +75,6 @@ const DeleteModal = ({ actionId, isOpen, onClose, onAction }) => {
   );
 }
 
-const Container = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
-
 const Wrapper = styled.div`
   width: 35rem;
   padding: 5rem;
