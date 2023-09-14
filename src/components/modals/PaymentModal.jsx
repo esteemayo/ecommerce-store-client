69aec1a50923d375ffddf4d0bf4b1056b7e5eeb6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-14 17:57:43 +0100

    fix: refactor

--- src/components/modals/PaymentModal.jsx
+++ src/components/modals/PaymentModal.jsx
@@ -12,6 +12,7 @@ import FormatPrice from '../FormatPrice';
 import FormError from '../form/FormError';
 import Form from '../form/Form';
 import { FormGroup } from '../form/FormGroup';
+import FormButton from '../form/FormButton';
 
 const initialState = {
   name: '',
@@ -132,7 +133,7 @@ const PaymentModal = ({ isOpen, onClose, onExit }) => {
             />
             {errors.address && <FormError message={errors.address} />}
           </FormGroup>
-          <FormButton type='submit'>Order</FormButton>
+          <FormButton label='Order' />
         </Form>
       </Wrapper>
     </Overlay>
@@ -261,44 +262,44 @@ const TextArea = styled.textarea`
   }
 `;
 
-const FormButton = styled.button`
-  border: none;
-  display: inline-block;
-  font-weight: 500;
-  font-size: 1.4rem;
-  width: 100%;
-  padding: 1.5rem;
-  background-color: ${({ theme }) => theme.bgBtn};
-  color: ${({ theme }) => theme.textBtn};
-  background-image: ${({ theme }) => css`
-    linear-gradient(
-      120deg,
-      transparent 0%,
-      transparent 50%,
-      ${theme.bgImgBtn} 50%
-    );
-  `};
-  background-size: 220%;
-  border-radius: 0.5rem;
-  outline-color: ${({ theme }) => theme.btnOut};
-  margin-top: 2rem;
-  cursor: pointer;
-  transition: all 0.3s;
+// const FormButton = styled.button`
+//   border: none;
+//   display: inline-block;
+//   font-weight: 500;
+//   font-size: 1.4rem;
+//   width: 100%;
+//   padding: 1.5rem;
+//   background-color: ${({ theme }) => theme.bgBtn};
+//   color: ${({ theme }) => theme.textBtn};
+//   background-image: ${({ theme }) => css`
+//     linear-gradient(
+//       120deg,
+//       transparent 0%,
+//       transparent 50%,
+//       ${theme.bgImgBtn} 50%
+//     );
+//   `};
+//   background-size: 220%;
+//   border-radius: 0.5rem;
+//   outline-color: ${({ theme }) => theme.btnOut};
+//   margin-top: 2rem;
+//   cursor: pointer;
+//   transition: all 0.3s;
 
-  @media only screen and (max-width: 56.25em), only screen and (hover: none) { 
-    padding-top: 1.4rem;
-    padding-bottom: 1.4rem;
-  }
+//   @media only screen and (max-width: 56.25em), only screen and (hover: none) { 
+//     padding-top: 1.4rem;
+//     padding-bottom: 1.4rem;
+//   }
 
-  &:hover {
-    background-position: 100%;
-  }
+//   &:hover {
+//     background-position: 100%;
+//   }
 
-  &:disabled {
-    opacity: 0.85;
-    cursor: not-allowed;
-  }
-`;
+//   &:disabled {
+//     opacity: 0.85;
+//     cursor: not-allowed;
+//   }
+// `;
 
 PaymentModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
