813f96cc278bc35839cb434aa1330a09e89cede5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-14 17:58:09 +0100

    fix: refactor

--- src/components/modals/PaymentModal.jsx
+++ src/components/modals/PaymentModal.jsx
@@ -262,45 +262,6 @@ const TextArea = styled.textarea`
   }
 `;
 
-// const FormButton = styled.button`
-//   border: none;
-//   display: inline-block;
-//   font-weight: 500;
-//   font-size: 1.4rem;
-//   width: 100%;
-//   padding: 1.5rem;
-//   background-color: ${({ theme }) => theme.bgBtn};
-//   color: ${({ theme }) => theme.textBtn};
-//   background-image: ${({ theme }) => css`
-//     linear-gradient(
-//       120deg,
-//       transparent 0%,
-//       transparent 50%,
-//       ${theme.bgImgBtn} 50%
-//     );
-//   `};
-//   background-size: 220%;
-//   border-radius: 0.5rem;
-//   outline-color: ${({ theme }) => theme.btnOut};
-//   margin-top: 2rem;
-//   cursor: pointer;
-//   transition: all 0.3s;
-
-//   @media only screen and (max-width: 56.25em), only screen and (hover: none) { 
-//     padding-top: 1.4rem;
-//     padding-bottom: 1.4rem;
-//   }
-
-//   &:hover {
-//     background-position: 100%;
-//   }
-
-//   &:disabled {
-//     opacity: 0.85;
-//     cursor: not-allowed;
-//   }
-// `;
-
 PaymentModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
