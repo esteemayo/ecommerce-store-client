4aa11e01fd0eb16c78954fa1c8b6974ee73efcb5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-14 17:50:00 +0100

    fix: refactor

--- src/components/modals/PaymentModal.jsx
+++ src/components/modals/PaymentModal.jsx
@@ -310,12 +310,6 @@ const FormButton = styled.button`
   }
 `;
 
-const ErrorMsg = styled.span`
-  font-size: 1.3rem;
-  color: var(--clr-secondary-red);
-  margin-top: 1px;
-`;
-
 PaymentModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
