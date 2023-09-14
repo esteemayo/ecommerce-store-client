4c3417ccbb78557630b66e9f83afdcf07aa6fcd5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-14 19:22:32 +0100

    fix: refactor

--- src/components/modals/ReviewModal.jsx
+++ src/components/modals/ReviewModal.jsx
@@ -114,13 +114,6 @@ const ReviewModal = ({ isOpen, onClose }) => {
   );
 }
 
-const Container = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
-
 const Wrapper = styled.div`
   width: 40rem;
   padding: 2rem 4rem;
