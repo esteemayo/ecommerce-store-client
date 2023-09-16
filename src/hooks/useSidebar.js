067382c4a4adae044bd50b604b8463df274e91fa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-16 02:57:59 +0100

    fix: add openSidebarHandler function

--- src/hooks/useSidebar.js
+++ src/hooks/useSidebar.js
@@ -6,6 +6,10 @@ import {closeSidebar,  openSidebar } from '@/features/submenu/submenuSlice';
 export const useSidebar = () => {
   const dispatch = useDispatch();
 
+  const openSidebarHandler = useCallback(() => {
+    dispatch(openSidebar());
+  }, [dispatch]);
+
   return (
     <div>useSidebar</div>
   );
