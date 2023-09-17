b1039ba110e70c6bd5d36d64f0bb1472fbbaceef
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-17 18:11:35 +0100

    fix: add handleChange function

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -3,6 +3,10 @@ import { useCallback, useState } from 'react';
 export const useSearch = () => {
   const [searchQuery, setSearchQuery] = useState('');
 
+  const handleChange = useCallback((e) => {
+    setSearchQuery(e.target.value);
+  }, []);
+
   const handleSearch = useCallback((e) => {
     e.preventDefault();
     console.log(searchQuery);
