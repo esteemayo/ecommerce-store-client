356208d9f9750ae78e779551f4d0a4de73a3cb53
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-17 19:46:20 +0100

    fix: add useEffect react hook

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { getFromStorage, searchKey, setToStorage } from '@/utils';
 
 const getAllHistories = () => {
