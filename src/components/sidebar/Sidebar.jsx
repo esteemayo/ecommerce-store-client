ed64a60f4e60497d5bf476afa24b6ed0cedf1ad0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-17 17:53:39 +0100

    fix: refactor

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useSelector } from 'react-redux';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { useCallback, useState, useMemo } from 'react';
+import { useMemo } from 'react';
 
 import { sublinks } from '@/data';
 import { useSidebar } from '@/hooks/useSidebar';
