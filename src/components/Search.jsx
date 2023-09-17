383c72253fa0a1c25cca188d9f8386783bf07ceb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-17 18:58:26 +0100

    fix: refactor

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -25,10 +25,6 @@ const Search = () => {
   // }, [handleSearch, handleHistory]);
 
   useEffect(() => {
-    setToStorage(searchKey, histories);
-  }, [histories]);
-
-  useEffect(() => {
     inputRef.current.focus();
   }, []);
 
