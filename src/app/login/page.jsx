701a573e6653c872b5bfc0cddaf19896ab722b61
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-18 13:17:48 +0100

    fix: refactor

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -164,31 +164,6 @@ const FormLabel = styled.label`
   color: ${({ theme }) => theme.textLabel};
 `;
 
-// const FormInput = styled.input`
-//   border: none;
-//   display: inline-block;
-//   font-family: inherit;
-//   font-size: 1.5rem;
-//   width: 100%;
-//   padding: 1.5rem 1rem;
-//   background-color: ${({ theme }) => theme.bgInput};
-//   color: ${({ theme }) => theme.textInput};
-//   border-radius: 0.5rem;
-//   outline-color:  ${({ theme }) => theme.inputOut};
-//   caret-color: ${({ theme }) => theme.inputCaret};
-//   transition: all 0.3s ease;
-
-//   &:focus {
-//     background-color: transparent;
-//   }
-
-//   &::-webkit-input-placeholder {
-//     font-weight: 300;
-//     font-size: 1.5rem;
-//     color: #bbb;
-//   }
-// `;
-
 const CheckBoxWrapper = styled.div`
   width: 100%;
   display: flex;
