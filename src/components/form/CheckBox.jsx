1a6a8d770c3e7d69ac20075b2fb148e99c369d49
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-18 13:30:44 +0100

    fix: create CheckBox component

---
+++ src/components/form/CheckBox.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+
+const CheckBox = ({ name, label, ...rest }) => {
+  const { mode } = useSelector((state) => ({ ...state.darkMode }));
+
+  const checkmarkClasses = useMemo(() => {
+    if (mode) {
+      return `checkmark dark`;
+    }
+
+    return `checkmark light`;
+  }, [mode]);
+
+  return (
+    <Container className='checkContainer'>
+      <StyledCheckBox
+        type='checkbox'
+        id={name}
+        name={name}
+        className='checkbox'
+      />
+      <CheckMark className={checkmarkClasses} />
+      <Label htmlFor={name}>{label}</Label>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+`;
+
+const StyledCheckBox = styled.input``;
+
+const Label = styled.label`
+  width: 10rem;
+  display: inline-block;
+  text-transform: capitalize;
+  font-weight: 600;
+  font-size: 1.4rem;
+  color: ${({ theme }) => theme.textLabel};
+  margin-left: 3.5rem;
+`;
+
+const CheckMark = styled.span``;
+
+export default CheckBox;
