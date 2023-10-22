3ba2a25a568376ef42a2a00f9d9f190438fa74f2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-22 03:07:16 +0100

    fix: add redux devtools

--- src/hooks/useReviewModal.js
+++ src/hooks/useReviewModal.js
@@ -1,9 +1,12 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
-const useReviewModal = create((set) => ({
-  isOpen: false,
-  onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: false }),
-}));
+const useReviewModal = create(
+  devtools((set) => ({
+    isOpen: false,
+    onOpen: () => set(() => ({ isOpen: true }), false, 'openReviewModal'),
+    onClose: () => set(() => ({ isOpen: false }), false, 'closeReviewModal'),
+  }))
+);
 
 export default useReviewModal;
