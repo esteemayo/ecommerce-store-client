fb89450a6885a5dc63ea1e23b7b5782a8deea967
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-22 03:02:25 +0100

    fix: add redux devtools

--- src/hooks/useFileModal.js
+++ src/hooks/useFileModal.js
@@ -1,9 +1,12 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
-const useFileModal = create((set) => ({
-  isOpen: false,
-  onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: false }),
-}));
+const useFileModal = create(
+  devtools((set) => ({
+    isOpen: false,
+    onOpen: () => set(() => ({ isOpen: true }), false, 'openFileModal'),
+    onClose: () => set(() => ({ isOpen: false }), false, 'closeFileModal'),
+  }))
+);
 
 export default useFileModal;
