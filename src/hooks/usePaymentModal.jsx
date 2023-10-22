ac6a3a0fad0ff6b6cd663fb38939ce76b572ef50
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-22 03:06:04 +0100

    fix: add redux devtools

--- src/hooks/usePaymentModal.jsx
+++ src/hooks/usePaymentModal.jsx
@@ -1,9 +1,12 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
-const usePaymentModal = create((set) => ({
-  isOpen: false,
-  onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: false }),
-}));
+const usePaymentModal = create(
+  devtools((set) => ({
+    isOpen: false,
+    onOpen: () => set(() => ({ isOpen: true }), false, 'openPaymentModal'),
+    onClose: () => set(() => ({ isOpen: false }), false, 'closePaymentModal'),
+  }))
+);
 
 export default usePaymentModal;
