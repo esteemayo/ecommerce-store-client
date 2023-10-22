9fd64d553914b6966a6a9d6a4d91ca3055534380
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-22 03:04:53 +0100

    fix: add redux devtools

--- src/hooks/usePasswordModal.js
+++ src/hooks/usePasswordModal.js
@@ -1,9 +1,12 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
-const usePasswordModal = create((set) => ({
-  isOpen: false,
-  onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: false }),
-}));
+const usePasswordModal = create(
+  devtools((set) => ({
+    isOpen: false,
+    onOpen: () => set(() => ({ isOpen: true }), false, 'openPasswordModal'),
+    onClose: () => set(() => ({ isOpen: false }), false, 'closePasswordModal'),
+  }))
+);
 
 export default usePasswordModal;
