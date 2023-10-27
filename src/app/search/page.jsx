290b0e45c2114e94970a66838f2b54e2e746b05e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-27 20:59:32 +0100

    fix: modify

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -3,26 +3,24 @@
 import { useState } from 'react';
 import dynamic from 'next/dynamic';
 
-import ClientOnly from '@/components/ClientOnly';
+import { storeProducts } from '@/data';
 import ProductBox from '@/components/products/ProductBox';
 
-import { storeProducts } from '@/data';
-
-const ProductList = dynamic(() => import('@/components/products/ProductList'), { ssr: false });
+const ProductList = dynamic(() => import('@/components/products/ProductList'), {
+  ssr: false,
+});
 
 const Search = () => {
   const [products, setProducts] = useState(storeProducts);
 
   return (
-    <ClientOnly>
-      <ProductBox>
-        <ProductList
-          products={products}
-          title='No result matches your search criteria'
-        />
-      </ProductBox>
-    </ClientOnly>
+    <ProductBox>
+      <ProductList
+        products={products}
+        title='No result matches your search criteria'
+      />
+    </ProductBox>
   );
-}
+};
 
 export default Search;
