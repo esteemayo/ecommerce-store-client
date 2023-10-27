a38f1640a495d266ec8414fa1032165e82428e8c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-27 20:57:57 +0100

    fix: modify

--- src/app/forgot/page.jsx
+++ src/app/forgot/page.jsx
@@ -9,8 +9,6 @@ import Form from '@/components/form/Form';
 import { StyledBox } from '@/components/form/StyledBox';
 import Heading from '@/components/form/Heading';
 
-import ClientOnly from '@/components/ClientOnly';
-
 const Forgot = () => {
   const [email, setEmail] = useState(null);
   const [errors, setErrors] = useState({});
@@ -21,7 +19,8 @@ const Forgot = () => {
     if (!email) {
       errors.email = 'Please enter your email address';
     } else {
-      const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)*[a-zA-Z]{2,9})$/;
+      const regEx =
+        /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)*[a-zA-Z]{2,9})$/;
       if (!email.match(regEx)) {
         errors.email = 'Email must be a valid email address';
       }
@@ -30,40 +29,38 @@ const Forgot = () => {
     return errors;
   }, [email]);
 
-  const handleSubmit = useCallback((e) => {
-    e.preventDefault();
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
 
-    const errors = validateForm();
-    if (Object.keys(errors).length > 0) return setErrors(errors);
-    setErrors({});
+      const errors = validateForm();
+      if (Object.keys(errors).length > 0) return setErrors(errors);
+      setErrors({});
 
-    console.log(email);
-  }, [email, validateForm]);
+      console.log(email);
+    },
+    [email, validateForm]
+  );
 
   return (
-    <ClientOnly>
-      <FormBox>
-        <StyledBox>
-          <Heading
-            small
-            title='Forgot password'
+    <FormBox>
+      <StyledBox>
+        <Heading small title='Forgot password' />
+        <Form onSubmit={handleSubmit}>
+          <FormInput
+            type='email'
+            name='email'
+            label='Email'
+            placeholder='Enter email address'
+            onChange={(e) => setEmail(e.target.value)}
+            error={errors.email}
+            autoFocus
           />
-          <Form onSubmit={handleSubmit}>
-            <FormInput
-              type='email'
-              name='email'
-              label='Email'
-              placeholder='Enter email address'
-              onChange={(e) => setEmail(e.target.value)}
-              error={errors.email}
-              autoFocus
-            />
-            <FormButton label='Reset password' />
-          </Form>
-        </StyledBox>
-      </FormBox>
-    </ClientOnly>
+          <FormButton label='Reset password' />
+        </Form>
+      </StyledBox>
+    </FormBox>
   );
-}
+};
 
 export default Forgot;
