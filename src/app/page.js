d906d1114741a0468db40823c557a6cb8c34d290
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-27 20:55:23 +0100

    fix: modify

--- src/app/page.js
+++ src/app/page.js
@@ -3,29 +3,33 @@
 import styled from 'styled-components';
 import dynamic from 'next/dynamic';
 
-import ClientOnly from '@/components/ClientOnly';
 import CarouselSlider from '@/components/sliders/CarouselSlider';
 
 import { useSubmenu } from '@/hooks/useSubmenu';
 
-const Categories = dynamic(() => import('@/components/categories/Categories'), { ssr: false });
-const FeaturedProducts = dynamic(() => import('@/components/products/FeaturedProducts'), { ssr: false });
-const TopReviews = dynamic(() => import('@/components/reviews/TopReviews'), { ssr: false });
+const Categories = dynamic(() => import('@/components/categories/Categories'), {
+  ssr: false,
+});
+const FeaturedProducts = dynamic(
+  () => import('@/components/products/FeaturedProducts'),
+  { ssr: false }
+);
+const TopReviews = dynamic(() => import('@/components/reviews/TopReviews'), {
+  ssr: false,
+});
 
 const Home = () => {
   const closeSubmenu = useSubmenu((state) => state.closeSubmenu);
 
   return (
-    <ClientOnly>
-      <Container onMouseOver={closeSubmenu}>
-        <CarouselSlider />
-        <Categories />
-        <FeaturedProducts />
-        <TopReviews />
-      </Container>
-    </ClientOnly>
+    <Container onMouseOver={closeSubmenu}>
+      <CarouselSlider />
+      <Categories />
+      <FeaturedProducts />
+      <TopReviews />
+    </Container>
   );
-}
+};
 
 const Container = styled.main``;
 
