4cefa9eac31be0138151a0267a3ab666c80ed893
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-28 03:51:25 +0100

    fix: add type annotation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata = {
   keywords: '',
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children } : { children: React.ReactNode }) {
   return (
     <html lang='en'>
       {/*
