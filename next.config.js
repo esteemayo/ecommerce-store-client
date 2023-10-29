dbef13c03811c34c8ed2f6d604936e1c5e34ac8d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-29 23:30:49 +0100

    fix: add configuration

--- next.config.js
+++ next.config.js
@@ -9,10 +9,13 @@ const nextConfig = {
   images: {
     domains: ['images.pexels.com', 'images.unsplash.com']
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   env: {
     // NEXT_PUBLIC_DEV_API_URL: process.env.NEXT_PUBLIC_DEV_API_URL,
     // NEXT_PUBLIC_PROD_API_URL: process.env.NEXT_PUBLIC_PROD_API_URL,
-  }
+  },
 }
 
 module.exports = nextConfig
