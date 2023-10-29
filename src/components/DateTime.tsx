880cb4b3233dfac283c444826f60bb8183995ed7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-29 19:35:13 +0100

    fix: add type annotation

--- src/components/DateTime.tsx
+++ src/components/DateTime.tsx
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 
+interface ITime {
+  type?: string;
+}
+
 const DateTime = ({ date, type, options }) => {
   const dateLabel = useMemo(() => {
     return new Date(date).toLocaleString('en-us', options);
@@ -12,11 +16,12 @@ const DateTime = ({ date, type, options }) => {
       {dateLabel}
     </Time>
   );
-}
+};
 
-const Time = styled.time`
-  font-size: ${({ type }) => type === 'review' ? '1.4rem' : '1.5rem'};
-  color: ${({ theme, type }) => type === 'review' ? theme.textRevDate : theme.textWlHeader};
+const Time = styled.time<ITime>`
+  font-size: ${({ type }) => (type === 'review' ? '1.4rem' : '1.5rem')};
+  color: ${({ theme, type }) =>
+    type === 'review' ? theme.textRevDate : theme.textWlHeader};
 
   @media only screen and (max-width: 56.25em) {
     font-size: ${({ type }) => type !== 'review' && '1.3rem'};
