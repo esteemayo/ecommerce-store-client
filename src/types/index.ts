d82a92b39e371207158f7167e965924b6bba80e1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-30 07:20:28 +0100

    fix: modify

--- src/types/index.ts
+++ src/types/index.ts
@@ -857,7 +857,7 @@ export interface DeleteAccountProps {
 }
 
 export interface ModalProps {
-  title: string;
+  title?: string;
   children: React.ReactNode;
   isOpen: boolean;
   onClose(): void;
