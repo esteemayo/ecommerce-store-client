d01ce1372c3c54396e2d2b5a52dc0ca55435310d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-31 20:33:16 +0100

    fix: modify

--- src/hooks/usePasswordModal.ts
+++ src/hooks/usePasswordModal.ts
@@ -1,13 +1,28 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
+import { produce } from 'immer';
 
 import { PasswordModalStore } from '@/types';
 
 const usePasswordModal = create<PasswordModalStore>()(
   devtools((set) => ({
     isOpen: false,
-    onOpen: () => set(() => ({ isOpen: true }), false, 'openPasswordModal'),
-    onClose: () => set(() => ({ isOpen: false }), false, 'closePasswordModal'),
+    onOpen: () =>
+      set(
+        produce((state) => {
+          state.isOpen = true;
+        }),
+        false,
+        'openPasswordModal'
+      ),
+    onClose: () =>
+      set(
+        produce((state) => {
+          state.isOpen = false;
+        }),
+        false,
+        'closePasswordModal'
+      ),
   }))
 );
 
