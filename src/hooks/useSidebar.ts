0afb77afc5dfc4cf8a0cc155f7095f2756731ac9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-31 20:12:08 +0100

    fix: modify

--- src/hooks/useSidebar.ts
+++ src/hooks/useSidebar.ts
@@ -1,12 +1,27 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
+import { produce } from 'immer';
 
 import { SidebarStore } from '@/types';
 
 export const useSidebar = create<SidebarStore>()(
   devtools((set) => ({
     isOpen: false,
-    onOpen: () => set(() => ({ isOpen: true }), false, 'openSidebar'),
-    onClose: () => set(() => ({ isOpen: false }), false, 'closeSidebar'),
+    onOpen: () =>
+      set(
+        produce((state) => {
+          state.isOpen = true;
+        }),
+        false,
+        'openSidebar'
+      ),
+    onClose: () =>
+      set(
+        produce((state) => {
+          state.isOpen = false;
+        }),
+        false,
+        'closeSidebar'
+      ),
   }))
 );
