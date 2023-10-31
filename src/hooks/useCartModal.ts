5ec7782b89d5af97c0dd3eba345c211909056391
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-31 20:28:12 +0100

    fix: modify

--- src/hooks/useCartModal.ts
+++ src/hooks/useCartModal.ts
@@ -1,12 +1,27 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
+import { produce } from 'immer';
 
 import { CartModalStore } from '@/types';
 
 export const useCartModal = create<CartModalStore>()(
   devtools((set) => ({
     isOpen: false,
-    onOpen: () => set(() => ({ isOpen: true }), false, 'openCartModal'),
-    onClose: () => set(() => ({ isOpen: false }), false, 'closeCartModal'),
+    onOpen: () =>
+      set(
+        produce((state) => {
+          state.isOpen = true;
+        }),
+        false,
+        'openCartModal'
+      ),
+    onClose: () =>
+      set(
+        produce((state) => {
+          state.isOpen = false;
+        }),
+        false,
+        'closeCartModal'
+      ),
   }))
 );
