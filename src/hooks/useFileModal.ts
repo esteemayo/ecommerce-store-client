3f098c733c429b1c651752717ec76ac125df1e81
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-31 20:21:17 +0100

    fix: modify

--- src/hooks/useFileModal.ts
+++ src/hooks/useFileModal.ts
@@ -1,13 +1,28 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
+import { produce } from 'immer';
 
 import { FileModalStore } from '@/types';
 
 const useFileModal = create<FileModalStore>()(
   devtools((set) => ({
     isOpen: false,
-    onOpen: () => set(() => ({ isOpen: true }), false, 'openFileModal'),
-    onClose: () => set(() => ({ isOpen: false }), false, 'closeFileModal'),
+    onOpen: () =>
+      set(
+        produce((state) => {
+          state.isOpen = true;
+        }),
+        false,
+        'openFileModal'
+      ),
+    onClose: () =>
+      set(
+        produce((state) => {
+          state.isOpen = false;
+        }),
+        false,
+        'closeFileModal'
+      ),
   }))
 );
 
