f0ea4bc1f3095043e19766242400d30594bee553
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-31 20:30:11 +0100

    fix: modify

--- src/hooks/useAccountModal.ts
+++ src/hooks/useAccountModal.ts
@@ -1,13 +1,28 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
+import { produce } from 'immer';
 
 import { AccountModalStore } from '@/types';
 
 const useAccountModal = create<AccountModalStore>()(
   devtools((set) => ({
     isOpen: false,
-    onOpen: () => set(() => ({ isOpen: true }), false, 'openAccountModal'),
-    onClose: () => set(() => ({ isOpen: false }), false, 'closeAccountModal'),
+    onOpen: () =>
+      set(
+        produce((state) => {
+          state.isOpen = true;
+        }),
+        false,
+        'openAccountModal'
+      ),
+    onClose: () =>
+      set(
+        produce((state) => {
+          state.isOpen = false;
+        }),
+        false,
+        'closeAccountModal'
+      ),
   }))
 );
 
