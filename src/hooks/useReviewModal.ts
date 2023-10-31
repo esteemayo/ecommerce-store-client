a3290122c90647bc13abb27afde78f775f389eed
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-31 20:07:37 +0100

    fix: modify

--- src/hooks/useReviewModal.ts
+++ src/hooks/useReviewModal.ts
@@ -1,13 +1,28 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
+import { produce } from 'immer';
 
 import { ReviewModalStore } from '@/types';
 
 const useReviewModal = create<ReviewModalStore>()(
   devtools((set) => ({
     isOpen: false,
-    onOpen: () => set(() => ({ isOpen: true }), false, 'openReviewModal'),
-    onClose: () => set(() => ({ isOpen: false }), false, 'closeReviewModal'),
+    onOpen: () =>
+      set(
+        produce((state) => {
+          state.isOpen = true;
+        }),
+        false,
+        'openReviewModal'
+      ),
+    onClose: () =>
+      set(
+        produce((state) => {
+          state.isOpen = false;
+        }),
+        false,
+        'closeReviewModal'
+      ),
   }))
 );
 
