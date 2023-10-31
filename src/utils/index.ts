f4cd5374678207938e77a094f7bc89055050bdbe
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-31 17:20:08 +0100

    fix: add type annotation

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,32 +4,34 @@ export const rememberKey = 'rememberMe';
 
 export const searchKey = 'histories';
 
-export const excerpts = (str, count) => {
+export const excerpts = (str: string, count: number) => {
   if (str.length > count) {
     str = str.substr(0, count) + ' ...';
   }
   return str;
-}
+};
 
-export const getUnique = (items, value) => {
-  const newItems = items.map((item) => item[value]).flat();
+export const getUnique = (items: any, value: string) => {
+  const newItems = items
+    .map((item: { [x: string]: any }) => item[value])
+    .flat();
   return [...new Set(newItems)];
-}
+};
 
-export const getFromStorage = (key) => {
+export const getFromStorage = (key: string) => {
   if (typeof window !== 'undefined') {
     return JSON.parse(window.localStorage.getItem(key));
   }
-}
+};
 
-export const setToStorage = (key, value) => {
+export const setToStorage = (key: string, value: any) => {
   if (typeof window !== 'undefined') {
     return window.localStorage.setItem(key, JSON.stringify(value));
   }
-}
+};
 
-export const removeFromStorage = (key) => {
+export const removeFromStorage = (key: string) => {
   if (typeof window !== 'undefined') {
     return window.localStorage.removeItem(key);
   }
-}
+};
