0a7f905c88a80c9b8093070ea609048a7b7952dd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-31 20:19:13 +0100

    fix: modify

--- src/hooks/useWishlistModal.ts
+++ src/hooks/useWishlistModal.ts
@@ -1,13 +1,28 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
+import { produce } from 'immer';
 
 import { WishlistModalStore } from '@/types';
 
 const useWishlistModal = create<WishlistModalStore>()(
   devtools((set) => ({
     isOpen: false,
-    onOpen: () => set(() => ({ isOpen: true }), false, 'openWishlistModal'),
-    onClose: () => set(() => ({ isOpen: false }), false, 'closeWishlistModal'),
+    onOpen: () =>
+      set(
+        produce((state) => {
+          state.isOpen = true;
+        }),
+        false,
+        'openWishlistModal'
+      ),
+    onClose: () =>
+      set(
+        produce((state) => {
+          state.isOpen = false;
+        }),
+        false,
+        'closeWishlistModal'
+      ),
   }))
 );
 
