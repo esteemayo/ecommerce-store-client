18ccbf08a7e6ca50ac618fe17e541707c73d0e01
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-02 02:53:30 +0100

    feat: create Annoucement component

---
+++ src/components/Annoucement.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Annoucement = () => {
+  return <Container>Annoucement</Container>;
+};
+
+const Container = styled.section``;
+
+export default Annoucement;
