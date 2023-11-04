f1b226c07bc6c8f3a7bc4591fafa6908d27ee957
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-04 04:56:47 +0100

    fix: modify

--- src/components/modals/ReviewModal.tsx
+++ src/components/modals/ReviewModal.tsx
@@ -2,13 +2,12 @@
 
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { FC, useCallback, useEffect, useMemo, useState } from 'react';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FC, useCallback, useEffect, useMemo, useState } from 'react';
 
 import { ReviewModalProps } from '@/types';
 import { useDarkMode } from '@/hooks/useDarkMode';
 
-import StarRating from '../StarRating';
 import CreateReview from '../reviews/CreateReview';
 
 interface IOverlay {
