b1b65b17e0bc9eb7279b1915687c60aa04894294
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-05 03:13:30 +0100

    fix: modify

--- src/components/icons/HeartIcon.tsx
+++ src/components/icons/HeartIcon.tsx
@@ -1,12 +1,11 @@
 import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
-import { FC } from 'react';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 interface HeartIconProps {
   isWished: boolean;
 }
 
-const HeartIcon: FC<HeartIconProps> = ({ isWished }) => {
+const HeartIcon = ({ isWished }: HeartIconProps) => {
   return isWished ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />;
 };
 
