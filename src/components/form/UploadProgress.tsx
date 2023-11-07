409164a7bc1e138f0509d32e7dd184bfce4fd0a4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-07 14:51:40 +0100

    fix: modify

--- src/components/form/UploadProgress.tsx
+++ src/components/form/UploadProgress.tsx
@@ -1,5 +1,6 @@
+'use client';
+
 import styled from 'styled-components';
-import { FC } from 'react';
 
 interface UploadProgressProps {
   type?: boolean;
@@ -10,7 +11,7 @@ interface ISpan {
   type?: boolean;
 }
 
-const UploadProgress: FC<UploadProgressProps> = ({ type, percentage }) => {
+const UploadProgress = ({ type, percentage }: UploadProgressProps) => {
   return <Container type={type}>Uploading: {percentage}%</Container>;
 };
 
