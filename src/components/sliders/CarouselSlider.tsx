e51d3e6bfc838c4a8487bec37aba9b22c3e25ee2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-09 00:57:08 +0100

    fix: refactor

--- src/components/sliders/CarouselSlider.tsx
+++ src/components/sliders/CarouselSlider.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useCallback, useEffect, useState } from 'react';
 import {
   faChevronLeft,
