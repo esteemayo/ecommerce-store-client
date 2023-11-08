b5d53d21d42d006b99b263fcd9ccc6198834903e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-08 02:38:35 +0100

    fix: refactor

---
+++ src/app/HomePage.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import styled from 'styled-components';
+import dynamic from 'next/dynamic';
+
+import CarouselSlider from '@/components/sliders/CarouselSlider';
+
+import { useSubmenu } from '@/hooks/useSubmenu';
+
+const Categories = dynamic(() => import('@/components/categories/Categories'), {
+  ssr: false,
+});
+const FeaturedProducts = dynamic(
+  () => import('@/components/products/FeaturedProducts'),
+  { ssr: false }
+);
+const TopReviews = dynamic(() => import('@/components/reviews/TopReviews'), {
+  ssr: false,
+});
+
+const HomePage = () => {
+  const closeSubmenu = useSubmenu((state) => state.closeSubmenu);
+
+  return (
+    <Container onMouseOver={closeSubmenu}>
+      <CarouselSlider />
+      <Categories />
+      <FeaturedProducts />
+      <TopReviews />
+    </Container>
+  );
+};
+
+const Container = styled.main``;
+
+export default HomePage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,7 @@
-'use client';
-
-import styled from 'styled-components';
-import dynamic from 'next/dynamic';
-
-import CarouselSlider from '@/components/sliders/CarouselSlider';
-
-import { useSubmenu } from '@/hooks/useSubmenu';
-
-const Categories = dynamic(() => import('@/components/categories/Categories'), {
-  ssr: false,
-});
-const FeaturedProducts = dynamic(
-  () => import('@/components/products/FeaturedProducts'),
-  { ssr: false }
-);
-const TopReviews = dynamic(() => import('@/components/reviews/TopReviews'), {
-  ssr: false,
-});
+import HomePage from './HomePage';
 
 const Home = () => {
-  const closeSubmenu = useSubmenu((state) => state.closeSubmenu);
-
-  return (
-    <Container onMouseOver={closeSubmenu}>
-      <CarouselSlider />
-      <Categories />
-      <FeaturedProducts />
-      <TopReviews />
-    </Container>
-  );
+  return <HomePage />;
 };
-
-const Container = styled.main``;
 
 export default Home;
