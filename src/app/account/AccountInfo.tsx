6f98032b2679716d2a3fcf61ace1ecaf05e14f18
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-10 04:44:07 +0100

    fix: add markup

--- src/app/account/AccountInfo.tsx
+++ src/app/account/AccountInfo.tsx
@@ -2,8 +2,53 @@
 
 import styled from 'styled-components';
 
-const AccountInfo = () => {
-  return <div>AccountInfo</div>;
+import AccountUpload from './AccountUpload';
+import AccountHead from './AccountHead';
+import DeactivateAccount from './DeactivateAccount';
+
+const AccountInfo = ({
+  user,
+  accountModal,
+  emailModal,
+  fileModal,
+  passwordModal,
+}) => {
+  return (
+    <Container>
+      <Left>
+        <AccountHead
+          onOpen={emailModal.onOpen}
+          onAction={passwordModal.onOpen}
+        />
+        <DeactivateAccount onOpen={accountModal.onOpen} />
+      </Left>
+      <Right>
+        <AccountUpload onOpen={fileModal.onOpen} currentUser={user} />
+      </Right>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 5rem;
+
+  @media only screen and (max-width: 43.75em) {
+    flex-direction: column-reverse;
+  }
+`;
+
+const Left = styled.div`
+  flex: 1.5;
+`;
+
+const Right = styled.div`
+  flex: 1;
+
+  @media only screen and (max-width: 43.75em) {
+    width: 100%;
+  }
+`;
 
 export default AccountInfo;
