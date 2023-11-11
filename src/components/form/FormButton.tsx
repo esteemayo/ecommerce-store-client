6af717a2d90f43a414a0d1246703c78273dc3c5c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-11 05:33:02 +0100

    fix: refactor

--- src/components/form/FormButton.tsx
+++ src/components/form/FormButton.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import { FormButtonProps } from '@/types';
+import { CommonButton } from '../buttons/CommonButton';
 
 const FormButton = ({ label, disabled, ...rest }: FormButtonProps) => {
   return (
@@ -12,7 +13,7 @@ const FormButton = ({ label, disabled, ...rest }: FormButtonProps) => {
   );
 };
 
-const Button = styled.button`
+const Button = styled(CommonButton)`
   margin-top: 2rem;
   border: none;
   display: inline-block;
@@ -20,28 +21,9 @@ const Button = styled.button`
   font-size: 1.4rem;
   width: 100%;
   padding: 1.5rem;
-  background-color: ${({ theme }) => theme.bgBtn};
-  color: ${({ theme }) => theme.textBtn};
-  background-image: ${({ theme }) => css`
-    linear-gradient(
-      120deg,
-      transparent 0%,
-      transparent 50%,
-      ${theme.bgImgBtn} 50%
-    );
-  `};
-  background-size: 220%;
-  border-radius: 0.5rem;
-  outline-color: ${({ theme }) => theme.btnOut};
-  cursor: pointer;
-  transition: all 0.3s;
 
   @media only screen and (max-width: 56.25em), only screen and (hover: none) {
     font-size: 1.398rem;
-  }
-
-  &:hover {
-    background-position: 100%;
   }
 
   &:disabled {
