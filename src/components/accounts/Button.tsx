4f02b730e59b23eeea7ad2bca6ea80624321e2b8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-11 06:43:43 +0100

    fix: refactor

--- src/components/accounts/Button.tsx
+++ src/components/accounts/Button.tsx
@@ -1,6 +1,7 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import { AccountButtonProps } from '@/types';
+import { CommonButton } from '../buttons/CommonButton';
 
 const Button = ({ text, disabled }: AccountButtonProps) => {
   return (
@@ -10,7 +11,7 @@ const Button = ({ text, disabled }: AccountButtonProps) => {
   );
 };
 
-const StyledButton = styled.button`
+const StyledButton = styled(CommonButton)`
   display: inline-block;
   border: none;
   text-transform: uppercase;
@@ -18,28 +19,10 @@ const StyledButton = styled.button`
   font-size: 1.4rem;
   width: 50%;
   padding: 1rem;
-  background-color: ${({ theme }) => theme.bgBtn};
   color: ${({ theme }) => theme.textModalDelBtn};
-  background-image: ${({ theme }) => css`
-    linear-gradient(
-      120deg,
-      transparent 0%,
-      transparent 50%,
-      ${theme.bgImgBtn} 50%
-    );
-  `};
-  background-size: 220%;
-  border-radius: 0.5rem;
-  outline-color: ${({ theme }) => theme.btnOut};
-  cursor: pointer;
-  transition: all 0.3s;
 
   @media only screen and (max-width: 56.25em), only screen and (hover: none) {
     font-size: 1.398rem;
-  }
-
-  &:hover {
-    background-position: 100%;
   }
 
   &:disabled {
