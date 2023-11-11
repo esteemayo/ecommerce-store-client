b52cfd4d47bc6654bad7103cbdfa88c5975f2ef6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-11 04:02:18 +0100

    fix: add markup

--- src/components/wishlists/WishlistInfo.tsx
+++ src/components/wishlists/WishlistInfo.tsx
@@ -1,9 +1,95 @@
 'use client';
 
 import styled from 'styled-components';
+import Link from 'next/link';
+import Image from 'next/image';
 
-const WishlistInfo = () => {
-  return <div>WishlistInfo</div>;
+const WishlistInfo = ({ id, desc, name, images, excerpts }) => {
+  return (
+    <Container>
+      <ImageContainer>
+        <StyledImage
+          src={images?.[0] ?? '/img/img-1.jpg'}
+          width={80}
+          height={80}
+          alt=''
+        />
+      </ImageContainer>
+      <OverviewContainer>
+        <ProductName>
+          <StyledLink href={`/products/${encodeURIComponent(id)}`} passHref>
+            {name}
+          </StyledLink>
+        </ProductName>
+        <Description>{excerpts(desc, 120)}</Description>
+      </OverviewContainer>
+    </Container>
+  );
 };
+
+const Container = styled.div``;
+
+const ImageContainer = styled.div`
+  width: 8rem;
+  height: 8rem;
+  background-color: #ede9e6;
+
+  @media only screen and (max-width: 18.75em) {
+    width: 6.5rem;
+    height: 6.5rem;
+  }
+`;
+
+const StyledImage = styled(Image)`
+  display: block;
+  width: 8rem;
+  height: 8rem;
+  object-fit: cover;
+
+  @media only screen and (max-width: 18.75em) {
+    width: 6.5rem;
+    height: 6.5rem;
+  }
+`;
+
+const OverviewContainer = styled.div``;
+
+const ProductName = styled.h2`
+  display: inline-block;
+  font-weight: 500;
+  font-size: 2rem;
+  color: ${({ theme }) => theme.textWlCardName};
+  margin-bottom: 0.5rem;
+  line-height: 1;
+
+  @media only screen and (max-width: 18.75em) {
+    font-size: 1.6rem;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: currentColor;
+  transition: all 0.3s ease;
+  outline-color: #f1e4f4;
+
+  &:hover {
+    text-decoration: underline;
+    text-decoration-color: ${({ theme }) => theme.textWlCardName};
+    text-underline-offset: 4px;
+    color: currentColor;
+  }
+`;
+
+const Description = styled.p`
+  font-size: 1.6rem;
+  color: ${({ theme }) => theme.textWlCard};
+  line-height: 1.2;
+  word-wrap: break-word;
+
+  @media only screen and (max-width: 18.75em) {
+    font-size: 1.4rem;
+  }
+`;
 
 export default WishlistInfo;
