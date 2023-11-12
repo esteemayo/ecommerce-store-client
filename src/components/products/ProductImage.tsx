8c5e44c9a38d2316e633f58e692d0639f1ec3244
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-13 00:18:57 +0100

    feat: create ProductImage component

---
+++ src/components/products/ProductImage.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import styled from 'styled-components';
+import Image from 'next/image';
+
+const ProductImage = () => {
+  return <div>ProductImage</div>;
+};
+
+export default ProductImage;
