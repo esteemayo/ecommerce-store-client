9c6b2d0f9900459ffe5d77259f0448a30b355fb6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-13 04:09:56 +0100

    fix: refactor

--- src/components/reviews/ReviewCard.tsx
+++ src/components/reviews/ReviewCard.tsx
@@ -179,34 +179,4 @@ const DateContainer = styled.div`
   justify-content: space-between;
 `;
 
-// const ShareButton = styled.button`
-//   display: flex;
-//   align-items: center;
-//   gap: 0.5rem;
-//   width: 8rem;
-//   padding: 0.7rem 1rem;
-//   font-size: 1.4rem;
-//   background-color: transparent;
-//   color: ${({ theme }) => theme.textRevDate};
-//   border: 1px solid ${({ theme }) => theme.textRevDate};
-//   border-radius: 0.5rem;
-//   outline-color: #ccc;
-//   cursor: pointer;
-
-//   @media only screen and (max-width: 26.875em) {
-//     font-size: 1.35rem;
-//   }
-
-//   @media only screen and (max-width: 25em) {
-//     gap: 1rem;
-//     font-size: 1.3rem;
-//     padding: 0.5rem 0.8rem;
-//   }
-
-//   svg {
-//     font-size: inherit;
-//     color: currentColor;
-//   }
-// `;
-
 export default ReviewCard;
