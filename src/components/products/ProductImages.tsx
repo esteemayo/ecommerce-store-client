bc3b1faea6c11bad3f1229276dc469354b19cc23
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-13 03:19:37 +0100

    fix: refactor

--- src/components/products/ProductImages.tsx
+++ src/components/products/ProductImages.tsx
@@ -146,38 +146,6 @@ const Wrapper = styled.figure`
   }
 `;
 
-const StyledImage = styled(Image)`
-  display: block;
-  width: 35rem;
-  height: 35rem;
-  object-fit: cover;
-  border-radius: 2px;
-  cursor: pointer;
-
-  @media only screen and (max-width: 64em) {
-    width: 30rem;
-  }
-
-  @media only screen and (max-width: 59.375em) {
-    width: 23rem;
-    height: 30rem;
-  }
-
-  @media only screen and (max-width: 50em) {
-    width: 225px;
-    border-radius: revert;
-    cursor: default;
-  }
-
-  @media only screen and (max-width: 37.5em) {
-    height: 20rem;
-  }
-
-  @media only screen and (max-width: 18.75em) {
-    height: 15rem;
-  }
-`;
-
 const ArrowButton = styled.button<IBtn>`
   border: none;
   width: 4rem;
