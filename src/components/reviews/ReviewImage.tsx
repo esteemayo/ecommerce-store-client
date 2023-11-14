3b720cf842b403740ac9d277e48652f644c44740
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-14 20:27:05 +0100

    feat: add markup

--- src/components/reviews/ReviewImage.tsx
+++ src/components/reviews/ReviewImage.tsx
@@ -1,9 +1,36 @@
 'use client';
 
 import styled from 'styled-components';
+import Image from 'next/image';
 
-const ReviewImage = () => {
-  return <div>ReviewImage</div>;
+const ReviewImage = ({ photo }) => {
+  return (
+    <Container>
+      <StyledImage src={photo} width={1200} height={1200} alt='' />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.bgRevImg};
+
+  @media only screen and (max-width: 62.5em) {
+    height: 45.2rem;
+  }
+
+  @media only screen and (max-width: 50em) {
+    height: 40.3rem;
+  }
+`;
+
+const StyledImage = styled(Image)`
+  width: 100%;
+  height: 100%;
+  display: block;
+  object-fit: cover;
+`;
 
 export default ReviewImage;
