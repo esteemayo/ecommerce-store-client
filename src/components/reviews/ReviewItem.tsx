b0b5b7fe6a7c8ed8264a64879bb33943a30bf0f4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-14 20:29:19 +0100

    fix: refactor

--- src/components/reviews/ReviewItem.tsx
+++ src/components/reviews/ReviewItem.tsx
@@ -66,11 +66,4 @@ const Right = styled.div`
   }
 `;
 
-const StyledImage = styled(Image)`
-  width: 100%;
-  height: 100%;
-  display: block;
-  object-fit: cover;
-`;
-
 export default ReviewItem;
