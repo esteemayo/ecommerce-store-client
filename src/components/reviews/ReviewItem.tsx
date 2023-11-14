3d308a5c0f7b388f444d8926e2122b9bc4225371
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-14 20:29:42 +0100

    fix: refactor

--- src/components/reviews/ReviewItem.tsx
+++ src/components/reviews/ReviewItem.tsx
@@ -51,19 +51,4 @@ const Wrapper = styled.div`
   height: 100%;
 `;
 
-const Right = styled.div`
-  flex: 1;
-  width: 100%;
-  height: 100%;
-  background-color: ${({ theme }) => theme.bgRevImg};
-
-  @media only screen and (max-width: 62.5em) {
-    height: 45.2rem;
-  }
-
-  @media only screen and (max-width: 50em) {
-    height: 40.3rem;
-  }
-`;
-
 export default ReviewItem;
