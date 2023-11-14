cf2ac2435e80aa39efa116a30403a97ba325e8e9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-14 19:51:15 +0100

    feat: add markup

--- src/components/reviews/ReviewInfo.tsx
+++ src/components/reviews/ReviewInfo.tsx
@@ -2,8 +2,38 @@
 
 import styled from 'styled-components';
 
-const ReviewInfo = () => {
-  return <div>ReviewInfo</div>;
+const ReviewInfo = ({ review, reviewer }) => {
+  return (
+    <Container>
+      <Review>{review}</Review>
+      <Reviewer>{reviewer}</Reviewer>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const Review = styled.p`
+  width: 70%;
+  font-weight: 600;
+  font-size: 1.8rem;
+  color: ${({ theme }) => theme.textRev};
+
+  @media only screen and (max-width: 18.75em) {
+    width: 60%;
+    font-size: 1.65rem;
+  }
+`;
+
+const Reviewer = styled.span`
+  display: inline-block;
+  font-weight: 300;
+  font-weight: 1.6rem;
+  color: ${({ theme }) => theme.textReviewer};
+`;
 
 export default ReviewInfo;
