bf4ab61f489c68469eaafc95974a9cfcb3707cb4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-14 20:16:19 +0100

    feat: add markup

--- src/components/reviews/ReviewContent.tsx
+++ src/components/reviews/ReviewContent.tsx
@@ -2,8 +2,38 @@
 
 import styled from 'styled-components';
 
-const ReviewContent = () => {
-  return <div>ReviewContent</div>;
+import RatingInfo from './RatingInfo';
+import ReviewInfo from './ReviewInfo';
+import ReviewLink from './ReviewLink';
+
+const ReviewContent = ({ rating, review, reviewer }) => {
+  return (
+    <Container>
+      <RatingInfo rating={rating} />
+      <ReviewInfo review={review} reviewer={reviewer} />
+      <ReviewLink />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+  width: 100%;
+  height: 100%;
+  padding: 8rem 0 10rem 4rem;
+  background-color: ${({ theme }) => theme.bgRevs};
+
+  @media only screen and (max-width: 64em) {
+    padding-container: 3rem;
+  }
+
+  @media only screen and (max-width: 50em) {
+    padding-top: 5rem;
+    gap: 3rem;
+  }
+`;
 
 export default ReviewContent;
