197d502e329d7ab207e9250ed4bbefb5e05cf155
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-14 20:21:54 +0100

    fix: refactor

--- src/components/reviews/ReviewItem.tsx
+++ src/components/reviews/ReviewItem.tsx
@@ -52,26 +52,6 @@ const Wrapper = styled.div`
   height: 100%;
 `;
 
-const Left = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  gap: 5rem;
-  width: 100%;
-  height: 100%;
-  padding: 8rem 0 10rem 4rem;
-  background-color: ${({ theme }) => theme.bgRevs};
-
-  @media only screen and (max-width: 64em) {
-    padding-left: 3rem;
-  }
-
-  @media only screen and (max-width: 50em) {
-    padding-top: 5rem;
-    gap: 3rem;
-  }
-`;
-
 const Right = styled.div`
   flex: 1;
   width: 100%;
