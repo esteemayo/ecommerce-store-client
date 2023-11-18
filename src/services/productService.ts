6aea25c532255428e131864e1c0ca21a470c8feb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-19 00:24:56 +0100

    fix: add fetch all products endpoint

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -2,8 +2,10 @@ import http from './httpService';
 
 const apiEndpoint = '/products';
 
-export const getFeaturedProducts = () =>
-  http.get(`${apiEndpoint}?featured=true`);
+export const getProducts = () => http.get(apiEndpoint);
 
 export const getCategoryCount = () =>
   http.get(`${apiEndpoint}/count-by-category`);
+  
+  export const getFeaturedProducts = () =>
+    http.get(`${apiEndpoint}?featured=true`);
